75366bfe9b211800cd2c154bb2cd37ce7d31b5a3
Samuel Attard <samuel.r.attard@gmail.com>
2016-12-04 20:20:31 +1100

    feat(importer): fix the projects gitignore on import

--- src/util/terminate.js
+++ src/util/terminate.js
@@ -4,17 +4,13 @@ import ora from 'ora';
 
 const d = debug('electron-forge:lifecycle');
 
-const runningOras = {};
-
 process.on('unhandledRejection', (err) => {
-  Object.keys(runningOras).forEach(key => runningOras[key].fail());
   process.stdout.write('\n\nAn unhandled rejection has occurred inside Forge:\n');
   console.error(colors.red(err.stack || JSON.stringify(err)));
   process.exit(1);
 });
 
 process.on('uncaughtException', (err) => {
-  Object.keys(runningOras).forEach(key => runningOras[key].fail());
   process.stdout.write('\n\nAn unhandled exception has occurred inside Forge:\n');
   console.error(colors.red(err.stack || JSON.stringify(err)));
   process.exit(1);
@@ -44,28 +40,5 @@ if (process.env.DEBUG && process.env.DEBUG.includes('electron-forge')) {
     return fake;
   };
 } else {
-  let oraID = 1;
-  ora.ora = (name) => {
-    const createdOra = ora(name);
-    createdOra.id = oraID;
-    const fns = {};
-    fns.start = createdOra.start.bind(createdOra);
-    fns.stop = createdOra.stop.bind(createdOra);
-    fns.succeed = createdOra.succeed.bind(createdOra);
-    fns.fail = createdOra.fail.bind(createdOra);
-    createdOra.start = () => {
-      runningOras[createdOra.id] = createdOra;
-      fns.start();
-      return createdOra;
-    };
-    ['stop', 'succeed', 'fail'].forEach((fnName) => {
-      createdOra[fnName] = (...args) => {
-        delete runningOras[createdOra.id];
-        fns[fnName](...args);
-        return createdOra;
-      };
-    });
-    oraID += 1;
-    return createdOra;
-  };
+  ora.ora = ora;
 }
