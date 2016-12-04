d320d9ec985b796d9ef3a290c4bf5d5637295969
Mark Lee <electronjs@lazymalevolence.com>
2016-12-03 17:39:59 -0800

    fix(packager): throw error when electron-prebuilt-compile is not found

--- src/util/resolve-dir.js
+++ src/util/resolve-dir.js
@@ -14,9 +14,13 @@ export default async (dir) => {
     if (await fs.exists(testPath)) {
       const packageJSON = JSON.parse(await fs.readFile(testPath, 'utf8'));
 
-      if (packageJSON.devDependencies && packageJSON.devDependencies['electron-prebuilt-compile']
-          && !/[0-9]/.test(packageJSON.devDependencies['electron-prebuilt-compile'][0])) {
-        global._resolveError = () => console.error('You must depend on an EXACT version of "electron-prebuilt-compile" not a range'.red);
+      if (packageJSON.devDependencies && packageJSON.devDependencies['electron-prebuilt-compile']) {
+        if (!/[0-9]/.test(packageJSON.devDependencies['electron-prebuilt-compile'][0])) {
+          global._resolveError = () => console.error('You must depend on an EXACT version of "electron-prebuilt-compile" not a range'.red);
+          return null;
+        }
+      } else {
+        global._resolveError = () => console.error('You must depend on "electron-prebuilt-compile" in your devDependencies'.red);
         return null;
       }
 
