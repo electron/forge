63ff52b2310a164e20c3db57678964e4d119aa4c
Mark Lee <electronjs@lazymalevolence.com>
2016-12-03 21:53:49 -0800

    fix(maker): correct path/arch of generated deb file

--- src/makers/linux/deb.js
+++ src/makers/linux/deb.js
@@ -4,13 +4,27 @@ import pify from 'pify';
 
 import { ensureFile } from '../../util/ensure-output';
 
+function debianArch(nodeArch) {
+  switch (nodeArch) {
+    case 'ia32': return 'i386';
+    case 'x64': return 'amd64';
+    case 'arm':
+      if (process.config.variables.arm_version === '7') {
+        return 'armhf';
+      }
+      return 'armel';
+    default: return nodeArch;
+  }
+}
+
 export default async (dir, appName, forgeConfig, packageJSON) => { // eslint-disable-line
-  const outPath = path.resolve(dir, '../make/debian');
+  const arch = debianArch(process.arch);
+  const outPath = path.resolve(dir, '../make', `${packageJSON.name}_${packageJSON.version}_${arch}.deb`);
 
   await ensureFile(outPath);
   const debianDefaults = {
-    arch: process.arch, // DOES NOT WORK WITH ARM
-    dest: outPath,
+    arch,
+    dest: path.dirname(outPath),
     src: dir,
   };
   const debianConfig = Object.assign({}, forgeConfig.electronInstallerDebian, debianDefaults);
