16f63d24f78ca4d7ceff16c0e07bd3dcf63b5e61
Mark Lee <electronjs@lazymalevolence.com>
2016-12-03 20:41:00 -0800

    feat(maker): add the deb maker for the linux target
    Requires the app's "description" to be non-blank.

---
+++ src/makers/linux/deb.js
@@ -0,0 +1,19 @@
+import installer from 'electron-installer-debian';
+import path from 'path';
+import pify from 'pify';
+
+import { ensureFile } from '../../util/ensure-output';
+
+export default async (dir, appName, forgeConfig) => {
+  const outPath = path.resolve(dir, '../make/debian');
+
+  await ensureFile(outPath);
+  const debianDefaults = {
+    arch: process.arch, // DOES NOT WORK WITH ARM
+    dest: outPath,
+    src: dir,
+  };
+  const debianConfig = Object.assign({}, forgeConfig.electronInstallerDebian, debianDefaults);
+
+  await pify(installer)(debianConfig);
+};
