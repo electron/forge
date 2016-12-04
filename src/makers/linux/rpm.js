85821f273d669a29e2567545154fa0d08a61067f
Mark Lee <electronjs@lazymalevolence.com>
2016-12-03 22:34:45 -0800

    feat(maker): add the rpm maker for the linux target

---
+++ src/makers/linux/rpm.js
@@ -0,0 +1,33 @@
+import installer from 'electron-installer-redhat';
+import path from 'path';
+import pify from 'pify';
+
+import { ensureFile } from '../../util/ensure-output';
+
+function rpmArch(nodeArch) {
+  switch (nodeArch) {
+    case 'ia32': return 'i386';
+    case 'x64': return 'x86_64';
+    case 'arm':
+      if (process.config.variables.arm_version === '7') {
+        return 'armv7hl';
+      }
+      return 'armv6hl';
+    default: return nodeArch;
+  }
+}
+
+export default async (dir, appName, forgeConfig, packageJSON) => { // eslint-disable-line
+  const arch = rpmArch(process.arch);
+  const outPath = path.resolve(dir, '../make', `${packageJSON.name}_${packageJSON.version}_${arch}.rpm`);
+
+  await ensureFile(outPath);
+  const rpmDefaults = {
+    arch,
+    dest: path.dirname(outPath),
+    src: dir,
+  };
+  const rpmConfig = Object.assign({}, forgeConfig.electronInstallerRedhat, rpmDefaults);
+
+  await pify(installer)(rpmConfig);
+};
