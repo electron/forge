07defb76de59b8b54a578dcb4085edaa8a4b7b88
Samuel Attard <samuel.r.attard@gmail.com>
2016-12-11 18:22:35 +1100

    chore(generic): fix changelog link parsing

--- ci/fix-changelog.js
+++ ci/fix-changelog.js
@@ -7,8 +7,8 @@ const changelog = fs.readFileSync(changelogPath, 'utf8');
 
 const fixedChangelog = changelog.replace(/\(([A-Za-z0-9]{8})\)/g,
   (match, commitID) => `([${commitID}](https://github.com/marshallofsound/electron-forge/commit/${commitID}))`
-).replace(/#### ([0-9]+\.[0-9]+\.[0-9]+) /g,
-  (match, version) => `#### [${version}](https://github.com/MarshallOfSound/electron-forge/releases/tag/v${version}) `
+).replace(/# ([0-9]+\.[0-9]+\.[0-9]+) /g,
+  (match, version) => `# [${version}](https://github.com/MarshallOfSound/electron-forge/releases/tag/v${version}) `
 );
 
 fs.writeFileSync(changelogPath, fixedChangelog);
