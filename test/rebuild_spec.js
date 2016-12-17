d20387b78a2589dc3bbc0e0f37b5d43a2ccfe645
Samuel Attard <samuel.r.attard@gmail.com>
2016-12-17 16:31:29 +1100

    refactor(tests): use different native modules so the tests run on CI

--- test/rebuild_spec.js
+++ test/rebuild_spec.js
@@ -1,10 +1,7 @@
 import fs from 'fs-promise';
-import mkdirp from 'mkdirp';
 import path from 'path';
-import pify from 'pify';
 import os from 'os';
 import ora from 'ora';
-import rimraf from 'rimraf';
 import { spawn as yarnOrNPMSpawn, hasYarn } from 'yarn-or-npm';
 
 import { expect } from 'chai';
@@ -17,14 +14,13 @@ describe('rebuilder', () => {
   const testModulePath = path.resolve(os.tmpdir(), 'electron-forge-rebuild-test');
 
   before(async () => {
-    await pify(rimraf)(testModulePath);
-    console.log(testModulePath);
-    await pify(mkdirp)(testModulePath);
+    await fs.remove(testModulePath);
+    await fs.mkdirs(testModulePath);
     await fs.writeFile(path.resolve(testModulePath, 'package.json'), await fs.readFile(path.resolve(__dirname, 'fixture/native_app/package.json'), 'utf8'));
     await new Promise((resolve, reject) => {
       const child = yarnOrNPMSpawn(hasYarn() ? [] : ['install'], {
         cwd: testModulePath,
-        stdio: 'inherit',
+        stdio: process.platform === 'win32' ? 'inherit' : 'pipe',
       });
       child.on('exit', (code) => {
         if (code === 0) resolve();
@@ -43,18 +39,18 @@ describe('rebuilder', () => {
   });
 
   it('should have rebuilt children of top level prod dependencies', async () => {
-    const forgeMeta = path.resolve(testModulePath, 'node_modules', 'mdns', 'build', 'Release', '.forge-meta');
-    expect(await fs.exists(forgeMeta), 'mdns build meta should exist').to.equal(true);
+    const forgeMeta = path.resolve(testModulePath, 'node_modules', 'microtime', 'build', 'Release', '.forge-meta');
+    expect(await fs.exists(forgeMeta), 'microtime build meta should exist').to.equal(true);
   });
 
   it('should have rebuilt children of scoped top level prod dependencies', async () => {
-    const forgeMeta = path.resolve(testModulePath, 'node_modules', 'bcrypt', 'build', 'Release', '.forge-meta');
-    expect(await fs.exists(forgeMeta), 'bcrypt build meta should exist').to.equal(true);
+    const forgeMeta = path.resolve(testModulePath, 'node_modules', '@newrelic/native-metrics', 'build', 'Release', '.forge-meta');
+    expect(await fs.exists(forgeMeta), '@newrelic/native-metrics build meta should exist').to.equal(true);
   });
 
   it('should have rebuilt top level optional dependencies', async () => {
-    const forgeMeta = path.resolve(testModulePath, 'node_modules', 'utp-native', 'build', 'Release', '.forge-meta');
-    expect(await fs.exists(forgeMeta), 'utp-native build meta should exist').to.equal(true);
+    const forgeMeta = path.resolve(testModulePath, 'node_modules', 'zipfile', 'build', 'Release', '.forge-meta');
+    expect(await fs.exists(forgeMeta), 'zipfile build meta should exist').to.equal(true);
   });
 
   it('should not have rebuilt top level devDependencies', async () => {
@@ -63,6 +59,6 @@ describe('rebuilder', () => {
   });
 
   after(async () => {
-    await pify(rimraf)(testModulePath);
+    await fs.remove(testModulePath);
   });
 });
