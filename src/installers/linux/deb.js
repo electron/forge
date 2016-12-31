9834cb1b4121b7dd517440cb389ef40d27b5d5e8
Mark Lee <electronjs@lazymalevolence.com>
2016-12-30 23:13:37 -0800

    refactor(installer): replace sudo-prompt with git branch of electron-sudo for Linux installers

--- src/installers/linux/deb.js
+++ src/installers/linux/deb.js
@@ -1,10 +1,5 @@
-import pify from 'pify';
-import sudo from 'sudo-prompt';
-
-import linuxInstaller from '../../util/linux-installer';
+import { sudo } from '../../util/linux-installer';
 
 export default async (filePath) => {
-  linuxInstaller('Debian', 'gdebi', pify(sudo.exec)(`gdebi -n ${filePath}`, {
-    name: 'Electron Forge',
-  }));
+  sudo('Debian', 'gdebi', `-n ${filePath}`);
 };
--- src/util/linux-installer.js
+++ src/util/linux-installer.js
@@ -1,9 +1,25 @@
 import { spawnSync } from 'child_process';
+import { default as Sudoer } from 'electron-sudo';
 
-export default async (type, prog, promise) => {
+async function which(type, prog, promise) {
   if (spawnSync('which', [prog]).status === 0) {
     await promise;
   } else {
     throw new Error(`${prog} is required to install ${type} packages`);
   }
-};
+}
+
+async function sudo(type, prog, args) {
+  const sudoer = new Sudoer({ name: 'Electron Forge' });
+  which(type, prog, sudoer.spawn(`${prog} ${args}`).then((child) => {
+    child.on('exit', async (code) => {
+      if (code !== 0) {
+        console.error(child.output.stdout.toString('utf8'));
+        console.error(child.output.stderr.toString('utf8'));
+        throw new Error(`${prog} failed with status code ${code}`);
+      }
+    });
+  }));
+}
+
+export { which as default, sudo };
