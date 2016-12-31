a0b5ac703f269d91b13e183140791c1f895d676a
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-01 09:26:14 +1300

    fix(installer): await promises through the linux install chain

--- src/installers/linux/rpm.js
+++ src/installers/linux/rpm.js
@@ -1,5 +1,5 @@
 import { sudo } from '../../util/linux-installer';
 
 export default async (filePath) => {
-  sudo('RPM', 'dnf', `--assumeyes --nogpgcheck install ${filePath}`);
+  await sudo('RPM', 'dnf', `--assumeyes --nogpgcheck install ${filePath}`);
 };
--- src/util/linux-installer.js
+++ src/util/linux-installer.js
@@ -1,25 +1,29 @@
 import { spawnSync } from 'child_process';
 import { default as Sudoer } from 'electron-sudo';
 
-async function which(type, prog, promise) {
+const which = async (type, prog, promise) => {
   if (spawnSync('which', [prog]).status === 0) {
     await promise;
   } else {
     throw new Error(`${prog} is required to install ${type} packages`);
   }
-}
+};
 
-async function sudo(type, prog, args) {
-  const sudoer = new Sudoer({ name: 'Electron Forge' });
-  which(type, prog, sudoer.spawn(`${prog} ${args}`).then((child) => {
-    child.on('exit', async (code) => {
-      if (code !== 0) {
-        console.error(child.output.stdout.toString('utf8'));
-        console.error(child.output.stderr.toString('utf8'));
-        throw new Error(`${prog} failed with status code ${code}`);
-      }
-    });
-  }));
-}
+export const sudo = (type, prog, args) =>
+  new Promise((resolve, reject) => {
+    const sudoer = new Sudoer({ name: 'Electron Forge' });
 
-export { which as default, sudo };
+    which(type, prog, sudoer.spawn(`${prog} ${args}`)
+      .then((child) => {
+        child.on('exit', async (code) => {
+          if (code !== 0) {
+            console.error(child.output.stdout.toString('utf8').red);
+            console.error(child.output.stderr.toString('utf8').red);
+            return reject(new Error(`${prog} failed with status code ${code}`));
+          }
+          resolve();
+        });
+      }));
+  });
+
+export default which;
