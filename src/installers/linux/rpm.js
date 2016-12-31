2ab37e3b089d456f0a9b0a1bd2e63e19ff246566
Mark Lee <electronjs@lazymalevolence.com>
2016-12-30 23:13:37 -0800

    refactor(installer): replace sudo-prompt with git branch of electron-sudo for Linux installers

--- src/installers/linux/rpm.js
+++ src/installers/linux/rpm.js
@@ -1,10 +1,5 @@
-import pify from 'pify';
-import sudo from 'sudo-prompt';
-
-import linuxInstaller from '../../util/linux-installer';
+import { sudo } from '../../util/linux-installer';
 
 export default async (filePath) => {
-  linuxInstaller('RPM', 'dnf', pify(sudo.exec)(`dnf --assumeyes --nogpgcheck install ${filePath}`, {
-    name: 'Electron Forge',
-  }));
+  sudo('RPM', 'dnf', `--assumeyes --nogpgcheck install ${filePath}`);
 };
