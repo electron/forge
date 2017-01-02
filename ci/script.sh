2f82115505ae40cfc7b5926bac1ff68c07f25d30
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-02 13:45:24 +1300

    chore(generic): enable coveralls

--- ci/script.sh
+++ ci/script.sh
@@ -5,5 +5,7 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
     sudo docker run -it electron-forge-ci ci/docker.sh $NODE_INSTALLER
 else
     if [[ "$NODE_INSTALLER" = "yarn" ]]; then npm i -g yarn; fi
-    npm run test -- --installer=$NODE_INSTALLER
+    npm run test-coverage -- --installer=$NODE_INSTALLER
+    # Only publish coverage on the macOS yarn agent
+    if [[ "$NODE_INSTALLER" = "yarn" ]]; then cat coverage/lcov.info | node node_modules/coveralls/bin/coveralls.js; fi
 fi
