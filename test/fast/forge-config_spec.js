75f217a57bd75cc110c15d7e0def4f9a92c3d6b5
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-03 18:58:40 +1100

    chore(tests): fix appx tests on rebased branch

--- test/fast/forge-config_spec.js
+++ test/fast/forge-config_spec.js
@@ -5,7 +5,7 @@ import findConfig from '../../src/util/forge-config';
 
 const defaults = {
   make_targets: {
-    win32: ['squirrel'],
+    win32: ['squirrel', 'appx'],
     darwin: ['zip'],
     linux: ['deb', 'rpm'],
     mas: ['zip'],
@@ -21,6 +21,7 @@ describe('forge-config', () => {
   it('should resolve the object in package.json with defaults  if one exists', async () => {
     expect(await findConfig(path.resolve(__dirname, '../fixture/dummy_app'))).to.be.deep.equal(Object.assign({}, defaults, {
       electronWinstallerConfig: { windows: 'magic' },
+      windowsStoreConfig: { packageName: 'test' },
     }));
   });
 
