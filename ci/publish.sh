d4b008d346a539107bfef8550a58b15e55c1f5cd
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-04 09:38:52 +1100

    chore(generic): only publish CI coverage on success

---
+++ ci/publish.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+    # Only publish coverage on the macOS yarn agent
+    if [[ "$NODE_INSTALLER" = "yarn" ]]; then cat coverage/lcov.info | node node_modules/coveralls/bin/coveralls.js; fi
+fi
