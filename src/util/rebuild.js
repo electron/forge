26f23b48e9c051f2a825671f7cd1f90066b2880f
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-07 18:56:20 +1100

    feat(rebuilder): show rebuild progress from the electron-rebuild lifecycle

--- src/util/rebuild.js
+++ src/util/rebuild.js
@@ -1,3 +1,22 @@
 import rebuild from 'electron-rebuild';
 
-export default (buildPath, electronVersion, platform, arch) => rebuild(buildPath, electronVersion, arch);
+import asyncOra from '../util/ora-handler';
+
+export default async (buildPath, electronVersion, platform, arch) => {
+  await asyncOra('Preparing native dependencies', async (rebuildSpinner) => {
+    const rebuilder = rebuild(buildPath, electronVersion, arch);
+    const { lifecycle } = rebuilder;
+
+    let found = 0;
+    let done = 0;
+
+    const redraw = () => {
+      rebuildSpinner.text = `Preparing native dependencies: ${done} / ${found}`; // eslint-disable-line
+    };
+
+    lifecycle.on('module-found', () => { found += 1; redraw(); });
+    lifecycle.on('module-done', () => { done += 1; redraw(); });
+
+    await rebuilder;
+  });
+};
