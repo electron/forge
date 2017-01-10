7ea5af8abf7043cd0caf12f988169a78fe7e2ba1
Samuel Attard <samuel.r.attard@gmail.com>
2017-01-11 00:23:22 +1100

    feat(installer): manually mount and scan a DMG file when installing for the .app
    copy the .app file discovered from the DMG into the users Applications directory

--- src/installers/darwin/dmg.js
+++ src/installers/darwin/dmg.js
@@ -1,5 +1,32 @@
-import opn from 'opn';
+import { spawn } from 'child_process';
+import fs from 'fs-promise';
+import path from 'path';
 
-export default async (filePath) => {
-  await opn(filePath, { wait: false });
+import { getMountedImages, mountImage, unmountImage } from '../../util/hdiutil';
+import moveApp from '../../util/move-app';
+
+export default async (filePath, installSpinner) => {
+  const mounts = await getMountedImages();
+  let targetMount = mounts.find(mount => mount.imagePath === filePath);
+
+  if (!targetMount) {
+    targetMount = await mountImage(filePath);
+  }
+
+  try {
+    const volumePath = path.resolve('/Volumes', targetMount.mountPath);
+    const appName = (await fs.readdir(volumePath)).find(file => file.endsWith('.app'));
+    if (!appName) {
+      // eslint-disable-next-line no-throw-literal
+      throw 'Failed to find .app file in DMG';
+    }
+    const appPath = path.resolve(volumePath, appName);
+    const targetApplicationPath = `/Applications/${path.basename(appPath)}`;
+
+    await moveApp(appPath, targetApplicationPath, installSpinner, true);
+
+    spawn('open', ['-R', targetApplicationPath], { detached: true });
+  } finally {
+    await unmountImage(targetMount);
+  }
 };
