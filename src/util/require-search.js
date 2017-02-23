b53af7f45da106f41d58fb67cc94a795b5047bd5
@anulman <anulman@gmail.com>
2017-02-22 22:50:06 -0500

    refactor(util): extend requireSearch to export a raw search fn

--- src/util/require-search.js
+++ src/util/require-search.js
@@ -3,20 +3,23 @@ import path from 'path';
 
 const d = debug('electron-forge:require-search');
 
-export default (relativeTo, paths) => {
+export function requireSearchRaw(relativeTo, paths) {
   const testPaths = paths
     .concat(paths.map(mapPath => path.resolve(relativeTo, mapPath)))
     .concat(paths.map(mapPath => path.resolve(relativeTo, 'node_modules', mapPath)));
   d('searching', testPaths, 'relative to', relativeTo);
-  let result;
   for (const testPath of testPaths) {
     try {
       d('testing', testPath);
-      result = require(testPath);
-      return typeof result === 'object' && result && result.default ? result.default : result;
+      return require(testPath);
     } catch (err) {
       // Ignore the error
     }
   }
   d('failed to find a module in', testPaths);
+}
+
+export default (relativeTo, paths) => {
+  const result = requireSearchRaw(relativeTo, paths);
+  return typeof result === 'object' && result && result.default ? result.default : result;
 };
