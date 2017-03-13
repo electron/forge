8af37ec2f9acbc8ccc7e2dcc698ed4e539c629d9
@anulman <anulman@gmail.com>
2017-03-13 19:49:06 -0400

    test(maker): add test to confirm dummy maker does not get called

---
+++ test/fixture/dummy-maker.js
@@ -0,0 +1,5 @@
+export const supportedPlatforms = [];
+
+export default async (dir, appName, targetArch, forgeConfig, packageJSON) => { // eslint-disable-line
+  return 'i will resolve, but i\'m supposed to throw upstream';
+};
