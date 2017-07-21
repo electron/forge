c0b8463d6ae1ec3af486b36a2fb565ab805f2769
Samuel Attard <samuel.r.attard@gmail.com>
2017-07-21 10:50:55 +1000

    chore(tests): fixup comma arch test

--- test/slow/api_spec_slow.js
+++ test/slow/api_spec_slow.js
@@ -293,11 +293,14 @@ describe(`electron-forge API (with installer=${installer.substr(12)})`, () => {
           })).to.eventually.be.rejectedWith(/incompatible with this version/);
         });
 
-        it('should succeed when provided comma seperated arches', async () => {
+        it('should succeed when provided comma separated arches', async () => {
           // Darwin only has x64 so we can't do this test on that platform
+          if (process.platform === 'darwin') return;
+
           await forge.make({
             dir,
-            arch: process.platform === 'darwin' ? 'x64' : 'ia32,x64',
+            arch: 'ia32,x64',
+            overrideTargets: ['zip'],
           });
         });
       });
