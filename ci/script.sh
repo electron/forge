0648c1903e098cb00808b82f7445d23a629aff5f
Mark Lee <electronjs@lazymalevolence.com>
2017-07-24 21:24:27 -0700

    Only install specific yarn when NODE_INSTALLER=yarn

--- ci/script.sh
+++ ci/script.sh
@@ -4,6 +4,5 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
     sudo docker build -f ci/Dockerfile . -t electron-forge-ci
     sudo docker run -it electron-forge-ci ci/docker.sh $NODE_INSTALLER
 else
-    if [[ "$NODE_INSTALLER" = "yarn" ]]; then npm i -g yarn; fi
     npm run test-coverage -- --installer=$NODE_INSTALLER
 fi
