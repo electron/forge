46486adb954542cbadcf4114f8331edfc2d6c146
Mark Lee <electronjs@lazymalevolence.com>
2017-08-06 15:12:38 -0700

    Refactor

--- src/util/check-system.js
+++ src/util/check-system.js
@@ -20,23 +20,23 @@ async function checkNodeVersion() {
 const NPM_WHITELISTED_VERSIONS = '^3.0.0 || ^4.0.0 || ~5.1.0 || ~5.2.0 || >= 5.4.0';
 const YARN_WHITELISTED_VERSIONS = '0.23.3 || 0.24.6';
 
-function versionWarning(packageManager, whitelistedVersions) {
-  console.warn(
-    logSymbols.warning,
-    (`You are using ${packageManager}, but not a known good version. The known ` +
-     `versions that work with Electron Forge are: ${whitelistedVersions}`).yellow
-  );
+function warnIfPackageManagerIsntAKnownGoodVersion(packageManager, version, whitelistedVersions) {
+  if (!semver.satisfies(version, whitelistedVersions)) {
+    console.warn(
+      logSymbols.warning,
+      (`You are using ${packageManager}, but not a known good version. The known ` +
+        `versions that work with Electron Forge are: ${whitelistedVersions}`).yellow
+    );
+  }
 }
 
 async function checkPackageManagerVersion() {
   return yarnOrNpmSpawn(['--version'])
     .then((version) => {
       if (hasYarn()) {
-        if (!semver.satisfies(version, YARN_WHITELISTED_VERSIONS)) {
-          versionWarning('Yarn', YARN_WHITELISTED_VERSIONS);
-        }
-      } else if (!semver.satisfies(version, NPM_WHITELISTED_VERSIONS)) {
-        versionWarning('NPM', NPM_WHITELISTED_VERSIONS);
+        warnIfPackageManagerIsntAKnownGoodVersion('Yarn', version, YARN_WHITELISTED_VERSIONS);
+      } else {
+        warnIfPackageManagerIsntAKnownGoodVersion('NPM', version, NPM_WHITELISTED_VERSIONS);
       }
 
       return true;
