d1abc62ccea14a071ba449e9af551c5b54d23cc5
Mark Lee <electronjs@lazymalevolence.com>
2017-08-18 22:38:50 -0700

    fix(generic): fix installing tab completion when installing Forge locally

--- tabtab-install.js
+++ tabtab-install.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const { findActualExecutable, spawnPromise } = require('spawn-rx');
-
-const tabtabPath = path.resolve(__dirname, './node_modules/.bin/tabtab');
-
-if (!fs.existsSync(path.resolve(__dirname, 'src'))) {
-  spawnPromise(findActualExecutable(tabtabPath).cmd, ['install', '--auto'], {
-    stdio: 'inherit',
-  }).catch(err => console.error('Failed to install tab completion:', err));
+process.argv.push('install');
+process.argv.push('--auto');
+try {
+  require('tabtab/src/cli');
+} catch (e) {
+  console.warning(`Failed to install tab completion: ${e}`);
 }
