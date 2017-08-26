2603b44609a7571d05b0cce270605776349ba49c
Mark Lee <electronjs@lazymalevolence.com>
2017-08-26 13:02:50 -0700

    refactor(publisher): rename target option to publishTargets in API
    BREAKING CHANGE: publish API option name renamed, only takes an Array of Strings.

--- src/api/publish.js
+++ src/api/publish.js
@@ -2,7 +2,6 @@ import 'colors';
 import path from 'path';
 
 import asyncOra from '../util/ora-handler';
-import deprecate from '../util/deprecate';
 import getForgeConfig from '../util/forge-config';
 import readPackageJSON from '../util/read-package-json';
 import requireSearch from '../util/require-search';
@@ -16,7 +15,7 @@ import make from './make';
  * @property {boolean} [interactive=false] Whether to use sensible defaults or prompt the user visually
  * @property {string} [authToken] An authentication token to use when publishing
  * @property {string} [tag=packageJSON.version] The string to tag this release with
- * @property {string} [target=github] The publish target
+ * @property {Array<string>} [publishTargets=[github]] The publish targets
  * @property {MakeOptions} [makeOptions] Options object to passed through to make()
  */
 
@@ -28,16 +27,14 @@ import make from './make';
  */
 export default async (providedOptions = {}) => {
   // eslint-disable-next-line prefer-const, no-unused-vars
-  let { dir, interactive, authToken, tag, target, makeOptions } = Object.assign({
+  let { dir, interactive, authToken, tag, publishTargets, makeOptions } = Object.assign({
     dir: process.cwd(),
     interactive: false,
     tag: null,
     makeOptions: {},
-    target: null,
+    publishTargets: null,
   }, providedOptions);
   asyncOra.interactive = interactive;
-  // FIXME(MarshallOfSound): Change the method param to publishTargets in the next major bump
-  let publishTargets = target;
 
   const makeResults = await make(Object.assign({
     dir,
@@ -60,10 +57,6 @@ export default async (providedOptions = {}) => {
 
   if (publishTargets === null) {
     publishTargets = forgeConfig.publish_targets[makeOptions.platform || process.platform];
-  } else if (typeof publishTargets === 'string') {
-    // FIXME(MarshallOfSound): Remove this fallback string typeof check in the next major bump
-    deprecate('publish target as a string').replaceWith('an array of publish targets');
-    publishTargets = [publishTargets];
   }
 
   for (const publishTarget of publishTargets) {
