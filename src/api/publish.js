chundany/electron-forge

feat(publisher): adds dryRun and resumeDryRun to the API to allow post-make publishes
Fixes #230

src/api/publish.js
@@ -1,4 +1,6 @@
 import 'colors';
+import debug from 'debug';
+import fs from 'fs-extra';
 import path from 'path';
 
 import asyncOra from '../util/ora-handler';
@@ -7,8 +9,11 @@ import getForgeConfig from '../util/forge-config';
 import readPackageJSON from '../util/read-package-json';
 import requireSearch from '../util/require-search';
 import resolveDir from '../util/resolve-dir';
+import PublishState from '../util/publish-state';
 
 import make from './make';
+
+const d = debug('electron-forge:publish');
 
 /**
  * @typedef {Object} PublishOptions
@@ -18,6 +23,10 @@ import make from './make';
  * @property {string} [tag=packageJSON.version] The string to tag this release with
  * @property {string} [target=github] The publish target
  * @property {MakeOptions} [makeOptions] Options object to passed through to make()
+ * @property {string} [outDir=`${dir}/out`] The path to the directory containing generated distributables
+ * @property {boolean} [dryRun=false] Whether or not to generate dry run meta data and not actually publish
+ * @property {boolean} [dryRunResume=false] Whether or not to attempt to resume a previously saved dryRun and publish
+ * @property {Object} [makeResults=null] Provide results from make so that the publish step doesn't run make itself
  */
 
 /**
@@ -26,23 +35,86 @@ import make from './make';
  * @param {PublishOptions} providedOptions - Options for the Publish method
  * @return {Promise} Will resolve when the publish process is complete
  */
-export default async (providedOptions = {}) => {
+const publish = async (providedOptions = {}) => {
   // eslint-disable-next-line prefer-const, no-unused-vars
-  let { dir, interactive, authToken, tag, target, makeOptions } = Object.assign({
+  let { dir, interactive, authToken, tag, target, makeOptions, dryRun, dryRunResume, makeResults } = Object.assign({
     dir: process.cwd(),
     interactive: false,
     tag: null,
     makeOptions: {},
     target: null,
+    dryRun: false,
+    dryRunResume: false,
+    makeResults: null,
   }, providedOptions);
   asyncOra.interactive = interactive;
   // FIXME(MarshallOfSound): Change the method param to publishTargets in the next major bump
   let publishTargets = target;
 
-  const makeResults = await make(Object.assign({
-    dir,
-    interactive,
-  }, makeOptions));
+  const outDir = providedOptions.outDir || path.resolve(dir, 'out');
+  const dryRunDir = path.resolve(outDir, 'publish-dry-run');
+
+  if (dryRun && dryRunResume) {
+    throw 'Can\'t dry run and resume a dry run at the same time';
+  }
+  if (dryRunResume && makeResults) {
+    throw 'Can\'t resume a dry run and use the provided makeResults at the same time';
+  }
+
+  let packageJSON = await readPackageJSON(dir);
+
+  let forgeConfig = await getForgeConfig(dir);
+
+  if (dryRunResume) {
+    d('attempting to resume from dry run');
+    const publishes = await PublishState.loadFromDirectory(dryRunDir);
+    for (const states of publishes) {
+      d('publishing for given state set');
+      await publish({
+        dir,
+        interactive,
+        authToken,
+        tag,
+        target,
+        makeOptions,
+        dryRun,
+        dryRunResume: false,
+        makeResults: states.map(({ state }) => state),
+      });
+    }
+    return;
+  } else if (!makeResults) {
+    d('triggering make');
+    makeResults = await make(Object.assign({
+      dir,
+      interactive,
+    }, makeOptions));
+  } else {
+    // Restore values from dry run
+    d('restoring publish settings from dry run');
+
+    for (const makeResult of makeResults) {
+      packageJSON = makeResult.packageJSON;
+      forgeConfig = makeResult.forgeConfig;
+      makeOptions.platform = makeResult.platform;
+      makeOptions.arch = makeResult.arch;
+
+      for (const makePath of makeResult.paths) {
+        if (!await fs.exists(makePath)) {
+          throw `Attempted to resume a dry run but an artifact (${makePath}) could not be found`;
+        }
+      }
+    }
+
+    makeResults = makeResults.map(makeResult => makeResult.paths);
+  }
+
+  if (dryRun) {
+    d('saving results of make in dry run state');
+    await fs.remove(dryRunDir);
+    await PublishState.saveToDirectory(dryRunDir, makeResults);
+    return;
+  }
 
   dir = await resolveDir(dir);
   if (!dir) {
@@ -53,10 +125,6 @@ export default async (providedOptions = {}) => {
     accum.push(...arr);
     return accum;
   }, []);
-
-  const packageJSON = await readPackageJSON(dir);
-
-  const forgeConfig = await getForgeConfig(dir);
 
   if (publishTargets === null) {
     publishTargets = forgeConfig.publish_targets[makeOptions.platform || process.platform];
@@ -84,3 +152,5 @@ export default async (providedOptions = {}) => {
     await publisher(artifacts, packageJSON, forgeConfig, authToken, tag, makeOptions.platform || process.platform, makeOptions.arch || process.arch);
   }
 };
+
+export default publish;

electron/forge

src/api/publish.js
@@ -1,4 +1,6 @@
 import 'colors';
+import debug from 'debug';
+import fs from 'fs-extra';
 import path from 'path';
 
 import asyncOra from '../util/ora-handler';
@@ -6,8 +8,11 @@ import getForgeConfig from '../util/forge-config';
 import readPackageJSON from '../util/read-package-json';
 import requireSearch from '../util/require-search';
 import resolveDir from '../util/resolve-dir';
+import PublishState from '../util/publish-state';
 
 import make from './make';
+
+const d = debug('electron-forge:publish');
 
 /**
  * @typedef {Object} PublishOptions
@@ -17,6 +22,10 @@ import make from './make';
  * @property {string} [tag=packageJSON.version] The string to tag this release with
  * @property {Array<string>} [publishTargets=[github]] The publish targets
  * @property {MakeOptions} [makeOptions] Options object to passed through to make()
+ * @property {string} [outDir=`${dir}/out`] The path to the directory containing generated distributables
+ * @property {boolean} [dryRun=false] Whether or not to generate dry run meta data and not actually publish
+ * @property {boolean} [dryRunResume=false] Whether or not to attempt to resume a previously saved dryRun and publish
+ * @property {Object} [makeResults=null] Provide results from make so that the publish step doesn't run make itself
  */
 
 /**
@@ -25,21 +34,84 @@ import make from './make';
  * @param {PublishOptions} providedOptions - Options for the Publish method
  * @return {Promise} Will resolve when the publish process is complete
  */
-export default async (providedOptions = {}) => {
+const publish = async (providedOptions = {}) => {
   // eslint-disable-next-line prefer-const, no-unused-vars
-  let { dir, interactive, authToken, tag, publishTargets, makeOptions } = Object.assign({
+  let { dir, interactive, authToken, tag, publishTargets, makeOptions, dryRun, dryRunResume, makeResults } = Object.assign({
     dir: process.cwd(),
     interactive: false,
     tag: null,
     makeOptions: {},
     publishTargets: null,
+    dryRun: false,
+    dryRunResume: false,
+    makeResults: null,
   }, providedOptions);
   asyncOra.interactive = interactive;
 
-  const makeResults = await make(Object.assign({
-    dir,
-    interactive,
-  }, makeOptions));
+  const outDir = providedOptions.outDir || path.resolve(dir, 'out');
+  const dryRunDir = path.resolve(outDir, 'publish-dry-run');
+
+  if (dryRun && dryRunResume) {
+    throw 'Can\'t dry run and resume a dry run at the same time';
+  }
+  if (dryRunResume && makeResults) {
+    throw 'Can\'t resume a dry run and use the provided makeResults at the same time';
+  }
+
+  let packageJSON = await readPackageJSON(dir);
+
+  let forgeConfig = await getForgeConfig(dir);
+
+  if (dryRunResume) {
+    d('attempting to resume from dry run');
+    const publishes = await PublishState.loadFromDirectory(dryRunDir);
+    for (const states of publishes) {
+      d('publishing for given state set');
+      await publish({
+        dir,
+        interactive,
+        authToken,
+        tag,
+        target,
+        makeOptions,
+        dryRun,
+        dryRunResume: false,
+        makeResults: states.map(({ state }) => state),
+      });
+    }
+    return;
+  } else if (!makeResults) {
+    d('triggering make');
+    makeResults = await make(Object.assign({
+      dir,
+      interactive,
+    }, makeOptions));
+  } else {
+    // Restore values from dry run
+    d('restoring publish settings from dry run');
+
+    for (const makeResult of makeResults) {
+      packageJSON = makeResult.packageJSON;
+      forgeConfig = makeResult.forgeConfig;
+      makeOptions.platform = makeResult.platform;
+      makeOptions.arch = makeResult.arch;
+
+      for (const makePath of makeResult.paths) {
+        if (!await fs.exists(makePath)) {
+          throw `Attempted to resume a dry run but an artifact (${makePath}) could not be found`;
+        }
+      }
+    }
+
+    makeResults = makeResults.map(makeResult => makeResult.paths);
+  }
+
+  if (dryRun) {
+    d('saving results of make in dry run state');
+    await fs.remove(dryRunDir);
+    await PublishState.saveToDirectory(dryRunDir, makeResults);
+    return;
+  }
 
   dir = await resolveDir(dir);
   if (!dir) {
@@ -50,10 +122,6 @@ export default async (providedOptions = {}) => {
     accum.push(...arr);
     return accum;
   }, []);
-
-  const packageJSON = await readPackageJSON(dir);
-
-  const forgeConfig = await getForgeConfig(dir);
 
   if (publishTargets === null) {
     publishTargets = forgeConfig.publish_targets[makeOptions.platform || process.platform];
@@ -77,3 +145,5 @@ export default async (providedOptions = {}) => {
     await publisher(artifacts, packageJSON, forgeConfig, authToken, tag, makeOptions.platform || process.platform, makeOptions.arch || process.arch);
   }
 };
+
+export default publish;