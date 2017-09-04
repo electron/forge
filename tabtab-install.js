fc3c03016926c7c30ea468125c8c75c023afa684
Samuel Attard <samuel.r.attard@gmail.com>
2017-09-05 01:08:01 +1000

    fix(generic): tabtab install script fails on non-*nix systems
    ISSUES CLOSED: #314

--- tabtab-install.js
+++ tabtab-install.js
@@ -3,5 +3,5 @@ process.argv.push('--auto');
 try {
   require('tabtab/src/cli');
 } catch (e) {
-  console.warning(`Failed to install tab completion: ${e}`);
+  console.warn(`Failed to install tab completion: ${e}`);
 }
