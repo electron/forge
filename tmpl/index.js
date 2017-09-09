212ccebaa7cffd19ed52af537f80f7e737b670b2
Mark Lee <electronjs@lazymalevolence.com>
2017-09-09 14:45:41 -0700

    Disable global-require check for default template call to electron-squirrel-startup

--- tmpl/index.js
+++ tmpl/index.js
@@ -1,7 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
