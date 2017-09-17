a39011b8bb341c458ae6f9ed93b8d8067114f314
Mark Lee <electronjs@lazymalevolence.com>
2017-09-17 15:55:18 -0700

    refactor(maker): DRY up linux config transformations
    Add fast tests for linux maker config.

--- src/makers/linux/rpm.js
+++ src/makers/linux/rpm.js
@@ -2,12 +2,12 @@ import path from 'path';
 import pify from 'pify';
 
 import { ensureFile } from '../../util/ensure-output';
-import configFn from '../../util/config-fn';
 import isInstalled from '../../util/is-installed';
+import { linuxConfig, populateConfig } from '../../util/linux-config';
 
 export const isSupportedOnCurrentPlatform = async () => isInstalled('electron-installer-redhat');
 
-function rpmArch(nodeArch) {
+export function rpmArch(nodeArch) {
   switch (nodeArch) {
     case 'ia32': return 'i386';
     case 'x64': return 'x86_64';
@@ -21,18 +21,18 @@ export default async ({ dir, targetArch, forgeConfig, packageJSON }) => {
   const installer = require('electron-installer-redhat');
 
   const arch = rpmArch(targetArch);
-  const userConfig = configFn(forgeConfig.electronInstallerRedhat, targetArch);
-  userConfig.options = userConfig.options || {};
-  const versionedName = `${userConfig.options.name || packageJSON.name}-${packageJSON.version}.${arch}`;
+  const config = populateConfig({ forgeConfig, configKey: 'electronInstallerRedhat', targetArch });
+  const name = config.options.name || packageJSON.name;
+  const versionedName = `${name}-${packageJSON.version}.${arch}`;
   const outPath = path.resolve(dir, '../make', `${versionedName}.rpm`);
 
   await ensureFile(outPath);
-  const rpmDefaults = {
-    arch,
-    dest: path.dirname(outPath),
-    src: dir,
-  };
-  const rpmConfig = Object.assign({}, userConfig, rpmDefaults);
+  const rpmConfig = linuxConfig({
+    config,
+    pkgArch: arch,
+    dir,
+    outPath,
+  });
 
   await pify(installer)(rpmConfig);
   return [outPath];
