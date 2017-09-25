486c4a8742fda40705161ae5e08730541798897c
Mark Lee <electronjs@lazymalevolence.com>
2017-09-24 23:43:39 -0400

    fix(generic): automatically warn w/a nightly package manager version
    ISSUES CLOSED: #334

--- test/fast/system_spec.js
+++ test/fast/system_spec.js
@@ -1,10 +1,24 @@
 import { expect } from 'chai';
 
-import checkSystem from '../../src/util/check-system';
+import checkSystem, { isNightlyYarnVersion } from '../../src/util/check-system';
 import { fakeOra } from '../../src/util/ora';
 
 describe('check-system', () => {
   it('should succeed on valid agents', async () => {
     expect(await checkSystem(fakeOra())).to.be.equal(true);
+  });
+
+  describe('isNightlyYarnVersion', () => {
+    it('should not match release versions', () => {
+      expect(isNightlyYarnVersion('0.10.0')).to.be.equal(false);
+    });
+
+    it('should not match rc/beta/alpha versions', () => {
+      expect(isNightlyYarnVersion('0.10.0-beta.1')).to.be.equal(false);
+    });
+
+    it('should match nightly versions', () => {
+      expect(isNightlyYarnVersion('0.23.0-20170311.0515')).to.be.equal(true);
+    });
   });
 });
