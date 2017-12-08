0a7fcb7cd73c7d6f0572e7e68af3a23ad1cf11cc
Mark Lee <electronjs@lazymalevolence.com>
2017-12-07 18:38:58 -0800

    chore(tests): remove intermediate layer when running via Docker

--- ci/script.sh
+++ ci/script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-    sudo docker build -f ci/Dockerfile . -t electron-forge-ci
-    sudo docker run -it electron-forge-ci ci/docker.sh $NODE_INSTALLER
+    sudo docker run --interactive --tty --volume $(pwd):/code malept/electron-forge-container:latest /code/ci/docker.sh $NODE_INSTALLER
 else
     npm run test-coverage -- --installer=$NODE_INSTALLER
 fi
