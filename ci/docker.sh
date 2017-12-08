6282a1150cde085e95c92ce78fbdccd4d3645e83
Mark Lee <electronjs@lazymalevolence.com>
2017-12-07 18:38:58 -0800

    chore(tests): remove intermediate layer when running via Docker

--- ci/docker.sh
+++ ci/docker.sh
@@ -3,4 +3,7 @@
 NODE_INSTALLER="$1"
 
 if [[ "$NODE_INSTALLER" = "yarn" ]]; then npm i -g yarn; fi
-npm run test -- --installer=$NODE_INSTALLER
+
+cd /code
+
+CI=true npm run test -- --installer=$NODE_INSTALLER
