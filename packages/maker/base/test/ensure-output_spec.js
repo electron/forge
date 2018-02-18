c17a6bbca7f785ca88c6be96c4ab0ae634d76e3b
Samuel Attard <samuel.r.attard@gmail.com>
2018-02-18 12:56:21 +1100

    Example maker

--- packages/maker/base/test/ensure-output_spec.js
+++ packages/maker/base/test/ensure-output_spec.js
@@ -3,9 +3,10 @@ import fs from 'fs-extra';
 import os from 'os';
 import path from 'path';
 
-import { ensureDirectory, ensureFile } from '../../src/util/ensure-output';
+import MakerBase from '../src/Maker';
 
 describe('ensure-output', () => {
+  const maker = new MakerBase('test');
   const tmpPath = path.resolve(os.tmpdir(), 'forge-ensure');
 
   before(async () => {
@@ -17,13 +18,13 @@ describe('ensure-output', () => {
       await fs.mkdirs(path.resolve(tmpPath, 'foo'));
       fs.writeFileSync(path.resolve(tmpPath, 'foo', 'touchedFile'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'foo', 'touchedFile'))).to.equal(true);
-      await ensureDirectory(path.resolve(tmpPath, 'foo'));
+      await maker.ensureDirectory(path.resolve(tmpPath, 'foo'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'foo', 'touchedFile'))).to.equal(false);
     });
 
     it('should create the directory if it does not exist', async () => {
       expect(await fs.pathExists(path.resolve(tmpPath, 'bar'))).to.equal(false);
-      await ensureDirectory(path.resolve(tmpPath, 'bar'));
+      await maker.ensureDirectory(path.resolve(tmpPath, 'bar'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'bar'))).to.equal(true);
     });
   });
@@ -33,13 +34,13 @@ describe('ensure-output', () => {
       await fs.mkdirs(path.resolve(tmpPath, 'foo'));
       fs.writeFileSync(path.resolve(tmpPath, 'foo', 'touchedFile'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'foo', 'touchedFile'))).to.equal(true);
-      await ensureFile(path.resolve(tmpPath, 'foo'));
+      await maker.ensureFile(path.resolve(tmpPath, 'foo'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'foo', 'touchedFile'))).to.equal(false);
     });
 
     it('should create the containing directory if it does not exist', async () => {
       expect(await fs.pathExists(path.resolve(tmpPath, 'bar'))).to.equal(false);
-      await ensureFile(path.resolve(tmpPath, 'bar', 'file'));
+      await maker.ensureFile(path.resolve(tmpPath, 'bar', 'file'));
       expect(await fs.pathExists(path.resolve(tmpPath, 'bar'))).to.equal(true);
     });
   });
---
+++ packages/maker/dmg/src/MakerDMG.js
@@ -0,0 +1,41 @@
+import MakerBase from '@electron-forge/maker-base';
+
+import fs from 'fs-extra';
+import path from 'path';
+import pify from 'pify';
+
+export default class MakerDMG extends MakerBase {
+  constructor() {
+    super('dmg');
+  }
+
+  async isSupportedOnCurrentPlatform() {
+    return process.platform === 'darwin';
+  }
+
+  async make({
+    dir,
+    makeDir,
+    appName,
+    config,
+    packageJSON,
+  }) {
+    const electronDMG = require('electron-installer-dmg');
+
+    const outPath = path.resolve(makeDir, `${config.name || appName}.dmg`);
+    const wantedOutPath = path.resolve(makeDir, `${appName}-${packageJSON.version}.dmg`);
+    await this.ensureFile(outPath);
+    const dmgConfig = Object.assign({
+      overwrite: true,
+      name: appName,
+    }, config, {
+      appPath: path.resolve(dir, `${appName}.app`),
+      out: path.dirname(outPath),
+    });
+    await pify(electronDMG)(dmgConfig);
+    if (!config.name) {
+      await fs.rename(outPath, wantedOutPath);
+    }
+    return [wantedOutPath];
+  }
+}
