406d78d36efb453efad4cb9e7831ba2ebdfc9725
Samuel Attard <samuel.r.attard@gmail.com>
2018-02-19 17:15:51 +1100

    Run bolt build on CI

--- ci/docker.sh
+++ ci/docker.sh
@@ -4,9 +4,11 @@ NODE_INSTALLER="$1"
 
 npm i -g yarn
 npm i -g bolt
-rm -rf node_modules
-bolt
 
 cd /code
+
+rm -rf node_modules
+bolt
+bolt build
 
 CI=true bolt ws test -- --installer=$NODE_INSTALLER
