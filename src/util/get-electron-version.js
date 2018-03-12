489a25ce8418a4ae0f0f02f2524057fbb6cbc485
Jacob Quant <jacobq@gmail.com>
2018-03-12 17:44:35 -0500

    refactor(generic): incorporate changes requested in PR

--- src/util/get-electron-version.js
+++ src/util/get-electron-version.js
@@ -7,21 +7,20 @@ const d = debug('electron-forge:util');
 export default async (projectDir) => {
   let result = null;
 
-  const modulesToExamine = ['electron-prebuilt-compile', 'electron-compile', 'electron'];
-  for (let i = 0; i < modulesToExamine.length; i += 1) {
-    const moduleName = modulesToExamine[i];
+  const modulesToExamine = ['electron-prebuilt-compile', 'electron', 'electron-prebuilt'];
+  for (const moduleName of modulesToExamine) {
     const moduleDir = path.join(projectDir, 'node_modules', moduleName);
     try {
       const packageJSON = await readPackageJSON(moduleDir);
       result = packageJSON.version;
       break;
     } catch (e) {
-      d(`Could not read package.json for moduleName=${moduleName}`);
+      d(`Could not read package.json for moduleName=${moduleName}`, e);
     }
   }
 
   if (!result) {
-    d(`getElectronVersion failed to determine electron version: projectDir=${projectDir}, result=${result}`);
+    d(`getElectronVersion failed to determine Electron version: projectDir=${projectDir}, result=${result}`);
   }
 
   return result;
