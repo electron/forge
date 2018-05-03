440213688010534177923ddb3a3aced71fd2ae67
Samuel Attard <samuel.r.attard@gmail.com>
2018-05-03 11:10:59 +1000

    docs(plugin-local-electron): add docs for the config options

--- packages/plugin/local-electron/src/Config.ts
+++ packages/plugin/local-electron/src/Config.ts
@@ -1,6 +1,32 @@
 export interface LocalElectronPluginConfig {
-  enabled: boolean;
+  /**
+   * Whether or not the plugin is enabled.
+   *
+   * Can be handy to set this to an environment variable for quick personal
+   * toggling of this plugin.
+   *
+   * Default: `true`
+   */
+  enabled?: boolean;
+  /**
+   * An absolute path to the folder containing your built version of Electron.
+   *
+   * Normally this looks like `/path/to/electron/out/D`
+   */
   electronPath: string;
+  /**
+   * The platform your local build of Electron is for.  You only need to set
+   * this if you have a local build for a platform that isn't your system's
+   * platform.
+   *
+   * Default: process.platform
+   */
   electronPlatform?: string;
+  /**
+   * The arch your local build of Electron is for.  You only need to set this if
+   * you have a local build for an arch that isn't your system's arch.
+   *
+   * Default: process.arch
+   */
   electronArch?: string;
 }
