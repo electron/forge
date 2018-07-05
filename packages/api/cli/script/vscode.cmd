f1b90b6728380048b4f3d5c4a7f5596a0d262e33
Johnny Robeson <johnny@localmomentum.net>
2018-07-05 05:15:29 -0400

    fix(cli): use scoped package path in vscode debugger scripts (#524)
    * fix(cli): use scoped package path in vscode debugger scripts
    
    * fix(cli): use windows style newlines in vscode.cmd

--- packages/api/cli/script/vscode.cmd
+++ packages/api/cli/script/vscode.cmd
@@ -4,6 +4,6 @@ SETLOCAL
 SET FORGE_ARGS=%*
 
 SET FORGE_ARGS=%FORGE_ARGS: =~ ~%
-node "%~dp0/../../electron-forge/dist/electron-forge-start.js" --vscode -- ~%FORGE_ARGS%~
+node "%~dp0/../../@electron-forge/cli/dist/electron-forge-start.js" --vscode -- ~%FORGE_ARGS%~
 
 ENDLOCAL
