12209b00d9182162474923f51c17d43cf7b02b36
Samuel Attard <samuel.r.attard@gmail.com>
2018-09-10 14:32:34 +1000

    chore: use yarn to run commands on CI to make output better

--- ci/script.sh
+++ ci/script.sh
@@ -6,12 +6,12 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
     sudo docker run --privileged --interactive --tty --volume $(pwd):/code malept/electron-forge-container:latest /code/ci/docker.sh $NODE_INSTALLER
 else
     bolt
-    bolt build
-    bolt lint
-    bolt ws test
+    yarn build
+    yarn lint
+    yarn test
     echo "$NODE_INSTALLER-$TRAVIS_SECURE_ENV_VARS-$TRAVIS_BRANCH"
     if [[ "$NODE_INSTALLER-$TRAVIS_SECURE_ENV_VARS-$TRAVIS_BRANCH" = "yarn-true-master" ]]; then
         npm i -g now
-        bolt docs:deploy:ci
+        yarn docs:deploy:ci
     fi
 fi
