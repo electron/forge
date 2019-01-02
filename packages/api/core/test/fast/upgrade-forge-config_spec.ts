648ef333e70da9fc3cfebb47da30ab38e39e8f13
Mark Lee <electronjs@lazymalevolence.com>
2019-01-02 01:15:59 -0800

    feat(core): add basic Forge v5 to v6 importer

---
+++ packages/api/core/test/fast/upgrade-forge-config_spec.ts
@@ -0,0 +1,173 @@
+import _merge from 'lodash.merge';
+import { IForgeResolvableMaker, IForgeResolvablePublisher } from '@electron-forge/shared-types';
+import { expect } from 'chai';
+import path from 'path';
+
+import upgradeForgeConfig, { updateUpgradedForgeDevDeps } from '../../src/util/upgrade-forge-config';
+
+describe('upgradeForgeConfig', () => {
+  it('converts Electron Packager config', () => {
+    const oldConfig = {
+      electronPackagerConfig: {
+        asar: true,
+        packageManager: 'npm',
+      },
+    };
+    const expected = { asar: true };
+
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.packagerConfig).to.deep.equal(expected);
+  });
+
+  it('converts electron-rebuild config', () => {
+    const rebuildConfig = { types: ['prod'] };
+    const oldConfig = { electronRebuildConfig: _merge({}, rebuildConfig) };
+
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.electronRebuildConfig).to.deep.equal(rebuildConfig);
+  });
+
+  it('converts maker config', () => {
+    const oldConfig = {
+      makeTargets: {
+        linux: ['deb'],
+      },
+      electronInstallerDebian: {
+        depends: ['liboath0'],
+      },
+    };
+    const expected = [
+      {
+        name: '@electron-forge/maker-deb',
+        config: {
+          depends: ['liboath0'],
+        },
+        platforms: ['linux'],
+      },
+    ] as IForgeResolvableMaker[];
+
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.makers).to.deep.equal(expected);
+  });
+
+  it('adds the zip maker when specified in makeTargets', () => {
+    const oldConfig = {
+      makeTargets: {
+        darwin: ['zip'],
+        linux: ['zip'],
+      },
+    };
+    const expected = [
+      {
+        name: '@electron-forge/maker-zip',
+        platforms: ['darwin', 'linux'],
+      },
+    ] as IForgeResolvableMaker[];
+
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.makers).to.deep.equal(expected);
+  });
+
+  it('converts publisher config', () => {
+    const oldConfig = {
+      snapStore: {
+        release: 'beta',
+      },
+    };
+    const expected = [
+      {
+        name: '@electron-forge/publisher-snapcraft',
+        config: {
+          release: 'beta',
+        },
+        platforms: null,
+      },
+    ] as IForgeResolvablePublisher[];
+
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.publishers).to.deep.equal(expected);
+  });
+
+  it('converts GitHub publisher config', () => {
+    const octokitOptions = {
+      timeout: 0,
+    };
+    const repo = {
+      name: 'myapp',
+      owner: 'user',
+    };
+    const oldConfig = {
+      github_repository: Object.assign({
+        options: octokitOptions,
+        draft: true,
+      }, repo),
+    };
+    const newConfig = upgradeForgeConfig(oldConfig);
+    expect(newConfig.publishers).to.have.lengthOf(1);
+    const publisherConfig = (newConfig.publishers[0] as IForgeResolvablePublisher).config;
+    expect(publisherConfig.repository).to.deep.equal(repo);
+    expect(publisherConfig.octokitOptions).to.deep.equal(octokitOptions);
+    expect(publisherConfig.draft).to.equal(true);
+  });
+});
+
+describe('updateUpgradedForgeDevDeps', () => {
+  const skeletonPackageJSON = {
+    config: {
+      forge: {
+        makers: [] as IForgeResolvableMaker[],
+        publishers: [] as IForgeResolvablePublisher[],
+      },
+    },
+    devDependencies: {},
+  };
+
+  it('removes unused makers from devDependencies', () => {
+    const packageJSON = _merge({}, skeletonPackageJSON);
+    const devDeps = updateUpgradedForgeDevDeps(packageJSON, ['@electron-forge/maker-squirrel']);
+    expect(devDeps).to.deep.equal([]);
+  });
+
+  it('adds makers to devDependencies', () => {
+    const packageJSON = _merge({}, skeletonPackageJSON);
+    packageJSON.config.forge.makers = [
+      {
+        name: '@electron-forge/maker-zip',
+        platforms: ['darwin', 'linux'],
+      },
+      {
+        name: '@electron-forge/maker-squirrel',
+        config: {},
+        platforms: ['win32'],
+      },
+    ] as IForgeResolvableMaker[];
+
+    const actual = updateUpgradedForgeDevDeps(packageJSON, []);
+    expect(actual).to.have.lengthOf(2);
+    expect(actual.find(dep => dep.startsWith('@electron-forge/maker-zip'))).to.not.equal(undefined);
+    expect(actual.find(dep => dep.startsWith('@electron-forge/maker-squirrel'))).to.not.equal(undefined);
+  });
+
+  it('adds publishers to devDependencies', () => {
+    const packageJSON = _merge({}, skeletonPackageJSON);
+    packageJSON.config.forge.publishers = [
+      { name: '@electron-forge/publisher-github' },
+      { name: '@electron-forge/publisher-snapcraft' },
+    ];
+
+    const actual = updateUpgradedForgeDevDeps(packageJSON, []);
+    expect(actual).to.have.lengthOf(2);
+    expect(actual.find(dep => dep.startsWith('@electron-forge/publisher-github'))).to.not.equal(undefined);
+    expect(actual.find(dep => dep.startsWith('@electron-forge/publisher-snapcraft'))).to.not.equal(undefined);
+  });
+
+  it('adds electron-compile plugin to devDependencies when electron-prebuilt-compile is in devDependencies', () => {
+    const packageJSON = _merge({}, skeletonPackageJSON, {
+      devDependencies: { 'electron-prebuilt-compile': '2.0.0' },
+    });
+
+    const actual = updateUpgradedForgeDevDeps(packageJSON, []);
+    expect(actual, JSON.stringify(actual)).to.have.lengthOf(1);
+    expect(actual[0]).to.match(/^@electron-forge\/plugin-compile/);
+  });
+});
