db89c4efdc4b2963bc60d0914b94620f9961c22c
Mark Lee <malept@users.noreply.github.com>
2019-05-23 13:19:53 -0700

    feat: Node 8 support (#875)
    BREAKING CHANGE: drops support for Node < 8.

--- ci/script.sh
+++ ci/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-    sudo docker run --privileged --interactive --tty --volume $(pwd):/code malept/electron-forge-container:latest /code/ci/docker.sh $NODE_INSTALLER
+    sudo docker run --privileged --interactive --tty --volume $(pwd):/code malept/electron-forge-container:node-8 /code/ci/docker.sh $NODE_INSTALLER
 else
     bolt
     yarn build
