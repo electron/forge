eb039a15284c98cf813a5e4bbfcaed0a6c5b4595
Mark Lee <electronjs@lazymalevolence.com>
2019-06-16 10:39:42 -0700

    Add debug & test

--- packages/api/core/src/util/determine-author.ts
+++ packages/api/core/src/util/determine-author.ts
@@ -1,8 +1,10 @@
 import childProcess from 'child_process';
+import debug from 'debug';
 import { PackagePerson } from '@electron-forge/shared-types';
 import { promisify } from 'util';
 import username from 'username';
 
+const d = debug('electron-forge:determine-author');
 const exec = promisify(childProcess.exec);
 
 const execAndTrimResult = async (command: string) => {
@@ -15,7 +17,8 @@ const getAuthorFromGitConfig = async (): Promise<PackagePerson> => {
     const name = await execAndTrimResult('git config --get user.name');
     const email = await execAndTrimResult('git config --get user.email');
     return { name, email };
-  } catch {
+  } catch (err) {
+    d('Error when getting git config:', err);
     return undefined;
   }
 };
