3cd44eb02fab5a5e16cc65f9f90cb93010e25d57
Mark Lee <electronjs@lazymalevolence.com>
2019-08-26 07:56:02 -0700

    test(core): fix custom template tests

--- packages/api/core/test/fixture/custom_init/index.js
+++ packages/api/core/test/fixture/custom_init/index.js
@@ -1,8 +1,11 @@
+const fs = require('fs-extra');
 const path = require('path');
 
 module.exports = {
   dependencies: ['react'],
   devDependencies: ['react-dom'],
-  templateDirectory: path.resolve(__dirname, './tmpl'),
-  postCopy: (initDir, ora, lintStyle) => {},
+  initializeTemplate: async (directory) => {
+    await fs.copy(path.resolve(__dirname, 'tmpl', '_bar'), path.resolve(directory, '.bar'));
+    await fs.copy(path.resolve(__dirname, 'tmpl', 'src'), path.resolve(directory, 'src'));
+  },
 };
