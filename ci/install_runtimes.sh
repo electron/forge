0700702a9da647fd4181dd2234fc3912071c9ccd
Mark <electronjs@lazymalevolence.com>
2019-11-21 09:29:09 -0800

    Try not running in a container

---
+++ ci/install_runtimes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xe
+
+install_app() {
+    local arch="$1"
+    local app="$2"
+    local branch=$3
+    install_flatpak "app/$app/$arch/$branch"
+}
+
+install_runtime() {
+    local arch="$1"
+    local runtime="$2"
+    local version="$3"
+    install_flatpak "runtime/${runtime}/$arch/$version"
+}
+
+install_flatpak() {
+    local ref="$1"
+    if [[ ! -d "$HOME/.local/share/flatpak/$ref" ]]; then
+        flatpak install --user --no-deps --assumeyes "$ref"
+    fi
+}
+
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+install_runtime x86_64 org.freedesktop.Sdk 1.6
+install_runtime x86_64 org.freedesktop.Platform 1.6
+install_app x86_64 org.electronjs.Electron2.BaseApp stable
