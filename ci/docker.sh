958cdc42f23fad0aaed550c66ac660e34e5f76aa
Mark <electronjs@lazymalevolence.com>
2019-11-20 18:13:31 -0800

    See if the fuse kernel module is loaded

--- ci/docker.sh
+++ ci/docker.sh
@@ -7,5 +7,6 @@ npm install -g bolt
 # For flatpak
 mkdir -p /var/run/dbus
 dbus-daemon --system
+modprobe fuse
 
 DEBUG=electron-installer-snap:snapcraft,electron-installer-flatpak,@malept/flatpak-bundler CI=true bolt coverage -- --installer=$NODE_INSTALLER
