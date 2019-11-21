ba3aacb2ad4f618d3dbfb32248c85aad44e40f4a
Mark <electronjs@lazymalevolence.com>
2019-11-20 18:31:59 -0800

    modprobe fuse

--- ci/docker.sh
+++ ci/docker.sh
@@ -7,6 +7,7 @@ npm install -g bolt
 # For flatpak
 mkdir -p /var/run/dbus
 dbus-daemon --system
-apt-get install -y fuse
+apt-get install -y kmod
+/sbin/modprobe fuse
 
 DEBUG=electron-installer-snap:snapcraft,electron-installer-flatpak,@malept/flatpak-bundler CI=true bolt coverage -- --installer=$NODE_INSTALLER
