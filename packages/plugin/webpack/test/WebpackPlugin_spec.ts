bc5ff5407de9bb3b3ff2fe50efc9419a1b1a3556
Mark Lee <electronjs@lazymalevolence.com>
2019-12-12 13:49:40 -0800

    fix(plugin-webpack): handle package.json files without config keys
    ISSUES CLOSED: #1326

--- packages/plugin/webpack/test/WebpackPlugin_spec.ts
+++ packages/plugin/webpack/test/WebpackPlugin_spec.ts
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import { tmpdir } from 'os';
 
 import WebpackPlugin from '../src/WebpackPlugin';
 
 describe('WebpackPlugin', () => {
+  const webpackTestDir = path.resolve(tmpdir(), 'electron-forge-plugin-webpack-test');
+
   describe('PRELOAD_WEBPACK_ENTRY', () => {
     it('should assign absolute preload script path in development', () => {
       const p = new WebpackPlugin({
@@ -50,6 +55,45 @@ describe('WebpackPlugin', () => {
       (p as any).isProd = true;
       const defines = p.getDefines();
       expect(defines.WINDOW_PRELOAD_WEBPACK_ENTRY).to.be.eq("require('path').resolve(__dirname, '../renderer', 'window', 'preload.js')");
+    });
+  });
+
+  describe('packageAfterCopy', () => {
+    const packageJSONPath = path.join(webpackTestDir, 'package.json');
+    const packagedPath = path.join(webpackTestDir, 'packaged');
+    const packagedPackageJSONPath = path.join(packagedPath, 'package.json');
+    let plugin: WebpackPlugin;
+
+    before(async () => {
+      await fs.ensureDir(packagedPath);
+      plugin = new WebpackPlugin({
+        mainConfig: {},
+        renderer: {
+          config: {},
+          entryPoints: [],
+        },
+      });
+      plugin.setDirectories(webpackTestDir);
+    });
+
+    it('should remove config.forge from package.json', async () => {
+      const packageJSON = { config: { forge: 'config.js' } };
+      await fs.writeJson(packageJSONPath, packageJSON);
+      await plugin.packageAfterCopy(null, packagedPath);
+      expect(await fs.pathExists(packagedPackageJSONPath)).to.equal(true);
+      expect((await fs.readJson(packagedPackageJSONPath)).config).to.not.have.property('forge');
+    });
+
+    it('should succeed if there is no config.forge', async () => {
+      const packageJSON = { name: 'test' };
+      await fs.writeJson(packageJSONPath, packageJSON);
+      await plugin.packageAfterCopy(null, packagedPath);
+      expect(await fs.pathExists(packagedPackageJSONPath)).to.equal(true);
+      expect((await fs.readJson(packagedPackageJSONPath))).to.not.have.property('config');
+    });
+
+    after(async () => {
+      await fs.remove(webpackTestDir);
     });
   });
 });
