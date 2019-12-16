80a00e6f33eeaa1db63576d8bb45a47add6338f7
Mark Lee <electronjs@lazymalevolence.com>
2019-12-15 23:57:31 -0800

    Call the base template in the custom template fixture

--- packages/api/core/test/fixture/custom_init/index.js
+++ packages/api/core/test/fixture/custom_init/index.js
@@ -1,3 +1,4 @@
+const baseTemplate = require('@electron-forge/template-base');
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -5,6 +6,7 @@ module.exports = {
   dependencies: ['debug'],
   devDependencies: ['lodash'],
   initializeTemplate: async (directory) => {
+    await baseTemplate.initializeTemplate(directory);
     await fs.copy(path.resolve(__dirname, 'tmpl', '_bar'), path.resolve(directory, '.bar'));
     await fs.copy(path.resolve(__dirname, 'tmpl', 'src'), path.resolve(directory, 'src'));
   },
