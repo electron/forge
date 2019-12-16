92605753c26f16c693ca61e0f5c0d3d0e9357da2
Mark Lee <electronjs@lazymalevolence.com>
2019-12-16 00:29:34 -0800

    Pass in an empty options object

--- packages/api/core/test/fixture/custom_init/index.js
+++ packages/api/core/test/fixture/custom_init/index.js
@@ -6,7 +6,7 @@ module.exports = {
   dependencies: ['debug'],
   devDependencies: ['lodash'],
   initializeTemplate: async (directory) => {
-    await baseTemplate.initializeTemplate(directory);
+    await baseTemplate.initializeTemplate(directory, {});
     await fs.copy(path.resolve(__dirname, 'tmpl', '_bar'), path.resolve(directory, '.bar'));
     await fs.copy(path.resolve(__dirname, 'tmpl', 'src'), path.resolve(directory, 'src'));
   },
