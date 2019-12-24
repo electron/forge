60428dc9006dd611307096683c10e7eeb994e93b
Liam Dawson <liam@ldaws.com>
2019-12-24 16:23:08 +1100

    Update VS Code debugger script
    Fix the debugger script to fix #1369, while still supporting the fix for #535.

--- packages/api/cli/script/vscode.sh
+++ packages/api/cli/script/vscode.sh
@@ -4,4 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ARGS=$@
 ARGS=${ARGS// /\~ \~}
 
-node $DIR/../../../@electron-forge/cli/dist/electron-forge-start --vscode -- \~$ARGS\~
+if [ -f "$DIR/../../../@electron-forge/cli/dist/electron-forge-start" ]; then
+  node "$DIR/../../../@electron-forge/cli/dist/electron-forge-start" --vscode -- \~$ARGS\~
+else
+  node "$DIR/../@electron-forge/cli/dist/electron-forge-start" --vscode -- \~$ARGS\~
+fi
