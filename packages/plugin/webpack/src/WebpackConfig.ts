bc18e3f38acbae46051ec95154378a85e2194675
Mark Lee <electronjs@lazymalevolence.com>
2020-02-02 16:49:42 -0800

    Fix lint warning

--- packages/plugin/webpack/src/WebpackConfig.ts
+++ packages/plugin/webpack/src/WebpackConfig.ts
@@ -198,13 +198,13 @@ export default class WebpackConfigGenerator {
 
     const defines = this.getDefines(false);
     const plugins = entryPoints.filter((entryPoint) => Boolean(entryPoint.html))
-        .map((entryPoint) => new HtmlWebpackPlugin({
-          title: entryPoint.name,
-          template: entryPoint.html,
-          filename: `${entryPoint.name}/index.html`,
-          chunks: [entryPoint.name].concat(entryPoint.additionalChunks || []),
-        }) as webpack.Plugin).concat([new webpack.DefinePlugin(defines)])
-        .concat(this.isProd ? [] : [new webpack.HotModuleReplacementPlugin()]);
+      .map((entryPoint) => new HtmlWebpackPlugin({
+        title: entryPoint.name,
+        template: entryPoint.html,
+        filename: `${entryPoint.name}/index.html`,
+        chunks: [entryPoint.name].concat(entryPoint.additionalChunks || []),
+      }) as webpack.Plugin).concat([new webpack.DefinePlugin(defines)])
+      .concat(this.isProd ? [] : [new webpack.HotModuleReplacementPlugin()]);
     return webpackMerge.smart({
       entry,
       devtool: 'inline-source-map',
