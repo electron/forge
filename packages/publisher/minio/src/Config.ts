75a5e118b75f3d530106c5cd81bb854be854d394
lyzerk <alm.ozdmr@gmail.com>
2020-02-16 15:40:25 +0300

    initial work for minio client

---
+++ packages/publisher/minio/src/Config.ts
@@ -0,0 +1,52 @@
+// eslint-disable-next-line import/prefer-default-export
+export interface PublisherMinioConfig {
+  /**
+   * Endpoint of your server. e.g. localhost
+   */
+  endPoint: string,
+  /**
+   * Port of your server. e.g. 9000
+   */
+  port: number,
+  /**
+   * Flag of SSL
+   * 
+   * Falls back to 'false'
+   */
+  useSSL?: boolean,
+  /**
+   * Your Minio Access Key ID
+   *
+   * Falls back to the MINIO_ACCESS_KEY environment variable if not provided
+   */
+  accessKeyId?: string;
+  /**
+   * The secret for your Minio Access Key
+   *
+   * Falls back to the MINIO_SECRET_KEY environment variable if not
+   * provided
+   */
+  secretAccessKey?: string;
+  /**
+   * The name of the MinIO/S3 bucket to upload artifacts to
+   */
+  bucket: string;
+  /**
+   * The key prefix to upload artifacts to.
+   *
+   * E.g. `my/prefix`
+   *
+   * Default: appVersion
+   */
+  folder?: string;
+  /**
+   * Whether or not to make uploaded artifacts public to the internet
+   *
+   * Default: false
+   */
+  public?: boolean;
+  /**
+   * Custom function to provide the key to upload a given file to
+   */
+  keyResolver?: (fileName: string, platform: string, arch: string) => string;
+}
---
+++ packages/publisher/minio/src/PublisherMinio.ts
@@ -0,0 +1,70 @@
+import PublisherBase, { PublisherOptions } from '@electron-forge/publisher-base';
+import { asyncOra } from '@electron-forge/async-ora';
+
+import debug from 'debug';
+import fs from 'fs';
+import path from 'path';
+import { Client } from 'minio';
+
+import { PublisherMinioConfig } from './Config';
+
+const d = debug('electron-forge:publish:minio');
+
+type MinioArtifact = {
+  path: string;
+  keyPrefix: string;
+  platform: string;
+  arch: string;
+};
+
+export default class PublisherMinio extends PublisherBase<PublisherMinioConfig> {
+  name = 'minio';
+
+  async publish({
+    makeResults,
+  }: PublisherOptions) {
+    const { config } = this;
+    const artifacts: MinioArtifact[] = [];
+
+    for (const makeResult of makeResults) {
+      artifacts.push(...makeResult.artifacts.map((artifact) => ({
+        path: artifact,
+        keyPrefix: config.folder || makeResult.packageJSON.version,
+        platform: makeResult.platform,
+        arch: makeResult.arch,
+      })));
+      d('artifact:', makeResult);
+    }
+
+    const configuration = {
+      endPoint: config.endPoint,
+      port: config.port,
+      useSSL: config.useSSL,
+      accessKey: (config.accessKeyId || process.env.MINIO_ACCESS_KEY) || '',
+      secretKey: (config.secretAccessKey || process.env.MINIO_SECRET_KEY) || ''
+    };
+
+
+    if (!config.endPoint || !config.port || !configuration.accessKey || !configuration.secretKey || !config.bucket) {
+      throw new Error('In order to publish to minio you must set the "minio.accessKey", "minio.secretKey", "minio.endPoint", "minio.port" and "bucket"');
+    }
+    var minioClient = new Client(configuration);
+
+    d('creating minio client with options:', config);
+
+    let uploaded = 0;
+    const spinnerText = () => `Uploading Artifacts ${uploaded}/${artifacts.length}`;
+
+    await asyncOra(spinnerText(), async (uploadSpinner) => {
+      await Promise.all(artifacts.map(async (artifact) => {
+        // const uploader = minioClient.fPutObject(config.bucket, artifact.path, artifact.path);
+        d('uploading:', artifact.path);
+        d('uploading:', artifact);
+        
+        // await uploader;
+        uploaded += 1;
+        uploadSpinner.text = spinnerText();
+      }));
+    });
+  }
+}
