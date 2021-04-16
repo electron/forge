db20ad84cc8a1a3e4e75e944f2b841e1f99c25a4
Yvan Guidoin <yvan@ajenta.net>
2021-04-16 19:01:39 +0100

    test(plugin-webpack): write test for once method

---
+++ packages/plugin/webpack/test/ElectronForgeLogging_spec.ts
@@ -0,0 +1,9 @@
+// import { expect } from 'chai';
+
+// const mockProjectDir = process.platform === 'win32' ? 'C:\\path' : '/path';
+
+describe('ElectronForgeLogging', () => {
+  describe('Triggers on stats', () => {
+    // TODO: Find how to get a trigger from tab
+  });
+});
