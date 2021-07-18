499373bc4b243375d94c2c14b056fdae810401d3
Mark Lee <electronjs@lazymalevolence.com>
2021-07-17 19:53:32 -0700

    refactor: convert bump script to TypeScript

--- tools/bump.ts
+++ tools/bump.ts
@@ -1,31 +1,30 @@
-#!/usr/bin/env node
+#!node_modules/.bin/ts-node
 
-require('colors');
-const fs = require('fs-extra');
-const path = require('path');
-const { spawn } = require('@malept/cross-spawn-promise')
-const semver = require('semver');
+import 'colors';
+import * as fs from 'fs-extra';
+import path from 'path';
+import { spawn } from '@malept/cross-spawn-promise';
+import * as semver from 'semver';
 
 const BASE_DIR = path.resolve(__dirname, '..');
 const PACKAGES_DIR = path.resolve(BASE_DIR, 'packages');
 const ELECTRON_FORGE_PREFIX = '@electron-forge/';
 
-
-async function run(command, args) {
+async function run(command: string, args: string[]): Promise<string> {
   return spawn(command, args, { cwd: BASE_DIR });
 }
 
-async function git(*args) {
-  return run('git', args)
+async function git(...args: string[]): Promise<string> {
+  return run('git', args);
 }
 
-async function checkCleanWorkingDir() {
+async function checkCleanWorkingDir(): Promise<void> {
   if ((await git('status', '--short')) !== '') {
     throw 'Your working directory is not clean, please ensure you have a clean working directory before version bumping'.red;
   }
 }
 
-async function updateChangelog(lastVersion, version) {
+async function updateChangelog(lastVersion: string, version: string): Promise<void> {
   await run('node_modules/.bin/changelog', [`--tag=v${lastVersion}..v${version}`]);
 
   require('../ci/fix-changelog'); // eslint-disable-line global-require
@@ -34,7 +33,7 @@ async function updateChangelog(lastVersion, version) {
   await git('commit', '-m', `Update CHANGELOG.md for ${version}`);
 }
 
-(async () => {
+async function main(): Promise<void> {
   await checkCleanWorkingDir();
 
   const version = process.argv[2];
@@ -80,4 +79,6 @@ async function updateChangelog(lastVersion, version) {
 
   // re-tag to include the changelog
   await git(`git tag --force v${version}`);
-})().catch(console.error);
+}
+
+main().catch(console.error);
