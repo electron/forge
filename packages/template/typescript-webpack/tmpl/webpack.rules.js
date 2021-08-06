9f2ef5a735be8abb2a44352601b03aecfe50379c
Mark Lee <electronjs@lazymalevolence.com>
2021-08-06 10:01:21 -0700

    fix(template-webpack): only use node-loader if the .node files are in native_modules/

--- packages/template/typescript-webpack/tmpl/webpack.rules.js
+++ packages/template/typescript-webpack/tmpl/webpack.rules.js
@@ -1,7 +1,9 @@
 module.exports = [
   // Add support for native node modules
   {
-    test: /\.node$/,
+    // We're specifying native_modules in the test because the asset relocator loader generates a
+    // "fake" .node file which is really a cjs file.
+    test: /native_modules\/.+\.node$/,
     use: 'node-loader',
   },
   {
