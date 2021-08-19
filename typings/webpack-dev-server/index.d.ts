522ccdf8649091c00a6d991bed34b4e9cb613ce7
Mark Lee <electronjs@lazymalevolence.com>
2021-08-19 08:33:34 -0700

    Fix typo

--- typings/webpack-dev-server/index.d.ts
+++ typings/webpack-dev-server/index.d.ts
@@ -3,7 +3,7 @@ declare module 'webpack-dev-server' {
   import { Server } from 'http';
   import { Compiler } from 'webpack';
   class WebpackDevServer {
-    constructor(options: {}, compilter: Compiler)
+    constructor(options: {}, compiler: Compiler)
     server: Server;
     start(): Promise<void>
     close(): void
