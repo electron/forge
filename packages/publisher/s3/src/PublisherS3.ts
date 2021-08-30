34b3a62234c477d266dbbb5989b67664aede5b38
Mark Lee <malept@users.noreply.github.com>
2021-08-29 23:19:47 -0700

    refactor(publisher-s3): use AWS-SDK v3 (#2487)
    * refactor(publisher-s3): remove unnecessary defaults
    * fix(publisher-s3): don't check for credentials
    
    Fixes #2226.
    Closes #2258.

--- packages/publisher/s3/src/PublisherS3.ts
+++ packages/publisher/s3/src/PublisherS3.ts
@@ -1,10 +1,12 @@
-import PublisherBase, { PublisherOptions } from '@electron-forge/publisher-base';
-import { asyncOra } from '@electron-forge/async-ora';
-
+import { Credentials } from '@aws-sdk/types';
 import debug from 'debug';
 import fs from 'fs';
 import path from 'path';
-import S3 from 'aws-sdk/clients/s3';
+import { S3Client } from '@aws-sdk/client-s3';
+import { Progress, Upload } from '@aws-sdk/lib-storage';
+
+import PublisherBase, { PublisherOptions } from '@electron-forge/publisher-base';
+import { asyncOra } from '@electron-forge/async-ora';
 
 import { PublisherS3Config } from './Config';
 
@@ -23,51 +25,54 @@ export default class PublisherS3 extends PublisherBase<PublisherS3Config> {
   async publish({
     makeResults,
   }: PublisherOptions) {
-    const { config } = this;
     const artifacts: S3Artifact[] = [];
+
+    if (!this.config.bucket) {
+      throw new Error('In order to publish to S3, you must set the "bucket" property in your Forge publisher config. See the docs for more info');
+    }
 
     for (const makeResult of makeResults) {
       artifacts.push(...makeResult.artifacts.map((artifact) => ({
         path: artifact,
-        keyPrefix: config.folder || makeResult.packageJSON.version,
+        keyPrefix: this.config.folder || makeResult.packageJSON.version,
         platform: makeResult.platform,
         arch: makeResult.arch,
       })));
     }
 
-    const s3Client = new S3({
-      accessKeyId: config.accessKeyId || process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: config.secretAccessKey || process.env.AWS_SECRET_ACCESS_KEY,
-      region: config.region || undefined,
-      endpoint: config.endpoint || undefined,
-      s3ForcePathStyle: !!config.s3ForcePathStyle,
+    const s3Client = new S3Client({
+      credentials: this.generateCredentials(),
+      region: this.config.region,
+      endpoint: this.config.endpoint,
+      forcePathStyle: !!this.config.s3ForcePathStyle,
     });
 
-    if (!s3Client.config.credentials || !config.bucket) {
-      throw new Error('In order to publish to s3 you must set the "s3.accessKeyId", "process.env.ELECTRON_FORGE_S3_SECRET_ACCESS_KEY" and "s3.bucket" properties in your Forge config. See the docs for more info');
-    }
-
-    d('creating s3 client with options:', config);
+    d('creating s3 client with options:', this.config);
 
     let uploaded = 0;
     const spinnerText = () => `Uploading Artifacts ${uploaded}/${artifacts.length}`;
 
     await asyncOra(spinnerText(), async (uploadSpinner) => {
       await Promise.all(artifacts.map(async (artifact) => {
-        const uploader = s3Client.upload({
-          Body: fs.createReadStream(artifact.path),
-          Bucket: config.bucket,
-          Key: this.keyForArtifact(artifact),
-          ACL: config.public ? 'public-read' : 'private',
-        } as S3.PutObjectRequest);
         d('uploading:', artifact.path);
-
-        uploader.on('httpUploadProgress', (progress) => {
-          const p = `${Math.round((progress.loaded / progress.total) * 100)}%`;
-          d(`Upload Progress (${path.basename(artifact.path)}) ${p}`);
+        const uploader = new Upload({
+          client: s3Client,
+          params: {
+            Body: fs.createReadStream(artifact.path),
+            Bucket: this.config.bucket,
+            Key: this.keyForArtifact(artifact),
+            ACL: this.config.public ? 'public-read' : 'private',
+          },
         });
 
-        await uploader.promise();
+        uploader.on('httpUploadProgress', (progress: Progress) => {
+          if (progress.total) {
+            const percentage = `${Math.round(((progress.loaded || 0) / progress.total) * 100)}%`;
+            d(`Upload Progress (${path.basename(artifact.path)}) ${percentage}`);
+          }
+        });
+
+        await uploader.done();
         uploaded += 1;
         uploadSpinner.text = spinnerText();
       }));
@@ -84,5 +89,16 @@ export default class PublisherS3 extends PublisherBase<PublisherS3Config> {
     }
 
     return `${artifact.keyPrefix}/${path.basename(artifact.path)}`;
+  }
+
+  generateCredentials(): Credentials | undefined {
+    const accessKeyId = this.config.accessKeyId || process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = this.config.secretAccessKey || process.env.AWS_SECRET_ACCESS_KEY;
+
+    if (accessKeyId && secretAccessKey) {
+      return { accessKeyId, secretAccessKey };
+    }
+
+    return undefined;
   }
 }
