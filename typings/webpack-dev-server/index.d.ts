bc0ed95ee83b39e576120bddf1e119d3e378f54c
Mark Lee <electronjs@lazymalevolence.com>
2021-09-10 15:08:38 -0700

    chore: update reason for custom webpack-dev-server types

--- typings/webpack-dev-server/index.d.ts
+++ typings/webpack-dev-server/index.d.ts
@@ -1,4 +1,5 @@
-// TODO: Replace with @types/webpack-dev-server when it is updated to v4
+// TODO: Replace with @types/webpack-dev-server when it no longer depends on @types/webpack
+// See: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55300#issuecomment-904636931
 declare module 'webpack-dev-server' {
   import { Server } from 'http';
   import { Compiler } from 'webpack';
