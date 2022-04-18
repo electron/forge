433c364fdfb7448dd116bbcfaff3caf8af3468f9
Calvin Watford <cwatford@slack-corp.com>
2022-04-18 10:09:51 -0600

    fix testing

--- .mocharc.js
+++ .mocharc.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   extension: ['ts'],
-  loader: 'ts-node/esm',
-  require: [path.join(__dirname, 'tools', 'test-setup.ts')],
+  require: ['ts-node/register', path.join(__dirname, 'tools', 'test-setup.ts')],
   timeout: 800000,
   recursive: true,
 };
