412327a25295ce3e718cb90d951aac3e73766837
Calvin <clavin@users.noreply.github.com>
2022-05-03 11:29:31 -0600

    refactor: typescript projects & docs generation (#2804)

--- tools/copy-now.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs-extra';
-import path from 'path';
-
-const BASE_DIR = path.resolve(__dirname, '..');
-const DOCS_DIR = path.resolve(BASE_DIR, 'docs');
-
-(async () => {
-  await fs.copy(path.resolve(BASE_DIR, '.now.json'), path.resolve(DOCS_DIR, 'now.json'));
-})();
