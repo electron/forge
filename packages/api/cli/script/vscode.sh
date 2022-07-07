9d7d4a03f72029f7e9213017b543ad152597bf96
wesyao <wesyao@dropbox.com>
2022-07-07 16:47:51 -0700

    fix vscode debugging shell file

--- packages/api/cli/script/vscode.sh
+++ packages/api/cli/script/vscode.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ARGS=$@
 ARGS=${ARGS// /\~ \~}
 
-node $DIR/../../../@electron-forge/cli/dist/electron-forge-start --vscode -- \~$ARGS\~
+node $DIR/../@electron-forge/cli/dist/electron-forge-start --vscode -- \~$ARGS\~
