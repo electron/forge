aad9c7e49de17712189c249d91f958e2da8ae00b
Calvin <clavin@users.noreply.github.com>
2022-07-11 09:06:14 -0600

    fix: keep stdin unpaused after ora completes (#2904)

--- packages/api/core/src/api/start.ts
+++ packages/api/core/src/api/start.ts
@@ -27,6 +27,11 @@ export default async ({
   inspectBrk = false,
 }: StartOptions): Promise<ElectronProcess> => {
   asyncOra.interactive = interactive;
+  // Since the `start` command is meant to be long-living (i.e. run forever,
+  // until interrupted) we should enable this to keep stdin flowing after ora
+  // completes. For more context:
+  // https://github.com/electron-userland/electron-forge/issues/2319
+  asyncOra.keepStdinFlowing = true;
 
   await asyncOra('Locating Application', async () => {
     const resolvedDir = await resolveDir(dir);
@@ -131,9 +136,13 @@ export default async ({
         process.stdin.resume();
       }
       spawned.on('exit', () => {
-        if (spawned.restarted) return;
+        if (spawned.restarted) {
+          return;
+        }
 
-        if (!process.stdin.isPaused()) process.stdin.pause();
+        if (interactive && !process.stdin.isPaused()) {
+          process.stdin.pause();
+        }
       });
     } else if (interactive && !process.stdin.isPaused()) {
       process.stdin.pause();
@@ -153,6 +162,7 @@ export default async ({
         lastSpawned.emit('restarted', await forgeSpawnWrapper());
       }
     });
+    process.stdin.resume();
   }
 
   return forgeSpawnWrapper();
