6599cb3e8ce091d1128f48987f5072a1c6a8762a
VerteDinde <vertedinde@electronjs.org>
2022-10-18 21:18:07 -0700

    fix: generate custom docs sidebar
    Co-authored-by: George Xu <georgexu99@users.noreply.github.com>

--- tools/gen-docs.ts
+++ tools/gen-docs.ts
@@ -17,7 +17,7 @@ import * as typedoc from 'typedoc';
     hideGenerator: true,
     includeVersion: true,
     name: 'Electron Forge',
-    plugin: ['typedoc-plugin-rename-defaults', 'typedoc-plugin-missing-exports'],
+    plugin: ['typedoc-plugin-rename-defaults', 'typedoc-plugin-missing-exports', './tools/doc-plugin/dist/index.js'],
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore (overloaded param `internalModule` lets us rename "<internal>"
     // in the generated docs to "InternalOptions")
@@ -28,6 +28,31 @@ import * as typedoc from 'typedoc';
   if (projReflection === undefined) {
     throw new Error('Failed to find package sources');
   }
+  const makers = new typedoc.ReflectionGroup('Makers', 2);
+  const plugins = new typedoc.ReflectionGroup('Plugins', 2);
+  const publishers = new typedoc.ReflectionGroup('Publishers', 2);
+  const templates = new typedoc.ReflectionGroup('Templates', 2);
+  const utils = new typedoc.ReflectionGroup('Utils', 2);
+
+  for (let child of projReflection.groups![0].children) {
+    if (child.name.includes('maker')) {
+      makers.children.push(child);
+    }
+    else if (child.name.includes('plugin')) {
+      plugins.children.push(child);
+    }
+    else if (child.name.includes('publisher')) {
+      publishers.children.push(child);
+    }
+    else if (child.name.includes('template')) {
+      templates.children.push(child);
+    }
+    else {
+      utils.children.push(child);
+    }
+  }
+  
+  projReflection.groups = [makers, plugins, publishers, templates, utils];
 
   await typedocApp.generateDocs(projReflection, path.resolve(__dirname, '..', 'docs'));
 })().catch(console.error);
