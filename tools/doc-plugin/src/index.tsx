36dcdd057993082190fdea9e7a6db53614057428
Keeley Hammond <khammond@slack-corp.com>
2022-10-24 15:52:45 -0700

    docs: generate custom docs sidebar (#2974)
    * fix: generate custom docs sidebar
    
    Co-authored-by: George Xu <georgexu99@users.noreply.github.com>
    
    * chore: prettier formatting
    
    * fix: clean up, compile plug-in on npm script
    
    * docs: fix undefined classes, remove h3
    
    Co-authored-by: George Xu <georgexu99@users.noreply.github.com>

---
+++ tools/doc-plugin/src/index.tsx
@@ -0,0 +1,120 @@
+import {
+  Application,
+  DeclarationReflection,
+  DefaultThemeRenderContext,
+  DefaultTheme,
+  Options,
+  JSX,
+  PageEvent,
+  Reflection,
+  ReflectionKind,
+  ReflectionGroup,
+} from 'typedoc';
+
+export class NavigationOverrideThemeContext extends DefaultThemeRenderContext {
+  constructor(theme: DefaultTheme, options: Options) {
+    super(theme, options);
+
+    this.navigation = (props) => {
+      return <>{overridePrimaryNavigation(this, props)}</>;
+    };
+  }
+}
+
+export class NavigationOverrideTheme extends DefaultTheme {
+  private _contextCache?: NavigationOverrideThemeContext;
+
+  override getRenderContext(): NavigationOverrideThemeContext {
+    this._contextCache ||= new NavigationOverrideThemeContext(
+      this,
+      this.application.options
+    );
+    return this._contextCache;
+  }
+}
+
+// Replicated from the TypeDoc codebase. This should be exported by TypeDoc.
+function classNames(
+  names: Record<string, boolean | null | undefined>,
+  extraCss?: string
+) {
+  const css = Object.keys(names)
+    .filter((key) => names[key])
+    .concat(extraCss || '')
+    .join(' ')
+    .trim()
+    .replace(/\s+/g, ' ');
+  return css.length ? css : undefined;
+}
+
+function overridePrimaryNavigation(
+  context: DefaultThemeRenderContext,
+  props: PageEvent<Reflection>
+) {
+  const modules = props.model.project.getChildrenByKind(
+    ReflectionKind.SomeModule
+  );
+  const projectLinkName = modules.some((m) => m.kindOf(ReflectionKind.Module))
+    ? 'All Modules'
+    : 'Exports';
+
+  const groups = props.model.project.groups;
+
+  if (groups && groups.length !== 0) {
+    return (
+      <nav class="tsd-navigation primary">
+        <ul>
+          <li>
+            <h2>
+              <a href={context.urlTo(props.model.project)}>{projectLinkName}</a>
+            </h2>
+          </li>
+          {groups.map(generateChildren)}
+        </ul>
+      </nav>
+    );
+  }
+
+  return (
+    <nav class="tsd-navigation primary">
+      <ul>
+        <li>
+          <a href={context.urlTo(props.model.project)}>{projectLinkName}</a>
+        </li>
+      </ul>
+    </nav>
+  );
+
+  function generateChildren(group: ReflectionGroup) {
+    let childNav: JSX.Element | undefined;
+    if (group.children) {
+      const title = group.title;
+      if (group.children?.length) {
+        childNav = <ul>{group.children.map(link)}</ul>;
+      }
+
+      return (
+        <li class={title}>
+          <a href={context.urlTo(props.model.project)}>{title}</a>
+          {childNav}
+        </li>
+      );
+    }
+  }
+
+  function link(mod: DeclarationReflection) {
+    return (
+      <li class={mod.name}>
+        <a href={context.urlTo(mod)}>{renderedName(mod)}</a>
+      </li>
+    );
+  }
+
+  function renderedName(mod: DeclarationReflection) {
+    return mod.name.split('/')[1];
+  }
+}
+
+export function load(app: Application) {
+  app.renderer.defineTheme('forge-theme', NavigationOverrideTheme);
+}
--- tools/gen-docs.ts
+++ tools/gen-docs.ts
@@ -2,6 +2,28 @@ import * as path from 'path';
 import { getPackageInfo } from './utils';
 import * as typedoc from 'typedoc';
 
+function generatedSidebarGroups(projReflection: typedoc.ProjectReflection) {
+  const maker = new typedoc.ReflectionGroup('Makers', 2);
+  const plugin = new typedoc.ReflectionGroup('Plugins', 2);
+  const publisher = new typedoc.ReflectionGroup('Publishers', 2);
+  const template = new typedoc.ReflectionGroup('Templates', 2);
+  const util = new typedoc.ReflectionGroup('Utils & Internal Helpers', 2);
+
+  const keys = ['maker', 'plugin', 'publisher', 'template', 'util'];
+  const groups = [maker, plugin, publisher, template, util];
+
+  for (const child of projReflection.groups![0].children) {
+    const key = keys.find((k) => child.name.includes(k));
+    if (key && !child.name.includes('maker-base')) {
+      const group = groups.find((t) => t.title.toLowerCase().includes(key))!;
+      group.children.push(child);
+    } else {
+      util.children.push(child);
+    }
+  }
+  return groups;
+}
+
 (async () => {
   const packages = await getPackageInfo();
 
@@ -17,7 +39,8 @@ import * as typedoc from 'typedoc';
     hideGenerator: true,
     includeVersion: true,
     name: 'Electron Forge',
-    plugin: ['typedoc-plugin-rename-defaults', 'typedoc-plugin-missing-exports'],
+    plugin: ['typedoc-plugin-rename-defaults', 'typedoc-plugin-missing-exports', './tools/doc-plugin/dist/index.js'],
+    theme: 'forge-theme',
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore (overloaded param `internalModule` lets us rename "<internal>"
     // in the generated docs to "InternalOptions")
@@ -28,6 +51,7 @@ import * as typedoc from 'typedoc';
   if (projReflection === undefined) {
     throw new Error('Failed to find package sources');
   }
+  projReflection.groups = generatedSidebarGroups(projReflection);
 
   await typedocApp.generateDocs(projReflection, path.resolve(__dirname, '..', 'docs'));
 })().catch(console.error);
