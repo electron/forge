macdja38/electron-forge

feat: add support for forge.config.ts et. al
This brings in rechoir and interpret which allow arbitrary extensions / loaders for our forge config.

Notably this allows us to make forge.config.ts a thing (and thus generate type safe configurations).

packages/api/core/src/util/plugin-interface.ts
@@ -1,5 +1,5 @@
 import PluginBase from '@electron-forge/plugin-base';
-import { IForgePluginInterface, ForgeConfig, IForgePlugin, StartResult } from '@electron-forge/shared-types';
+import { IForgePluginInterface, ResolvedForgeConfig, IForgePlugin, StartResult } from '@electron-forge/shared-types';
 import debug from 'debug';
 
 import { StartOptions } from '../api';
@@ -14,9 +14,9 @@ function isForgePlugin(plugin: IForgePlugin | unknown): plugin is IForgePlugin {
 export default class PluginInterface implements IForgePluginInterface {
   private plugins: IForgePlugin[];
 
-  private config: ForgeConfig;
+  private config: ResolvedForgeConfig;
 
-  constructor(dir: string, forgeConfig: ForgeConfig) {
+  constructor(dir: string, forgeConfig: ResolvedForgeConfig) {
     this.plugins = forgeConfig.plugins.map((plugin) => {
       if (isForgePlugin(plugin)) {
         return plugin;

packages/maker/base/src/Maker.ts
@@ -1,4 +1,4 @@
-import { ForgeArch, ForgeConfig, ForgePlatform, IForgeMaker } from '@electron-forge/shared-types';
+import { ForgeArch, ResolvedForgeConfig, ForgePlatform, IForgeMaker } from '@electron-forge/shared-types';
 import fs from 'fs-extra';
 import path from 'path';
 import which from 'which';
@@ -30,7 +30,7 @@ export interface MakerOptions {
   /**
    * Fully resolved forge configuration, you shouldn't really need this
    */
-  forgeConfig: ForgeConfig;
+  forgeConfig: ResolvedForgeConfig;
   /**
    * The application's package.json file
    */

packages/plugin/auto-unpack-natives/src/AutoUnpackNativesPlugin.ts
@@ -1,4 +1,4 @@
-import { ForgeConfig, ForgeHookFn } from '@electron-forge/shared-types';
+import { ResolvedForgeConfig, ForgeHookFn } from '@electron-forge/shared-types';
 import PluginBase from '@electron-forge/plugin-base';
 
 import { AutoUnpackNativesConfig } from './Config';
@@ -13,7 +13,7 @@ export default class AutoUnpackNativesPlugin extends PluginBase<AutoUnpackNative
     return null;
   }
 
-  resolveForgeConfig = async (forgeConfig: ForgeConfig): Promise<ForgeConfig> => {
+  resolveForgeConfig = async (forgeConfig: ResolvedForgeConfig): Promise<ResolvedForgeConfig> => {
     if (!forgeConfig.packagerConfig) {
       forgeConfig.packagerConfig = {};
     }

packages/plugin/webpack/src/WebpackPlugin.ts
@@ -1,6 +1,6 @@
 import { asyncOra } from '@electron-forge/async-ora';
 import PluginBase from '@electron-forge/plugin-base';
-import { ElectronProcess, ForgeArch, ForgeConfig, ForgeHookFn, ForgePlatform } from '@electron-forge/shared-types';
+import { ElectronProcess, ForgeArch, ResolvedForgeConfig, ForgeHookFn, ForgePlatform } from '@electron-forge/shared-types';
 import Logger, { Tab } from '@electron-forge/web-multi-logger';
 
 import chalk from 'chalk';
@@ -155,7 +155,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
     switch (name) {
       case 'prePackage':
         this.isProd = true;
-        return async (config: ForgeConfig, platform: ForgePlatform, arch: ForgeArch) => {
+        return async (config: ResolvedForgeConfig, platform: ForgePlatform, arch: ForgeArch) => {
           await fs.remove(this.baseDir);
           await utils.rebuildHook(
             this.projectDir,
@@ -168,7 +168,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
           await this.compileRenderers();
         };
       case 'postStart':
-        return async (_config: ForgeConfig, child: ElectronProcess) => {
+        return async (_config: ResolvedForgeConfig, child: ElectronProcess) => {
           if (!this.loggedOutputUrl) {
             console.info(`\n\nWebpack Output Available: ${chalk.cyan(`http://localhost:${this.loggerPort}`)}\n`);
             this.loggedOutputUrl = true;
@@ -188,7 +188,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
     }
   }
 
-  resolveForgeConfig = async (forgeConfig: ForgeConfig): Promise<ForgeConfig> => {
+  resolveForgeConfig = async (forgeConfig: ResolvedForgeConfig): Promise<ResolvedForgeConfig> => {
     if (!forgeConfig.packagerConfig) {
       forgeConfig.packagerConfig = {};
     }
@@ -222,7 +222,7 @@ Your packaged app may be larger than expected if you dont ignore everything othe
     return forgeConfig;
   };
 
-  packageAfterCopy = async (_forgeConfig: ForgeConfig, buildPath: string): Promise<void> => {
+  packageAfterCopy = async (_forgeConfig: ResolvedForgeConfig, buildPath: string): Promise<void> => {
     const pj = await fs.readJson(path.resolve(this.projectDir, 'package.json'));
 
     if (!pj.main?.endsWith('.webpack/main')) {

packages/publisher/base/src/Publisher.ts
@@ -1,4 +1,4 @@
-import { ForgePlatform, ForgeConfig, ForgeMakeResult, IForgePublisher } from '@electron-forge/shared-types';
+import { ForgePlatform, ResolvedForgeConfig, ForgeMakeResult, IForgePublisher } from '@electron-forge/shared-types';
 
 export interface PublisherOptions {
   /**
@@ -14,7 +14,7 @@ export interface PublisherOptions {
    *
    * You probably shouldn't use this
    */
-  forgeConfig: ForgeConfig;
+  forgeConfig: ResolvedForgeConfig;
 }
 
 export default abstract class Publisher<C> implements IForgePublisher {

packages/publisher/electron-release-server/test/PublisherERS_spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { ForgeConfig, ForgeMakeResult } from '@electron-forge/shared-types';
+import { ResolvedForgeConfig, ForgeMakeResult } from '@electron-forge/shared-types';
 import fetchMock from 'fetch-mock';
 import proxyquire from 'proxyquire';
 import { stub } from 'sinon';
@@ -55,7 +55,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -102,7 +102,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -139,7 +139,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
       expect(calls).to.have.length(2);
@@ -178,7 +178,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -198,7 +198,7 @@ describe('PublisherERS', () => {
   it('fails if username and password are not provided', () => {
     const publisher = new PublisherERS({});
 
-    expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ForgeConfig })).to.eventually.be.rejectedWith(
+    expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ResolvedForgeConfig })).to.eventually.be.rejectedWith(
       'In order to publish to ERS you must set the "electronReleaseServer.baseUrl", "electronReleaseServer.username" and "electronReleaseServer.password" properties in your Forge config. See the docs for more info'
     );
   });
@@ -211,7 +211,7 @@ describe('PublisherERS', () => {
       username: 'test',
       password: 'test',
     });
-    return expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ForgeConfig })).to.eventually.be.rejectedWith(
+    return expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ResolvedForgeConfig })).to.eventually.be.rejectedWith(
       'ERS publish failed with status code: 400 (http://example.com/api/auth/login)'
     );
   });

electron/forge

packages/api/core/src/util/plugin-interface.ts
@@ -1,5 +1,5 @@
 import PluginBase from '@electron-forge/plugin-base';
-import { ForgeConfig, IForgePlugin, IForgePluginInterface, StartResult } from '@electron-forge/shared-types';
+import { IForgePlugin, IForgePluginInterface, ResolvedForgeConfig, StartResult } from '@electron-forge/shared-types';
 import debug from 'debug';
 
 import { StartOptions } from '../api';
@@ -15,9 +15,9 @@ function isForgePlugin(plugin: IForgePlugin | unknown): plugin is IForgePlugin {
 export default class PluginInterface implements IForgePluginInterface {
   private plugins: IForgePlugin[];
 
-  private config: ForgeConfig;
+  private config: ResolvedForgeConfig;
 
-  constructor(dir: string, forgeConfig: ForgeConfig) {
+  constructor(dir: string, forgeConfig: ResolvedForgeConfig) {
     this.plugins = forgeConfig.plugins.map((plugin) => {
       if (isForgePlugin(plugin)) {
         return plugin;

packages/maker/base/src/Maker.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 
-import { ForgeArch, ForgeConfig, ForgePlatform, IForgeMaker } from '@electron-forge/shared-types';
+import { ForgeArch, ForgePlatform, IForgeMaker, ResolvedForgeConfig } from '@electron-forge/shared-types';
 import fs from 'fs-extra';
 import which from 'which';
 
@@ -31,7 +31,7 @@ export interface MakerOptions {
   /**
    * Fully resolved forge configuration, you shouldn't really need this
    */
-  forgeConfig: ForgeConfig;
+  forgeConfig: ResolvedForgeConfig;
   /**
    * The application's package.json file
    */

packages/plugin/auto-unpack-natives/src/AutoUnpackNativesPlugin.ts
@@ -1,5 +1,5 @@
 import { PluginBase } from '@electron-forge/plugin-base';
-import { ForgeConfig, ForgeHookFn } from '@electron-forge/shared-types';
+import { ForgeHookFn, ResolvedForgeConfig } from '@electron-forge/shared-types';
 
 import { AutoUnpackNativesConfig } from './Config';
 
@@ -13,7 +13,7 @@ export default class AutoUnpackNativesPlugin extends PluginBase<AutoUnpackNative
     return null;
   }
 
-  resolveForgeConfig = async (forgeConfig: ForgeConfig): Promise<ForgeConfig> => {
+  resolveForgeConfig = async (forgeConfig: ResolvedForgeConfig): Promise<ResolvedForgeConfig> => {
     if (!forgeConfig.packagerConfig) {
       forgeConfig.packagerConfig = {};
     }

packages/plugin/webpack/src/WebpackPlugin.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { asyncOra } from '@electron-forge/async-ora';
 import { utils } from '@electron-forge/core';
 import { PluginBase } from '@electron-forge/plugin-base';
-import { ElectronProcess, ForgeArch, ForgeConfig, ForgeHookFn, ForgePlatform } from '@electron-forge/shared-types';
+import { ElectronProcess, ForgeArch, ForgeHookFn, ForgePlatform, ResolvedForgeConfig } from '@electron-forge/shared-types';
 import Logger, { Tab } from '@electron-forge/web-multi-logger';
 import chalk from 'chalk';
 import debug from 'debug';
@@ -155,7 +155,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
     switch (name) {
       case 'prePackage':
         this.isProd = true;
-        return async (config: ForgeConfig, platform: ForgePlatform, arch: ForgeArch) => {
+        return async (config: ResolvedForgeConfig, platform: ForgePlatform, arch: ForgeArch) => {
           await fs.remove(this.baseDir);
           await utils.rebuildHook(
             this.projectDir,
@@ -168,7 +168,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
           await this.compileRenderers();
         };
       case 'postStart':
-        return async (_config: ForgeConfig, child: ElectronProcess) => {
+        return async (_config: ResolvedForgeConfig, child: ElectronProcess) => {
           if (!this.loggedOutputUrl) {
             console.info(`\n\nWebpack Output Available: ${chalk.cyan(`http://localhost:${this.loggerPort}`)}\n`);
             this.loggedOutputUrl = true;
@@ -188,7 +188,7 @@ export default class WebpackPlugin extends PluginBase<WebpackPluginConfig> {
     }
   }
 
-  resolveForgeConfig = async (forgeConfig: ForgeConfig): Promise<ForgeConfig> => {
+  resolveForgeConfig = async (forgeConfig: ResolvedForgeConfig): Promise<ResolvedForgeConfig> => {
     if (!forgeConfig.packagerConfig) {
       forgeConfig.packagerConfig = {};
     }
@@ -222,7 +222,7 @@ Your packaged app may be larger than expected if you dont ignore everything othe
     return forgeConfig;
   };
 
-  packageAfterCopy = async (_forgeConfig: ForgeConfig, buildPath: string): Promise<void> => {
+  packageAfterCopy = async (_forgeConfig: ResolvedForgeConfig, buildPath: string): Promise<void> => {
     const pj = await fs.readJson(path.resolve(this.projectDir, 'package.json'));
 
     if (!pj.main?.endsWith('.webpack/main')) {

packages/publisher/base/src/Publisher.ts
@@ -1,4 +1,4 @@
-import { ForgeConfig, ForgeMakeResult, ForgePlatform, IForgePublisher } from '@electron-forge/shared-types';
+import { ForgeMakeResult, ForgePlatform, IForgePublisher, ResolvedForgeConfig } from '@electron-forge/shared-types';
 
 export interface PublisherOptions {
   /**
@@ -14,7 +14,7 @@ export interface PublisherOptions {
    *
    * You probably shouldn't use this
    */
-  forgeConfig: ForgeConfig;
+  forgeConfig: ResolvedForgeConfig;
 }
 
 export default abstract class Publisher<C> implements IForgePublisher {

packages/publisher/electron-release-server/test/PublisherERS_spec.ts
@@ -1,4 +1,4 @@
-import { ForgeConfig, ForgeMakeResult } from '@electron-forge/shared-types';
+import { ForgeMakeResult, ResolvedForgeConfig } from '@electron-forge/shared-types';
 import { expect } from 'chai';
 import fetchMock from 'fetch-mock';
 import proxyquire from 'proxyquire';
@@ -55,7 +55,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -102,7 +102,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -139,7 +139,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
       expect(calls).to.have.length(2);
@@ -178,7 +178,7 @@ describe('PublisherERS', () => {
         },
       ];
 
-      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ForgeConfig });
+      await publisher.publish({ makeResults, dir: '', forgeConfig: {} as ResolvedForgeConfig });
 
       const calls = fetch.calls();
 
@@ -198,7 +198,7 @@ describe('PublisherERS', () => {
   it('fails if username and password are not provided', () => {
     const publisher = new PublisherERS({});
 
-    expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ForgeConfig })).to.eventually.be.rejectedWith(
+    expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ResolvedForgeConfig })).to.eventually.be.rejectedWith(
       'In order to publish to ERS you must set the "electronReleaseServer.baseUrl", "electronReleaseServer.username" and "electronReleaseServer.password" properties in your Forge config. See the docs for more info'
     );
   });
@@ -211,7 +211,7 @@ describe('PublisherERS', () => {
       username: 'test',
       password: 'test',
     });
-    return expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ForgeConfig })).to.eventually.be.rejectedWith(
+    return expect(publisher.publish({ makeResults: [], dir: '', forgeConfig: {} as ResolvedForgeConfig })).to.eventually.be.rejectedWith(
       'ERS publish failed with status code: 400 (http://example.com/api/auth/login)'
     );
   });