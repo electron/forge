macdja38/electron-forge

refactor: better internal type safety for hooks

packages/api/core/src/util/hook.ts
@@ -1,32 +1,41 @@
-import { ForgeConfig } from '@electron-forge/shared-types';
+import { ForgeConfig, ForgeSimpleHookSignatures, ForgeMutatingHookSignatures, ForgeSimpleHookFn, ForgeMutatingHookFn } from '@electron-forge/shared-types';
 import debug from 'debug';
 
 const d = debug('electron-forge:hook');
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const runHook = async (forgeConfig: ForgeConfig, hookName: string, ...hookArgs: any[]): Promise<void> => {
+export const runHook = async <Hook extends keyof ForgeSimpleHookSignatures>(
+  forgeConfig: ForgeConfig,
+  hookName: Hook,
+  ...hookArgs: ForgeSimpleHookSignatures[Hook]
+): Promise<void> => {
   const { hooks } = forgeConfig;
   if (hooks) {
     d(`hook triggered: ${hookName}`);
     if (typeof hooks[hookName] === 'function') {
       d('calling hook:', hookName, 'with args:', hookArgs);
-      await hooks[hookName](forgeConfig, ...hookArgs);
+      await (hooks[hookName] as ForgeSimpleHookFn<Hook>)(forgeConfig, ...hookArgs);
     }
   }
   await forgeConfig.pluginInterface.triggerHook(hookName, hookArgs);
 };
 
-export async function runMutatingHook<T>(forgeConfig: ForgeConfig, hookName: string, item: T): Promise<T> {
+export async function runMutatingHook<Hook extends keyof ForgeMutatingHookSignatures>(
+  forgeConfig: ForgeConfig,
+  hookName: Hook,
+  ...item: ForgeMutatingHookSignatures[Hook]
+): Promise<ForgeMutatingHookSignatures[Hook][0]> {
   const { hooks } = forgeConfig;
   if (hooks) {
     d(`hook triggered: ${hookName}`);
     if (typeof hooks[hookName] === 'function') {
-      d('calling mutating hook:', hookName, 'with item:', item);
-      const result = await hooks[hookName](forgeConfig, item);
+      d('calling mutating hook:', hookName, 'with item:', item[0]);
+      const hook = hooks[hookName] as ForgeMutatingHookFn<Hook>;
+      const result = await hook(forgeConfig, ...item);
       if (typeof result !== 'undefined') {
-        item = result;
+        item[0] = result;
       }
     }
   }
-  return forgeConfig.pluginInterface.triggerMutatingHook(hookName, item);
+  return forgeConfig.pluginInterface.triggerMutatingHook(hookName, item[0]);
 }

packages/plugin/compile/src/CompilePlugin.ts
@@ -1,4 +1,4 @@
-import { ForgeHookFn } from '@electron-forge/shared-types';
+import { ForgeConfig, ForgeHookMap } from '@electron-forge/shared-types';
 import PluginBase, { StartOptions } from '@electron-forge/plugin-base';
 import * as path from 'path';
 
@@ -14,19 +14,19 @@ export default class LocalElectronPlugin extends PluginBase<CompilePluginConfig>
     super(c);
 
     this.init = this.init.bind(this);
-    this.getHook = this.getHook.bind(this);
+    this.getHooks = this.getHooks.bind(this);
     this.startLogic = this.startLogic.bind(this);
   }
 
-  init(dir: string): void {
+  init(dir: string, config: ForgeConfig): void {
+    super.init(dir, config);
     this.dir = dir;
   }
 
-  getHook(hookName: string): ForgeHookFn | null {
-    if (hookName === 'packageAfterCopy') {
-      return createCompileHook(this.dir);
-    }
-    return null;
+  getHooks(): ForgeHookMap {
+    return {
+      packageAfterCopy: createCompileHook(this.dir),
+    };
   }
 
   async startLogic(_opts: StartOptions): Promise<string[]> {

packages/plugin/local-electron/test/LocalElectronPlugin_spec.ts
@@ -51,11 +51,8 @@ describe('LocalElectronPlugin', () => {
 
     beforeEach(() => {
       p = new LocalElectronPlugin({ electronPath: 'test/foo' });
-    });
-
-    it('should return for all other hooks', () => {
-      expect(p.getHook('prePackage')).to.equal(null);
-      expect(p.getHook('postMake')).to.equal(null);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      p.init('', {} as any);
     });
 
     describe('with afterExtract hook', () => {
@@ -69,15 +66,15 @@ describe('LocalElectronPlugin', () => {
       afterEach(() => fs.remove(tmpDir));
 
       it('should return a function for packageAfterExtract', () => {
-        expect(p.getHook('packageAfterExtract')).to.be.a('function');
+        expect(p.getHooks().packageAfterExtract).to.be.a('function');
       });
 
       it('should do nothing when disabled', async () => {
         p.config.enabled = false;
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await fn({} as ForgeConfig, tmpDir, null, process.platform, process.arch);
+        await fn({} as ForgeConfig, tmpDir, 'null', process.platform, process.arch);
 
         expect(await fs.pathExists(tmpDir)).to.equal(true);
         expect(await fs.pathExists(path.resolve(tmpDir, 'touch'))).to.equal(true);
@@ -85,18 +82,18 @@ describe('LocalElectronPlugin', () => {
 
       it("should throw an error if the platform doesn't match", async () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await expect(fn({} as ForgeConfig, tmpDir, null, 'bad', process.arch)).to.eventually.be.rejectedWith(
+        await expect(fn({} as ForgeConfig, tmpDir, 'null', 'bad', process.arch)).to.eventually.be.rejectedWith(
           `Can not use local Electron version, required platform "bad" but local platform is "${process.platform}"`
         );
       });
 
       it("should throw an error if the arch doesn't match", async () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await expect(fn({} as ForgeConfig, tmpDir, null, process.platform, 'bad')).to.eventually.be.rejectedWith(
+        await expect(fn({} as ForgeConfig, tmpDir, 'null', process.platform, 'bad')).to.eventually.be.rejectedWith(
           `Can not use local Electron version, required arch "bad" but local arch is "${process.arch}"`
         );
       });
@@ -106,9 +103,9 @@ describe('LocalElectronPlugin', () => {
         await fs.writeFile(path.resolve(electronDir, 'electron'), 'hi i am electron I swear');
         p.config.electronPath = electronDir;
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await fn({} as ForgeConfig, tmpDir, null, process.platform, process.arch);
+        await fn({} as ForgeConfig, tmpDir, 'null', process.platform, process.arch);
 
         expect(await fs.pathExists(path.resolve(tmpDir, 'touch'))).to.equal(false);
         expect(await fs.pathExists(path.resolve(tmpDir, 'electron'))).to.equal(true);

packages/utils/types/src/index.ts
@@ -1,20 +1,65 @@
 import { ChildProcess } from 'child_process';
 import { ArchOption, Options as ElectronPackagerOptions, TargetPlatform } from 'electron-packager';
 import { RebuildOptions } from 'electron-rebuild';
+import { OraImpl } from '@electron-forge/async-ora';
 
 export type ElectronProcess = ChildProcess & { restarted: boolean };
 
 export type ForgePlatform = TargetPlatform;
 export type ForgeArch = ArchOption;
-// Why: hooks have any number/kind of args/return values
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type ForgeHookFn = (forgeConfig: ForgeConfig, ...args: any[]) => Promise<any>;
 export type ForgeConfigPublisher = IForgeResolvablePublisher | IForgePublisher;
 export type ForgeConfigMaker = IForgeResolvableMaker | IForgeMaker;
 export type ForgeConfigPlugin = IForgeResolvablePlugin | IForgePlugin;
+
+export interface ForgeSimpleHookSignatures {
+  generateAssets: [platform: ForgePlatform, version: ForgeArch];
+  postStart: [appProcess: ElectronProcess];
+  prePackage: [platform: ForgePlatform, version: ForgeArch];
+  packageAfterCopy: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  packageAfterPrune: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  packageAfterExtract: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  postPackage: [
+    packageResult: {
+      platform: ForgePlatform;
+      arch: ForgeArch;
+      outputPaths: string[];
+      spinner?: OraImpl;
+    }
+  ];
+  preMake: [];
+}
+
+export interface ForgeMutatingHookSignatures {
+  postMake: [makeResults: ForgeMakeResult[]];
+  resolveForgeConfig: [currentConfig: ForgeConfig];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readPackageJson: [packageJson: Record<string, any>];
+}
+
+export type ForgeHookName = keyof (ForgeSimpleHookSignatures & ForgeMutatingHookSignatures);
+export type ForgeSimpleHookFn<Hook extends keyof ForgeSimpleHookSignatures> = (
+  forgeConfig: ForgeConfig,
+  ...args: ForgeSimpleHookSignatures[Hook]
+) => Promise<void>;
+export type ForgeMutatingHookFn<Hook extends keyof ForgeMutatingHookSignatures> = (
+  forgeConfig: ForgeConfig,
+  ...args: ForgeMutatingHookSignatures[Hook]
+) => Promise<ForgeMutatingHookSignatures[Hook][0] | undefined>;
+export type ForgeHookFn<Hook extends ForgeHookName> = Hook extends keyof ForgeSimpleHookSignatures
+  ? ForgeSimpleHookFn<Hook>
+  : Hook extends keyof ForgeMutatingHookSignatures
+  ? ForgeMutatingHookFn<Hook>
+  : never;
+export type ForgeHookMap = {
+  [S in ForgeHookName]?: ForgeHookFn<S>;
+};
+
 export interface IForgePluginInterface {
-  triggerHook(hookName: string, hookArgs: any[]): Promise<void>;
-  triggerMutatingHook<T>(hookName: string, item: T): Promise<any>;
+  triggerHook<Hook extends keyof ForgeSimpleHookSignatures>(hookName: Hook, hookArgs: ForgeSimpleHookSignatures[Hook]): Promise<void>;
+  triggerMutatingHook<Hook extends keyof ForgeMutatingHookSignatures>(
+    hookName: Hook,
+    item: ForgeMutatingHookSignatures[Hook][0]
+  ): Promise<ForgeMutatingHookSignatures[Hook][0]>;
   overrideStartLogic(opts: StartOptions): Promise<StartResult>;
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
@@ -29,9 +74,7 @@ export interface ForgeConfig {
    * If a function is provided, it must synchronously return the buildIdentifier
    */
   buildIdentifier?: string | (() => string);
-  hooks?: {
-    [hookName: string]: ForgeHookFn;
-  };
+  hooks?: ForgeHookMap;
   /**
    * @internal
    */
@@ -75,7 +118,7 @@ export interface IForgePlugin {
   name: string;
 
   init(dir: string, forgeConfig: ForgeConfig): void;
-  getHook?(hookName: string): ForgeHookFn | null;
+  getHooks?(): ForgeHookMap;
   startLogic?(opts: StartOptions): Promise<StartResult>;
 }
 

electron/forge

packages/api/core/src/util/hook.ts
@@ -1,32 +1,47 @@
-import { ResolvedForgeConfig } from '@electron-forge/shared-types';
+import {
+  ForgeMutatingHookFn,
+  ForgeMutatingHookSignatures,
+  ForgeSimpleHookFn,
+  ForgeSimpleHookSignatures,
+  ResolvedForgeConfig,
+} from '@electron-forge/shared-types';
 import debug from 'debug';
 
 const d = debug('electron-forge:hook');
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const runHook = async (forgeConfig: ResolvedForgeConfig, hookName: string, ...hookArgs: any[]): Promise<void> => {
+export const runHook = async <Hook extends keyof ForgeSimpleHookSignatures>(
+  forgeConfig: ResolvedForgeConfig,
+  hookName: Hook,
+  ...hookArgs: ForgeSimpleHookSignatures[Hook]
+): Promise<void> => {
   const { hooks } = forgeConfig;
   if (hooks) {
     d(`hook triggered: ${hookName}`);
     if (typeof hooks[hookName] === 'function') {
       d('calling hook:', hookName, 'with args:', hookArgs);
-      await hooks[hookName](forgeConfig, ...hookArgs);
+      await (hooks[hookName] as ForgeSimpleHookFn<Hook>)(forgeConfig, ...hookArgs);
     }
   }
   await forgeConfig.pluginInterface.triggerHook(hookName, hookArgs);
 };
 
-export async function runMutatingHook<T>(forgeConfig: ResolvedForgeConfig, hookName: string, item: T): Promise<T> {
+export async function runMutatingHook<Hook extends keyof ForgeMutatingHookSignatures>(
+  forgeConfig: ResolvedForgeConfig,
+  hookName: Hook,
+  ...item: ForgeMutatingHookSignatures[Hook]
+): Promise<ForgeMutatingHookSignatures[Hook][0]> {
   const { hooks } = forgeConfig;
   if (hooks) {
     d(`hook triggered: ${hookName}`);
     if (typeof hooks[hookName] === 'function') {
-      d('calling mutating hook:', hookName, 'with item:', item);
-      const result = await hooks[hookName](forgeConfig, item);
+      d('calling mutating hook:', hookName, 'with item:', item[0]);
+      const hook = hooks[hookName] as ForgeMutatingHookFn<Hook>;
+      const result = await hook(forgeConfig, ...item);
       if (typeof result !== 'undefined') {
-        item = result;
+        item[0] = result;
       }
     }
   }
-  return forgeConfig.pluginInterface.triggerMutatingHook(hookName, item);
+  return forgeConfig.pluginInterface.triggerMutatingHook(hookName, item[0]);
 }

packages/plugin/compile/src/CompilePlugin.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 
 import { PluginBase, StartOptions } from '@electron-forge/plugin-base';
-import { ForgeHookFn } from '@electron-forge/shared-types';
+import { ForgeHookMap, ResolvedForgeConfig } from '@electron-forge/shared-types';
 
 import { CompilePluginConfig } from './Config';
 import { createCompileHook } from './lib/compile-hook';
@@ -15,19 +15,19 @@ export default class CompileElectronPlugin extends PluginBase<CompilePluginConfi
     super(c);
 
     this.init = this.init.bind(this);
-    this.getHook = this.getHook.bind(this);
+    this.getHooks = this.getHooks.bind(this);
     this.startLogic = this.startLogic.bind(this);
   }
 
-  init(dir: string): void {
+  init(dir: string, config: ResolvedForgeConfig): void {
+    super.init(dir, config);
     this.dir = dir;
   }
 
-  getHook(hookName: string): ForgeHookFn | null {
-    if (hookName === 'packageAfterCopy') {
-      return createCompileHook(this.dir);
-    }
-    return null;
+  getHooks(): ForgeHookMap {
+    return {
+      packageAfterCopy: createCompileHook(this.dir),
+    };
   }
 
   async startLogic(_opts: StartOptions): Promise<string[]> {

packages/plugin/local-electron/test/LocalElectronPlugin_spec.ts
@@ -52,11 +52,8 @@ describe('LocalElectronPlugin', () => {
 
     beforeEach(() => {
       p = new LocalElectronPlugin({ electronPath: 'test/foo' });
-    });
-
-    it('should return for all other hooks', () => {
-      expect(p.getHook('prePackage')).to.equal(null);
-      expect(p.getHook('postMake')).to.equal(null);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      p.init('', {} as any);
     });
 
     describe('with afterExtract hook', () => {
@@ -70,15 +67,15 @@ describe('LocalElectronPlugin', () => {
       afterEach(() => fs.remove(tmpDir));
 
       it('should return a function for packageAfterExtract', () => {
-        expect(p.getHook('packageAfterExtract')).to.be.a('function');
+        expect(p.getHooks().packageAfterExtract).to.be.a('function');
       });
 
       it('should do nothing when disabled', async () => {
         p.config.enabled = false;
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await fn({} as ResolvedForgeConfig, tmpDir, null, process.platform, process.arch);
+        await fn({} as ResolvedForgeConfig, tmpDir, 'null', process.platform, process.arch);
 
         expect(await fs.pathExists(tmpDir)).to.equal(true);
         expect(await fs.pathExists(path.resolve(tmpDir, 'touch'))).to.equal(true);
@@ -86,18 +83,18 @@ describe('LocalElectronPlugin', () => {
 
       it("should throw an error if the platform doesn't match", async () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await expect(fn({} as ResolvedForgeConfig, tmpDir, null, 'bad', process.arch)).to.eventually.be.rejectedWith(
+        await expect(fn({} as ResolvedForgeConfig, tmpDir, 'null', 'bad', process.arch)).to.eventually.be.rejectedWith(
           `Can not use local Electron version, required platform "bad" but local platform is "${process.platform}"`
         );
       });
 
       it("should throw an error if the arch doesn't match", async () => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await expect(fn({} as ResolvedForgeConfig, tmpDir, null, process.platform, 'bad')).to.eventually.be.rejectedWith(
+        await expect(fn({} as ResolvedForgeConfig, tmpDir, 'null', process.platform, 'bad')).to.eventually.be.rejectedWith(
           `Can not use local Electron version, required arch "bad" but local arch is "${process.arch}"`
         );
       });
@@ -107,9 +104,9 @@ describe('LocalElectronPlugin', () => {
         await fs.writeFile(path.resolve(electronDir, 'electron'), 'hi i am electron I swear');
         p.config.electronPath = electronDir;
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const fn = p.getHook('packageAfterExtract')!;
+        const fn = p.getHooks().packageAfterExtract!;
 
-        await fn({} as ResolvedForgeConfig, tmpDir, null, process.platform, process.arch);
+        await fn({} as ResolvedForgeConfig, tmpDir, 'null', process.platform, process.arch);
 
         expect(await fs.pathExists(path.resolve(tmpDir, 'touch'))).to.equal(false);
         expect(await fs.pathExists(path.resolve(tmpDir, 'electron'))).to.equal(true);

packages/utils/types/src/index.ts
@@ -1,5 +1,6 @@
 import { ChildProcess } from 'child_process';
 
+import { OraImpl } from '@electron-forge/async-ora';
 import { ArchOption, Options as ElectronPackagerOptions, TargetPlatform } from 'electron-packager';
 import { RebuildOptions } from 'electron-rebuild';
 
@@ -7,15 +8,59 @@ export type ElectronProcess = ChildProcess & { restarted: boolean };
 
 export type ForgePlatform = TargetPlatform;
 export type ForgeArch = ArchOption;
-// Why: hooks have any number/kind of args/return values
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type ForgeHookFn = (forgeConfig: ResolvedForgeConfig, ...args: any[]) => Promise<any>;
 export type ForgeConfigPublisher = IForgeResolvablePublisher | IForgePublisher;
 export type ForgeConfigMaker = IForgeResolvableMaker | IForgeMaker;
 export type ForgeConfigPlugin = IForgeResolvablePlugin | IForgePlugin;
+
+export interface ForgeSimpleHookSignatures {
+  generateAssets: [platform: ForgePlatform, version: ForgeArch];
+  postStart: [appProcess: ElectronProcess];
+  prePackage: [platform: ForgePlatform, version: ForgeArch];
+  packageAfterCopy: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  packageAfterPrune: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  packageAfterExtract: [buildPath: string, electronVersion: string, platform: ForgePlatform, arch: ForgeArch];
+  postPackage: [
+    packageResult: {
+      platform: ForgePlatform;
+      arch: ForgeArch;
+      outputPaths: string[];
+      spinner?: OraImpl;
+    }
+  ];
+  preMake: [];
+}
+
+export interface ForgeMutatingHookSignatures {
+  postMake: [makeResults: ForgeMakeResult[]];
+  resolveForgeConfig: [currentConfig: ResolvedForgeConfig];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  readPackageJson: [packageJson: Record<string, any>];
+}
+
+export type ForgeHookName = keyof (ForgeSimpleHookSignatures & ForgeMutatingHookSignatures);
+export type ForgeSimpleHookFn<Hook extends keyof ForgeSimpleHookSignatures> = (
+  forgeConfig: ResolvedForgeConfig,
+  ...args: ForgeSimpleHookSignatures[Hook]
+) => Promise<void>;
+export type ForgeMutatingHookFn<Hook extends keyof ForgeMutatingHookSignatures> = (
+  forgeConfig: ResolvedForgeConfig,
+  ...args: ForgeMutatingHookSignatures[Hook]
+) => Promise<ForgeMutatingHookSignatures[Hook][0] | undefined>;
+export type ForgeHookFn<Hook extends ForgeHookName> = Hook extends keyof ForgeSimpleHookSignatures
+  ? ForgeSimpleHookFn<Hook>
+  : Hook extends keyof ForgeMutatingHookSignatures
+  ? ForgeMutatingHookFn<Hook>
+  : never;
+export type ForgeHookMap = {
+  [S in ForgeHookName]?: ForgeHookFn<S>;
+};
+
 export interface IForgePluginInterface {
-  triggerHook(hookName: string, hookArgs: any[]): Promise<void>;
-  triggerMutatingHook<T>(hookName: string, item: T): Promise<any>;
+  triggerHook<Hook extends keyof ForgeSimpleHookSignatures>(hookName: Hook, hookArgs: ForgeSimpleHookSignatures[Hook]): Promise<void>;
+  triggerMutatingHook<Hook extends keyof ForgeMutatingHookSignatures>(
+    hookName: Hook,
+    item: ForgeMutatingHookSignatures[Hook][0]
+  ): Promise<ForgeMutatingHookSignatures[Hook][0]>;
   overrideStartLogic(opts: StartOptions): Promise<StartResult>;
 }
 /* eslint-enable @typescript-eslint/no-explicit-any */
@@ -30,9 +75,7 @@ export interface ResolvedForgeConfig {
    * If a function is provided, it must synchronously return the buildIdentifier
    */
   buildIdentifier?: string | (() => string);
-  hooks?: {
-    [hookName: string]: ForgeHookFn;
-  };
+  hooks?: ForgeHookMap;
   /**
    * @internal
    */
@@ -77,7 +120,7 @@ export interface IForgePlugin {
   name: string;
 
   init(dir: string, forgeConfig: ResolvedForgeConfig): void;
-  getHook?(hookName: string): ForgeHookFn | null;
+  getHooks?(): ForgeHookMap;
   startLogic?(opts: StartOptions): Promise<StartResult>;
 }
 