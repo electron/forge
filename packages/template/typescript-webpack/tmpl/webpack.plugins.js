aa81df23de7ff4a7b8a76291e43848144e677811
Samuel Attard <samuel.r.attard@gmail.com>
2022-10-30 16:12:14 -0700

    chore: force ts-checker plugin to pipe to webpack-infra logging channel

--- packages/template/typescript-webpack/tmpl/webpack.plugins.js
+++ packages/template/typescript-webpack/tmpl/webpack.plugins.js
@@ -1,3 +1,7 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+module.exports = [
+  new ForkTsCheckerWebpackPlugin({
+    logger: 'webpack-infrastructure',
+  }),
+];
