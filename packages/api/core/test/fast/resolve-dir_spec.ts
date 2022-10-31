5d52bbbfa78c4be203dfdd2739ea3b03d914b9f8
Keeley Hammond <khammond@slack-corp.com>
2022-10-31 11:49:44 -0700

    test: add test for detect forge.config

--- packages/api/core/test/fast/resolve-dir_spec.ts
+++ packages/api/core/test/fast/resolve-dir_spec.ts
@@ -9,6 +9,13 @@ describe('resolve-dir', () => {
     expect(await resolveDir('/foo/var/fake')).to.be.equal(null);
   });
 
+  it('should return a directory if a forge config is found, but no package.json.forge.config', async () => {
+    expect(await resolveDir(path.resolve(__dirname, '../fixture/forge-config-no-package-json-config/'))).to.not.be.equal(null);
+    expect(await resolveDir(path.resolve(__dirname, '../fixture/forge-config-no-package-json-config/'))).to.be.equal(
+      path.resolve(__dirname, '../fixture/forge-config-no-package-json-config')
+    );
+  });
+
   it('should return a directory if it finds a node module', async () => {
     expect(await resolveDir(path.resolve(__dirname, '../fixture/dummy_app/foo'))).to.not.be.equal(null);
     expect(await resolveDir(path.resolve(__dirname, '../fixture/dummy_app/foo'))).to.be.equal(path.resolve(__dirname, '../fixture/dummy_app'));
