e1d5db00d4b9510d54e4982dccf484d1b08cfb68
Samuel Attard <sam@electronjs.org>
2022-10-31 17:09:17 -0700

    fix: ensure webpack typescript template runs (#3038)

--- packages/template/webpack-typescript/tmpl/webpack.plugins.ts
+++ packages/template/webpack-typescript/tmpl/webpack.plugins.ts
@@ -3,4 +3,8 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-export const plugins = [new ForkTsCheckerWebpackPlugin()];
+export const plugins = [
+  new ForkTsCheckerWebpackPlugin({
+    logger: 'webpack-infrastructure',
+  }),
+];
