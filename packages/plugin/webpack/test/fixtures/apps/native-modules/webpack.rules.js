25474cb72eb3c8b2f6998ca7f018f05292236d16
Erick Zhao <erick@hotmail.ca>
2022-11-09 14:49:15 -0800

    construct loader test with path.sep

--- packages/plugin/webpack/test/fixtures/apps/native-modules/webpack.rules.js
+++ packages/plugin/webpack/test/fixtures/apps/native-modules/webpack.rules.js
@@ -1,6 +1,18 @@
+const path = require('path');
+
+// platform-aware path separator for loader test regex
+const sep = '\\' + path.sep;
+
 module.exports = [
+  // Add support for native node modules
   {
-    test: /\/node_modules\/.*\.(m?js|node)$/,
+    // We're specifying native_modules in the test because the asset relocator loader generates a
+    // "fake" .node file which is really a cjs file.
+    test: new RegExp(`native_modules${sep}.+\\.node$`),
+    use: 'node-loader',
+  },
+  {
+    test: new RegExp(`${sep}node_modules${sep}.+\\.(m?js|node)$`),
     parser: { amd: false },
     use: {
       loader: '@vercel/webpack-asset-relocator-loader',
--- packages/template/webpack-typescript/tmpl/webpack.rules.ts
+++ packages/template/webpack-typescript/tmpl/webpack.rules.ts
@@ -1,15 +1,19 @@
 import type { ModuleOptions } from 'webpack';
+import path from 'path';
+
+// platform-aware path separator for loader test regex
+const sep = '\\' + path.sep;
 
 export const rules: Required<ModuleOptions>['rules'] = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
-    test: /native_modules\/.+\.node$/,
+    test: new RegExp(`native_modules${sep}.+\\.node$`),
     use: 'node-loader',
   },
   {
-    test: /\/node_modules\/.*\.(m?js|node)$/,
+    test: new RegExp(`${sep}node_modules${sep}.+\\.(m?js|node)$`),
     parser: { amd: false },
     use: {
       loader: '@vercel/webpack-asset-relocator-loader',
