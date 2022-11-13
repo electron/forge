c78d18074aa3e80861c35696be7dce4b631149e8
Samuel Attard <samuel.r.attard@gmail.com>
2022-11-12 22:57:24 -0800

    refactor: use inline regex

--- packages/template/webpack-typescript/tmpl/webpack.rules.ts
+++ packages/template/webpack-typescript/tmpl/webpack.rules.ts
@@ -1,24 +1,20 @@
-import type { ModuleOptions } from 'webpack';
-import path from 'path';
+import type { ModuleOptions } from "webpack";
 
-// platform-aware path separator for loader test regex
-const sep = '\\' + path.sep;
-
-export const rules: Required<ModuleOptions>['rules'] = [
+export const rules: Required<ModuleOptions>["rules"] = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
-    test: new RegExp(`native_modules${sep}.+\\.node$`),
-    use: 'node-loader',
+    test: /native_modules[/\\].+\.node$/,
+    use: "node-loader",
   },
   {
-    test: new RegExp(`${sep}node_modules${sep}.+\\.(m?js|node)$`),
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
@@ -26,7 +22,7 @@ export const rules: Required<ModuleOptions>['rules'] = [
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
-      loader: 'ts-loader',
+      loader: "ts-loader",
       options: {
         transpileOnly: true,
       },
