f86eb28d40413bf73834d0100239ab64f20306af
Samuel Attard <sam@electronjs.org>
2022-11-14 11:40:45 -0800

    fix(template-webpack): only run asset relocator loader on `node_modules` folder (#3060)
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

--- packages/template/webpack-typescript/tmpl/webpack.rules.ts
+++ packages/template/webpack-typescript/tmpl/webpack.rules.ts
@@ -5,11 +5,11 @@ export const rules: Required<ModuleOptions>['rules'] = [
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
-    test: /native_modules\/.+\.node$/,
+    test: /native_modules[/\\].+\.node$/,
     use: 'node-loader',
   },
   {
-    test: /\.(m?js|node)$/,
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
       loader: '@vercel/webpack-asset-relocator-loader',
--- packages/template/webpack/tmpl/webpack.rules.js
+++ packages/template/webpack/tmpl/webpack.rules.js
@@ -3,11 +3,11 @@ module.exports = [
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
-    test: /native_modules\/.+\.node$/,
+    test: /native_modules[/\\].+\.node$/,
     use: 'node-loader',
   },
   {
-    test: /\.(m?js|node)$/,
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
       loader: '@vercel/webpack-asset-relocator-loader',
