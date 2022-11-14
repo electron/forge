66cb30c3bcb17a1a63dc7534d67fb864aabf7b6d
Erick Zhao <erick@hotmail.ca>
2022-11-14 11:17:34 -0800

    fix lint

--- packages/plugin/webpack/test/fixtures/apps/native-modules/webpack.rules.js
+++ packages/plugin/webpack/test/fixtures/apps/native-modules/webpack.rules.js
@@ -4,15 +4,15 @@ module.exports = [
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
     test: /native_modules[/\\].+\.node$/,
-    use: "node-loader",
+    use: 'node-loader',
   },
   {
     test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: "@vercel/webpack-asset-relocator-loader",
+      loader: '@vercel/webpack-asset-relocator-loader',
       options: {
-        outputAssetBase: "native_modules",
+        outputAssetBase: 'native_modules',
       },
     },
   },
