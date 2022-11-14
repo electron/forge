949851c8a109548ec49bb3e674a56079a6c4fcc3
Erick Zhao <erick@hotmail.ca>
2022-11-14 14:33:03 -0800

    feat(template): enable asar by default

--- packages/template/base/tmpl/forge.config.js
+++ packages/template/base/tmpl/forge.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
   rebuildConfig: {},
   makers: [
     {
--- packages/template/webpack-typescript/tmpl/forge.config.ts
+++ packages/template/webpack-typescript/tmpl/forge.config.ts
@@ -9,7 +9,9 @@ import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
-  packagerConfig: {},
+  packagerConfig: {
+    asar: true,
+  },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
   plugins: [
