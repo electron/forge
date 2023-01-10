ac1d56faf720422fbee351dcd875efe02742b148
Erik Moura <erikian@erikian.dev>
2023-01-10 18:04:10 -0300

    fix: move `@electron/fuses` to `devDependencies` and remove `index.ts` from `files`

--- packages/plugin/fuses/src/FusesPlugin.ts
+++ packages/plugin/fuses/src/FusesPlugin.ts
@@ -2,10 +2,6 @@ import path from 'path';
 
 import { namedHookWithTaskFn, PluginBase } from '@electron-forge/plugin-base';
 import { ForgeMultiHookMap } from '@electron-forge/shared-types';
-// Why: users are expected to have this package installed
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-// eslint-disable-next-line import/no-unresolved,node/no-missing-import
 import { flipFuses, FuseConfig } from '@electron/fuses';
 
 import { getElectronExecutablePath } from './util/getElectronExecutablePath';
