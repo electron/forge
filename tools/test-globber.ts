e3e9cc713887a16716e0ff4c70d3e2e9c559891e
Erik Moura <erikian@erikian.dev>
2023-01-31 22:53:38 -0300

    ci: create a `test:slow` script to avoid re-running fast tests in the `slow-tests` job

--- tools/test-globber.ts
+++ tools/test-globber.ts
@@ -7,6 +7,7 @@ import { getPackageInfoSync } from './utils';
 
 const argv = minimist(process.argv.slice(process.argv.findIndex((arg) => arg === 'mocha.opts')));
 
+const isSlow = argv.slow || process.env.TEST_SLOW_ONLY;
 const isFast = argv.fast || process.env.TEST_FAST_ONLY;
 
 const packages = getPackageInfoSync();
@@ -22,7 +23,7 @@ for (const p of packages) {
   if (argv.glob) {
     specGlob.push(path.posix.join(packagePath, argv.glob));
   } else {
-    specGlob.push(path.posix.join(packagePath, 'test', '**', `*_spec${isFast ? '' : '*'}.ts`));
+    specGlob.push(path.posix.join(packagePath, 'test', '**', `*_spec${isFast ? '' : isSlow ? '_slow' : '*'}.ts`));
   }
   testFiles.push(...glob.sync(specGlob));
 }
