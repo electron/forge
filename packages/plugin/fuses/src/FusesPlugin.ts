133ad6350e0ef39844b1cbac23c48fcdd3647ba0
Erik Moura <erikian@erikian.dev>
2023-02-21 16:17:58 -0300

    feat: add Fuses plugin (#3132)

---
+++ packages/plugin/fuses/src/FusesPlugin.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+
+import { namedHookWithTaskFn, PluginBase } from '@electron-forge/plugin-base';
+import { ForgeMultiHookMap } from '@electron-forge/shared-types';
+import { flipFuses, FuseConfig } from '@electron/fuses';
+
+import { getElectronExecutablePath } from './util/getElectronExecutablePath';
+
+export default class FusesPlugin extends PluginBase<FuseConfig> {
+  name = 'fuses';
+
+  fusesConfig = {} as FuseConfig;
+
+  constructor(fusesConfig: FuseConfig) {
+    super(fusesConfig);
+
+    this.fusesConfig = fusesConfig;
+  }
+
+  getHooks(): ForgeMultiHookMap {
+    return {
+      packageAfterCopy: namedHookWithTaskFn<'packageAfterCopy'>(async (listrTask, resolvedForgeConfig, resourcesPath, electronVersion, platform, arch) => {
+        const { fusesConfig } = this;
+
+        if (Object.keys(fusesConfig).length) {
+          const pathToElectronExecutable = getElectronExecutablePath({
+            appName: ['darwin', 'mas'].includes(platform) ? 'Electron' : 'electron',
+            basePath: path.resolve(resourcesPath, '../..'),
+            platform,
+          });
+
+          const osxSignConfig = resolvedForgeConfig.packagerConfig.osxSign;
+          const hasOSXSignConfig = (typeof osxSignConfig === 'object' && Boolean(Object.keys(osxSignConfig).length)) || Boolean(osxSignConfig);
+
+          await flipFuses(pathToElectronExecutable, {
+            resetAdHocDarwinSignature: !hasOSXSignConfig && platform === 'darwin' && arch === 'arm64',
+            ...this.fusesConfig,
+          });
+        }
+      }, 'Flipping Fuses'),
+    };
+  }
+}
+
+export { FusesPlugin };
---
+++ packages/plugin/fuses/test/FusesPlugin_spec_slow.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import path from 'path';
+
+import { CrossSpawnOptions, spawn } from '@malept/cross-spawn-promise';
+import { expect } from 'chai';
+import fsExtra from 'fs-extra';
+
+import { getElectronExecutablePath } from '../src/util/getElectronExecutablePath';
+
+describe('FusesPlugin', () => {
+  const appPath = path.join(__dirname, 'fixtures', 'app');
+
+  const spawnOptions: CrossSpawnOptions = {
+    cwd: appPath,
+    shell: true,
+  };
+
+  const packageJSON = JSON.parse(
+    fs.readFileSync(path.join(appPath, 'package.json'), {
+      encoding: 'utf-8',
+    })
+  );
+
+  const { name: appName } = packageJSON;
+
+  // @TODO get rid of this once https://github.com/electron/forge/pull/3123 lands
+  const platformArchSuffix = `${process.platform}-x64`;
+
+  const outDir = path.join(appPath, 'out', `${appName}-${platformArchSuffix}`);
+
+  before(async () => {
+    delete process.env.TS_NODE_PROJECT;
+    await spawn('yarn', ['install'], spawnOptions);
+  });
+
+  after(async () => {
+    await fsExtra.remove(path.resolve(outDir, '../'));
+
+    // @TODO this can be removed once the mock app installs a published version of @electron-forge/plugin-fuses instead of a local package
+    await fsExtra.remove(path.join(__dirname, './fixtures/app/node_modules'));
+  });
+
+  it('should flip Fuses', async () => {
+    await spawn('yarn', ['package'], spawnOptions);
+
+    const electronExecutablePath = getElectronExecutablePath({
+      appName,
+      basePath: path.join(outDir, ...(process.platform === 'darwin' ? [`${appName}.app`, 'Contents'] : [])),
+      platform: process.platform,
+    });
+
+    /**
+     * If the `RunAsNode` fuse had not been flipped,
+     * this would return the Node.js version (e.g. `v14.16.0`)
+     * instead of the `console.log` from `main.js`.
+     */
+    const output = (
+      await spawn(electronExecutablePath, ['-v'], {
+        env: {
+          ELECTRON_RUN_AS_NODE: '1',
+        },
+      })
+    ).trim();
+
+    expect(output).to.equals('The Fuses plugin is working');
+  });
+});
