6bec6ba81ac433e1c3ca48c7690c546341c74c16
Erick Zhao <erick@hotmail.ca>
2023-06-15 14:52:20 -0700

    feat(template): enable asar by default (#3077)

--- packages/api/core/src/api/init-scripts/init-npm.ts
+++ packages/api/core/src/api/init-scripts/init-npm.ts
@@ -15,7 +15,14 @@ export function siblingDep(name: string): string {
 }
 
 export const deps = ['electron-squirrel-startup'];
-export const devDeps = [siblingDep('cli'), siblingDep('maker-squirrel'), siblingDep('maker-zip'), siblingDep('maker-deb'), siblingDep('maker-rpm')];
+export const devDeps = [
+  siblingDep('cli'),
+  siblingDep('maker-squirrel'),
+  siblingDep('maker-zip'),
+  siblingDep('maker-deb'),
+  siblingDep('maker-rpm'),
+  siblingDep('plugin-auto-unpack-natives'),
+];
 export const exactDevDeps = ['electron'];
 
 export const initNPM = async <T>(dir: string, task: ForgeListrTask<T>): Promise<void> => {
