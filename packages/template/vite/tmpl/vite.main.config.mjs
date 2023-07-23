015734a19ae8f0a2673adf010333242a442de0ac
Marks Polakovs <github@markspolakovs.me>
2023-07-23 21:57:35 +0100

    fix(template/vite): incorrectly bundling browser entrypoints

--- packages/template/vite/tmpl/vite.main.config.mjs
+++ packages/template/vite/tmpl/vite.main.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
+    conditions: ["node"],
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
 });
