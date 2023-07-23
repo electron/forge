4309b1b5385cdff23afa9711eab7c6445de02241
Marks Polakovs <github@markspolakovs.me>
2023-07-23 22:02:49 +0100

    Fix code style

--- packages/template/vite/tmpl/vite.main.config.mjs
+++ packages/template/vite/tmpl/vite.main.config.mjs
@@ -5,7 +5,7 @@ export default defineConfig({
   resolve: {
     // Some libs that can run in both Web and Node.js, such as `axios`, we need to tell Vite to build them in Node.js.
     browserField: false,
-    conditions: ["node"],
+    conditions: ['node'],
     mainFields: ['module', 'jsnext:main', 'jsnext'],
   },
 });
