a7f7a9f2dd0bf892faedae23cc03bd46c173b970
Erick Zhao <erick@hotmail.ca>
2023-08-23 10:26:26 -0700

    build: add circleci test reporting (#3316)

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,16 @@
 const path = require('path');
 
+const reporterConfig = process.env.CI
+  ? {
+      reporter: 'mocha-junit-reporter',
+      'reporter-option': ['mochaFile=./reports/test_output.xml'],
+    }
+  : {};
+
 module.exports = {
   extension: ['ts'],
   require: ['ts-node/register', path.join(__dirname, 'tools', 'test-setup.ts')],
   timeout: 800000,
   recursive: true,
+  ...reporterConfig,
 };
