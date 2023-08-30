9ab274d3e6d056ed9da09f194ffc0119b423b0e0
Erick Zhao <erick@hotmail.ca>
2023-08-30 14:52:38 -0700

    ci: junit reporter adjustments (#3324)

--- .mocharc.js
+++ .mocharc.js
@@ -31,7 +31,7 @@ if (args.length === 0) {
 const reporterConfig = process.env.CI
   ? {
       reporter: 'mocha-junit-reporter',
-      'reporter-option': ['mochaFile=./reports/test_output.xml'],
+      'reporter-option': ['mochaFile=./reports/out/test_output.xml'],
     }
   : {};
 
