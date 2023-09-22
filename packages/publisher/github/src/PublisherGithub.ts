mahnunchik/electron-forge

feat(publisher-github): allow to override existing assets
ISSUES CLOSED: #1760

packages/publisher/github/src/PublisherGithub.ts
@@ -108,9 +108,19 @@ export default class PublisherGithub extends PublisherBase<PublisherGitHubConfig
             .replace(/\.$/g, '')
             .replace(/[^\w.-]/g, '-');
           // eslint-disable-next-line max-len
-          if (release!.assets.find((asset: OctokitReleaseAsset) => asset.name === artifactName)) {
-            return done();
+          const asset = release!.assets.find((asset: OctokitReleaseAsset) => item.name === artifactName);
+          if (asset !== undefined) {
+            if (config.override === true) {
+              await github.getGitHub().repos.deleteReleaseAsset({
+                owner: config.repository.owner,
+                repo: config.repository.name,
+                asset_id: asset.id,
+              });
+            } else {
+              return done();
+            }
           }
+
           await github.getGitHub().repos.uploadReleaseAsset({
             owner: config.repository.owner,
             repo: config.repository.name,

electron/forge

packages/publisher/github/src/PublisherGithub.ts
@@ -108,8 +108,17 @@ export default class PublisherGithub extends PublisherBase<PublisherGitHubConfig
               .replace(/\.$/g, '')
               .replace(/[^\w.-]/g, '-');
             // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-            if (release!.assets.find((asset: OctokitReleaseAsset) => asset.name === artifactName)) {
-              return done();
+            const asset = release!.assets.find((item: OctokitReleaseAsset) => item.name === artifactName);
+            if (asset !== undefined) {
+              if (config.override === true) {
+                await github.getGitHub().repos.deleteReleaseAsset({
+                  owner: config.repository.owner,
+                  repo: config.repository.name,
+                  asset_id: asset.id,
+                });
+              } else {
+                return done();
+              }
             }
             await github.getGitHub().repos.uploadReleaseAsset({
               owner: config.repository.owner,