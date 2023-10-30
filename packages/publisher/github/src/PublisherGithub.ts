30a404c901c79a32857739ea0ef8e2a25913bb2e
Erik Moura <erikian@erikian.dev>
2023-10-30 19:10:59 -0300

    Update packages/publisher/github/src/PublisherGithub.ts
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

--- packages/publisher/github/src/PublisherGithub.ts
+++ packages/publisher/github/src/PublisherGithub.ts
@@ -36,7 +36,7 @@ export default class PublisherGithub extends PublisherBase<PublisherGitHubConfig
 
     if (!(config.repository && typeof config.repository === 'object' && config.repository.owner && config.repository.name)) {
       throw new Error(
-        'In order to publish to Github, you must set the "repository.owner" and "repository.name" properties in your Forge config. See the docs for more info'
+        'In order to publish to GitHub, you must set the "repository.owner" and "repository.name" properties in your Forge config. See the docs for more info'
       );
     }
 
