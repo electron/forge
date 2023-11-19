47194d71baee25d8b6b985339e44dc78f4ced8ed
Erik Moura <erikian@electronjs.org>
2023-11-19 20:35:13 -0300

    chore: update import

--- packages/api/core/src/util/parse-archs.ts
+++ packages/api/core/src/util/parse-archs.ts
@@ -1,8 +1,5 @@
 import { ForgeArch, ForgePlatform } from '@electron-forge/shared-types';
-
-// TODO: convert to an import statement when this is a public API
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { allOfficialArchsForPlatformAndVersion } = require('@electron/packager/src/targets');
+import { allOfficialArchsForPlatformAndVersion } from '@electron/packager';
 
 export default function parseArchs(platform: ForgePlatform | string, declaredArch: ForgeArch | 'all' | string, electronVersion: string): ForgeArch[] {
   if (declaredArch === 'all') {
