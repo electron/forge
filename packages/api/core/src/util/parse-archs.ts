335c388278caa339cdcb253516f1b08d7596cf1d
Erik Moura <erikian@erikian.dev>
2023-12-18 20:02:17 -0300

    chore: update Packager (#3419)
    * chore: update import
    
    * chore: update `@electron/packager` and fix types

--- packages/api/core/src/util/parse-archs.ts
+++ packages/api/core/src/util/parse-archs.ts
@@ -1,12 +1,9 @@
 import { ForgeArch, ForgePlatform } from '@electron-forge/shared-types';
-
-// TODO: convert to an import statement when this is a public API
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { allOfficialArchsForPlatformAndVersion } = require('@electron/packager/src/targets');
+import { allOfficialArchsForPlatformAndVersion, SupportedPlatform } from '@electron/packager';
 
 export default function parseArchs(platform: ForgePlatform | string, declaredArch: ForgeArch | 'all' | string, electronVersion: string): ForgeArch[] {
   if (declaredArch === 'all') {
-    return allOfficialArchsForPlatformAndVersion(platform, electronVersion) || ['x64'];
+    return allOfficialArchsForPlatformAndVersion(platform as SupportedPlatform, electronVersion) || ['x64'];
   }
 
   return declaredArch.split(',') as ForgeArch[];
