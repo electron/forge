69b7554542e3bc7097f9f319df199ba1d17d5b3c
David Sanders <dsanders11@ucsbalum.com>
2024-02-05 17:40:31 -0800

    chore: update import
    Co-authored-by: Erick Zhao <erick@hotmail.ca>

--- packages/publisher/github/src/PublisherGithub.ts
+++ packages/publisher/github/src/PublisherGithub.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import path from 'node:path';
 
 import { PublisherBase, PublisherOptions } from '@electron-forge/publisher-base';
 import { ForgeMakeResult } from '@electron-forge/shared-types';
