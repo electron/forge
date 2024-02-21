50c40f3f33c4439351276071d0afc7e7dc752f36
Lutz Roeder <lutzroeder@users.noreply.github.com>
2024-02-21 09:46:05 -0800

    feat: allow a custom out dir from forge config (#3458)
    Co-authored-by: Keeley Hammond <khammond@slack-corp.com>

--- packages/api/core/src/util/out-dir.ts
+++ packages/api/core/src/util/out-dir.ts
@@ -5,12 +5,15 @@ import { ResolvedForgeConfig } from '@electron-forge/shared-types';
 const BASE_OUT_DIR = 'out';
 
 export default (baseDir: string, forgeConfig: ResolvedForgeConfig): string => {
+  const baseOutDir = forgeConfig.outDir || BASE_OUT_DIR;
+
   if (forgeConfig.buildIdentifier) {
     let identifier = forgeConfig.buildIdentifier;
     if (typeof identifier === 'function') {
       identifier = identifier();
     }
-    if (identifier) return path.resolve(baseDir, BASE_OUT_DIR, identifier);
+    if (identifier) return path.resolve(baseDir, baseOutDir, identifier);
   }
-  return path.resolve(baseDir, BASE_OUT_DIR);
+
+  return path.resolve(baseDir, baseOutDir);
 };
