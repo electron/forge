d80ea3f15c7b631e5c5d2fe13d531d16d27f6c6c
Alice Zhao <alice@Alices-MacBook-Pro-2.local>
2024-03-09 20:41:31 -0800

    chore: run prettier

--- packages/template/base/tmpl/index.js
+++ packages/template/base/tmpl/index.js
@@ -27,16 +27,16 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  
+  createWindow();
+
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
