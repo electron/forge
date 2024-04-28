6ee4eb6acd973e185e75a52661b5289c6aba94bb
SpacingBat3 <git@spacingbat3.anonaddy.com>
2024-04-28 17:50:24 +0200

    feat(core): support ESM module packages
    Replace some require() calls with dynamicImport() to support loading
    makers/publishers/plugins etc. that are ESM makers

--- packages/api/core/helper/dynamic-import.d.ts
+++ packages/api/core/helper/dynamic-import.d.ts
@@ -1,1 +1,3 @@
 export declare function dynamicImport(path: string): Promise<any>;
+/** Like {@linkcode dynamicImport()}, but falls back to require on failure. */
+export declare function dynamicImportMaybe(path: string): Promise<any>;
