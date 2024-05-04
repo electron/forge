1bae6ab81db482308604a67fca3b6c007bd439a2
Leo Wang(草鞋没号) <308487730@qq.com>
2024-05-04 08:14:34 +0800

    feat(plugin-vite): remove copy `dependencies` to built App

--- packages/plugin/vite/src/util/package.ts
+++
@@ -1,8 +0,0 @@
-import { DepType, Walker } from 'flora-colossus';
-
-export async function getFlatDependencies(root: string) {
-  const walker = new Walker(root);
-  const deps = await walker.walkTree();
-
-  return deps.filter((dep) => dep.depType === DepType.PROD);
-}
