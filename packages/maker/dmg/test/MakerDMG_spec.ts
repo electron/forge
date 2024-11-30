7f3e53745eb2fab64549b3cb599f45cdcb487827
Erick Zhao <erick@hotmail.ca>
2024-11-29 22:01:24 -0800

    fix test

--- packages/maker/dmg/test/MakerDMG_spec.ts
+++ packages/maker/dmg/test/MakerDMG_spec.ts
@@ -34,9 +34,6 @@ describe('MakerDMG', () => {
     ensureFileStub = stub().returns(Promise.resolve());
     eidStub = stub().returns(Promise.resolve());
     renameStub = stub().returns(Promise.resolve());
-    config = {
-      appPath: 'fake',
-    };
 
     MakerDMG = proxyquire
       .noPreserveCache()
