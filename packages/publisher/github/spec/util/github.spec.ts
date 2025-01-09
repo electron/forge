8ec5baf27b996e47736ae99193187b4b20250fa9
Erick Zhao <erick@hotmail.ca>
2025-01-08 16:16:58 -0800

    publisher github

--- packages/publisher/github/spec/util/github.spec.ts
+++ packages/publisher/github/spec/util/github.spec.ts
@@ -1,23 +1,20 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
 import { OctokitOptions } from '@octokit/core/dist-types/types.d';
-import { Octokit } from '@octokit/rest';
-import { expect } from 'chai';
-import proxyquire from 'proxyquire';
-import { SinonSpy, spy } from 'sinon';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
-import InternalGitHub from '../src/util/github';
+import GitHub from '../../src/util/github';
 
-describe('GitHub', () => {
-  let GitHub: typeof InternalGitHub;
-  let gitHubSpy: SinonSpy;
-  let MockGitHub;
+const gitHubSpy = vi.fn();
 
-  beforeEach(() => {
-    gitHubSpy = spy();
-    MockGitHub = class {
+vi.mock('@octokit/rest', async (importOriginal) => {
+  const mod = await importOriginal<object>();
+  return {
+    ...mod,
+    Octokit: class {
       private options: OctokitOptions;
 
       constructor(options: OctokitOptions) {
-        gitHubSpy();
+        gitHubSpy(options);
         this.options = options;
       }
 
@@ -25,84 +22,76 @@ describe('GitHub', () => {
       static plugin(): object {
         return this;
       }
-    };
-    GitHub = proxyquire.noCallThru().load('../src/util/github', {
-      '@octokit/rest': {
-        Octokit: MockGitHub,
-      },
-    }).default;
-  });
+    },
+  };
+});
 
+describe('GitHub', () => {
   it('should read token from constructor', () => {
-    expect(new GitHub('token1').token).to.equal('token1');
+    expect(new GitHub('token1').token).toEqual('token1');
   });
 
   it('should fall back to token from environment', () => {
     process.env.GITHUB_TOKEN = 'abc123';
-    expect(new GitHub().token).to.equal('abc123');
+    expect(new GitHub().token).toEqual('abc123');
     delete process.env.GITHUB_TOKEN;
   });
 
   describe('getGitHub', () => {
-    function getOptions(api: Octokit): OctokitOptions {
-      // TODO: figure out if there's a legit way to extract options
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const { options } = api as any;
-      delete options.log;
-      return options;
-    }
+    beforeEach(() => {
+      gitHubSpy.mockClear();
+    });
 
     it('should create a new GitHubAPI', () => {
       const gh = new GitHub();
-      expect(gitHubSpy.callCount).to.equal(0);
+      expect(gitHubSpy).not.toHaveBeenCalled();
       gh.getGitHub();
-      expect(gitHubSpy.callCount).to.equal(1);
+      expect(gitHubSpy).toHaveBeenCalledOnce();
     });
 
     it('should be able to set the Enterprise URL settings', () => {
       const gh = new GitHub('1234', true, {
         baseUrl: 'https://github.example.com:8443/enterprise',
       });
-      const api = gh.getGitHub();
-
-      expect(getOptions(api)).to.deep.equal({
+      gh.getGitHub();
+      expect(gitHubSpy).toHaveBeenCalledWith({
         auth: '1234',
         baseUrl: 'https://github.example.com:8443/enterprise',
         userAgent: 'Electron Forge',
+        log: expect.anything(),
       });
     });
 
     it('should not override the user agent', () => {
       const gh = new GitHub('1234', true, { userAgent: 'Something' });
-      const api = gh.getGitHub();
-
-      expect(getOptions(api).userAgent).to.equal('Electron Forge');
+      gh.getGitHub();
+      expect(gitHubSpy).toHaveBeenCalledWith(expect.objectContaining({ userAgent: 'Electron Forge' }));
     });
 
     it('should authenticate if a token is present', () => {
       const gh = new GitHub('token');
-      const api = gh.getGitHub();
       gh.getGitHub();
-      expect(getOptions(api)).to.deep.equal({
-        auth: 'token',
-        userAgent: 'Electron Forge',
-      });
+      expect(gitHubSpy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          auth: 'token',
+          userAgent: 'Electron Forge',
+        })
+      );
     });
 
     it('should not authenticate if a token is not present', () => {
       const gh = new GitHub();
-      const api = gh.getGitHub();
       gh.getGitHub();
-      expect(getOptions(api)).to.deep.equal({
+      expect(gitHubSpy).toHaveBeenCalledWith({
         userAgent: 'Electron Forge',
+        log: expect.anything(),
       });
     });
-
-    it('should throw an exception if a token is required', () => {
+    it('should throw an exception if a token is required', async () => {
       expect(() => {
         const gh = new GitHub(undefined, true);
         gh.getGitHub();
-      }).to.throw('Please set GITHUB_TOKEN in your environment to access these features');
+      }).toThrow('Please set GITHUB_TOKEN in your environment to access these features');
     });
   });
 
