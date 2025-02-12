0802c42aa4c42f77e8a8fcfde5ca1265e6819201
Erick Zhao <erick@hotmail.ca>
2025-02-11 20:50:39 -0800

    fix: upgrade `commander` 4 -> 11 (#3848)
    * build(deps): upgrade `commander` 4 -> 11
    
    * Update packages/api/cli/src/electron-forge.ts
    
    Co-authored-by: Kevin Cui <github@bugs.cc>
    
    ---------
    
    Co-authored-by: Kevin Cui <github@bugs.cc>

--- packages/api/cli/src/electron-forge-start.ts
+++ packages/api/cli/src/electron-forge-start.ts
@@ -1,11 +1,10 @@
-import path from 'node:path';
-
 import { api, StartOptions } from '@electron-forge/core';
 import { ElectronProcess } from '@electron-forge/shared-types';
-import program from 'commander';
-import fs from 'fs-extra';
+import { program } from 'commander';
 
 import './util/terminate';
+import packageJSON from '../package.json';
+
 import workingDir from './util/working-dir';
 
 (async () => {
@@ -20,7 +19,7 @@ import workingDir from './util/working-dir';
 
   let dir = process.cwd();
   program
-    .version((await fs.readJson(path.resolve(__dirname, '../package.json'))).version, '-V, --version', 'Output the current version')
+    .version(packageJSON.version, '-V, --version', 'Output the current version')
     .arguments('[cwd]')
     .option('-p, --app-path <path>', "Override the path to the Electron app to launch (defaults to '.')")
     .option('-l, --enable-logging', 'Enable advanced logging.  This will log internal Electron things')
@@ -28,35 +27,38 @@ import workingDir from './util/working-dir';
     .option('--vscode', 'Used to enable arg transformation for debugging Electron through VSCode.  Do not use yourself.')
     .option('-i, --inspect-electron', 'Triggers inspect mode on Electron to allow debugging the main process.  Electron >1.7 only')
     .option('--inspect-brk-electron', 'Triggers inspect-brk mode on Electron to allow debugging the main process.  Electron >1.7 only')
-    .helpOption('-h, --help', 'Output usage information')
+    .addHelpText(
+      'after',
+      `
+      Any arguments found after "--" will be passed to the Electron app. For example...
+      
+          $ npx electron-forge start /path/to/project --enable-logging -- -d -f foo.txt
+                                    
+      ...will pass the arguments "-d -f foo.txt" to the Electron app.`
+    )
+    .passThroughOptions(true) // allows args to be passed down to the Electron executable
     .action((cwd) => {
       dir = workingDir(dir, cwd);
     })
     .parse(commandArgs);
 
-  program.on('--help', () => {
-    console.log('  Any arguments found after "--" will be passed to the Electron app, e.g.');
-    console.log('');
-    console.log('    $ electron-forge /path/to/project -l -- -d -f foo.txt');
-    console.log('');
-    console.log('  will pass the arguments "-d -f foo.txt" to the Electron app');
-  });
+  const options = program.opts();
 
   const opts: StartOptions = {
     dir,
     interactive: true,
-    enableLogging: !!program.enableLogging,
-    runAsNode: !!program.runAsNode,
-    inspect: !!program.inspectElectron,
-    inspectBrk: !!program.inspectBrkElectron,
+    enableLogging: !!options.enableLogging,
+    runAsNode: !!options.runAsNode,
+    inspect: !!options.inspectElectron,
+    inspectBrk: !!options.inspectBrkElectron,
   };
 
-  if (program.vscode && appArgs) {
+  if (options.vscode && appArgs) {
     // Args are in the format ~arg~ so we need to strip the "~"
     appArgs = appArgs.map((arg) => arg.substr(1, arg.length - 2)).filter((arg) => arg.length > 0);
   }
 
-  if (program.appPath) opts.appPath = program.appPath;
+  if (options.appPath) opts.appPath = options.appPath;
   if (appArgs) opts.args = appArgs;
 
   const spawned = await api.start(opts);
--- packages/api/cli/src/electron-forge.ts
+++ packages/api/cli/src/electron-forge.ts
@@ -2,35 +2,17 @@
 // This file requires a shebang above. If it is missing, this is an error.
 
 import chalk from 'chalk';
-import program from 'commander';
+import { program } from 'commander';
 import { Listr } from 'listr2';
 
 import './util/terminate';
 
+import packageJSON from '../package.json';
+
 import { checkSystem } from './util/check-system';
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const metadata = require('../package.json');
-
-const originalSC = program.executeSubCommand.bind(program);
-program.executeSubCommand = (argv: string[], args: string[], unknown: string[]) => {
-  let indexOfDoubleDash = process.argv.indexOf('--');
-  indexOfDoubleDash = indexOfDoubleDash < 0 ? process.argv.length + 1 : indexOfDoubleDash;
-
-  const passThroughArgs = args.filter((arg) => process.argv.indexOf(arg) > indexOfDoubleDash);
-  const normalArgs = args.filter((arg) => process.argv.indexOf(arg) <= indexOfDoubleDash);
-
-  let newArgs = args;
-  let newUnknown = unknown;
-  if (passThroughArgs.length > 0) {
-    newArgs = normalArgs.concat(unknown).concat('--').concat(passThroughArgs);
-    newUnknown = [];
-  }
-  return originalSC(argv, newArgs, newUnknown);
-};
-
 program
-  .version(metadata.version, '-V, --version', 'Output the current version')
+  .version(packageJSON.version, '-V, --version', 'Output the current version')
   .option('--verbose', 'Enables verbose mode')
   .helpOption('-h, --help', 'Output usage information')
   .command('init', 'Initialize a new Electron application')
@@ -39,13 +21,8 @@ program
   .command('package', 'Package the current Electron application')
   .command('make', 'Generate distributables for the current Electron application')
   .command('publish', 'Publish the current Electron application')
-  .on('command:*', async (commands) => {
-    if (!program._execs.has(commands[0])) {
-      console.error();
-      console.error(chalk.red(`Unknown command "${program.args.join(' ')}".`));
-      console.error('See --help for a list of available commands.');
-      process.exit(1);
-    } else if (!process.argv.includes('--help') && !process.argv.includes('--h')) {
+  .hook('preSubcommand', async () => {
+    if (!process.argv.includes('--help') && !process.argv.includes('-h')) {
       const runner = new Listr<never>(
         [
           {
@@ -66,7 +43,7 @@ program
       if (runner.errors.length) {
         console.error(
           chalk.red(`\nIt looks like you are missing some dependencies you need to get Electron running.
-Make sure you have git installed and Node.js version ${metadata.engines.node}`)
+Make sure you have git installed and Node.js version ${packageJSON.engines.node}`)
         );
         process.exit(1);
       }
