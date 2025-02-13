e38fd65094750e85c822b14e9871ac7214067656
Erick Zhao <erick@hotmail.ca>
2025-02-12 18:30:30 -0800

    reduce nesting

--- packages/api/cli/src/util/resolve-working-dir.ts
+++ packages/api/cli/src/util/resolve-working-dir.ts
@@ -3,23 +3,21 @@ import path from 'node:path';
 import fs from 'fs-extra';
 
 /**
- * Resolves the directory in which to use
+ * Resolves the directory in which to use a CLI command.
  * @param dir - The directory specified by the user (can be relative or absolute)
- * @param checkExisting - Checks if the directory already exists
+ * @param checkExisting - Checks if the directory exists. If true and directory is non-existent, it will fall back to the current working directory
  * @returns
  */
 export function resolveWorkingDir(dir: string, checkExisting = true): string {
-  let finalDir = process.cwd();
-  if (dir) {
-    if (path.isAbsolute(dir) && (!checkExisting || fs.existsSync(dir))) {
-      finalDir = dir;
-    } else {
-      const resolved = path.resolve(finalDir, dir);
-      if (!checkExisting || fs.existsSync(resolved)) {
-        finalDir = resolved;
-      }
-    }
+  if (!dir) {
+    return process.cwd();
   }
 
-  return finalDir;
+  const resolved = path.isAbsolute(dir) ? dir : path.resolve(process.cwd(), dir);
+
+  if (checkExisting && !fs.existsSync(resolved)) {
+    return process.cwd();
+  } else {
+    return resolved;
+  }
 }
