2e7d1f84241b7af2b3fa9910dd29955ae6aa509e
Erick Zhao <erick@hotmail.ca>
2025-03-05 12:49:15 -0800

    shuffle

--- packages/api/core/spec/slow/api.slow.spec.ts
+++ packages/api/core/spec/slow/api.slow.spec.ts
@@ -169,7 +169,7 @@ describe.each([PACKAGE_MANAGERS['npm'], PACKAGE_MANAGERS['yarn'], PACKAGE_MANAGE
     });
   });
 
-  describe('after init', () => {
+  describe('package', () => {
     beforeAll(async () => {
       dir = path.join(await ensureTestDirIsNonexistent(), 'electron-forge-test');
       await api.init({ dir });
@@ -201,236 +201,267 @@ describe.each([PACKAGE_MANAGERS['npm'], PACKAGE_MANAGERS['yarn'], PACKAGE_MANAGE
       };
     });
 
-    describe('package', () => {
-      it('throws an error when packagerConfig.all is set', async () => {
-        await updatePackageJSON(dir, async (packageJSON) => {
-          assert(packageJSON.config.forge.packagerConfig);
-          packageJSON.config.forge.packagerConfig.all = true;
-        });
-        await expect(api.package({ dir })).rejects.toThrow(/packagerConfig\.all is not supported by Electron Forge/);
-        await updatePackageJSON(dir, async (packageJSON) => {
-          assert(packageJSON.config.forge.packagerConfig);
-          delete packageJSON.config.forge.packagerConfig.all;
-        });
+    it('throws an error when packagerConfig.all is set', async () => {
+      await updatePackageJSON(dir, async (packageJSON) => {
+        assert(packageJSON.config.forge.packagerConfig);
+        packageJSON.config.forge.packagerConfig.all = true;
       });
-
-      it('can package without errors', async () => {
-        await updatePackageJSON(dir, async (packageJSON) => {
-          assert(packageJSON.config.forge.packagerConfig);
-          packageJSON.config.forge.packagerConfig.asar = true;
-        });
-
-        await api.package({ dir });
-
-        // If config.forge is set in package.json, it should not exist in the packaged app's package.json
-        const cleanPackageJSON = await readMetadata({
-          src: path.resolve(dir, 'out', `Test-App-${process.platform}-${process.arch}`),
-          logger: console.error,
-        });
-        expect(cleanPackageJSON).not.toHaveProperty('config.forge');
-
-        // However, it should remain in the original package.json
-        const normalPackageJSON = await readRawPackageJson(dir);
-        expect(normalPackageJSON).toHaveProperty('config.forge');
-      });
-
-      describe('with custom outDir', () => {
-        it('should package to custom directory', async () => {
-          const outDir = `${dir}/foo`;
-
-          expect(fs.existsSync(outDir)).toEqual(false);
-
-          await api.package({ dir, outDir });
-
-          expect(fs.existsSync(outDir)).toEqual(true);
-        });
-
-        it('can make from custom directory', async () => {
-          await updatePackageJSON(dir, async (packageJSON) => {
-            // eslint-disable-next-line n/no-missing-require
-            packageJSON.config.forge.makers = [{ name: require.resolve('@electron-forge/maker-zip') } as IForgeResolvableMaker];
-          });
-
-          await api.make({ dir, skipPackage: true, outDir: `${dir}/foo` });
-
-          // Cleanup here to ensure things dont break in the make tests
-          await fs.promises.rm(path.resolve(dir, 'foo'), { recursive: true, force: true });
-          await fs.promises.rm(path.resolve(dir, 'out'), { recursive: true, force: true });
-        });
-      });
-
-      describe('with prebuilt native module deps installed', () => {
-        beforeAll(async () => {
-          await installDeps(pm, dir, ['ref-napi']);
-
-          return async () => {
-            await fs.promises.rm(path.resolve(dir, 'node_modules/ref-napi'), { recursive: true, force: true });
-            await updatePackageJSON(dir, async (packageJSON) => {
-              delete packageJSON.dependencies['ref-napi'];
-            });
-          };
-        });
-
-        it('should succeed', async () => {
-          await api.package({ dir });
-        });
+      await expect(api.package({ dir })).rejects.toThrow(/packagerConfig\.all is not supported by Electron Forge/);
+      await updatePackageJSON(dir, async (packageJSON) => {
+        assert(packageJSON.config.forge.packagerConfig);
+        delete packageJSON.config.forge.packagerConfig.all;
       });
     });
 
-    describe('make', () => {
-      let devCert: string;
-
-      beforeAll(async () => {
-        if (process.platform === 'win32') {
-          await fs.promises.copyFile(path.join(__dirname, '..', 'fixture', 'bogus-private-key.pvk'), path.join(dir, 'default.pvk'));
-          devCert = await createDefaultCertificate('CN=Test Author', { certFilePath: dir });
-        }
-
-        if (process.platform !== 'win32') {
-          process.env.DISABLE_SQUIRREL_TEST = 'true';
-        }
+    it('can package without errors', async () => {
+      await updatePackageJSON(dir, async (packageJSON) => {
+        assert(packageJSON.config.forge.packagerConfig);
+        packageJSON.config.forge.packagerConfig.asar = true;
       });
 
-      function getMakers(good: boolean) {
-        const allMakers = [
-          '@electron-forge/maker-appx',
-          '@electron-forge/maker-deb',
-          '@electron-forge/maker-dmg',
-          '@electron-forge/maker-flatpak',
-          '@electron-forge/maker-rpm',
-          '@electron-forge/maker-snap',
-          '@electron-forge/maker-squirrel',
-          '@electron-forge/maker-wix',
-          '@electron-forge/maker-zip',
-        ];
-        return allMakers
-          .map((maker) => require.resolve(maker))
-          .filter((makerPath) => {
-            // eslint-disable-next-line @typescript-eslint/no-require-imports
-            const MakerClass = require(makerPath).default;
-            const maker = new MakerClass();
+      await api.package({ dir });
 
-            // Skip the Squirrel.Windows maker test on non-Windows platforms
-            if (makerPath.includes('MakerSquirrel') && process.platform !== 'win32') {
-              return false;
-            }
-            return maker.isSupportedOnCurrentPlatform() === good && maker.externalBinariesExist() === good;
-          })
-          .map((makerPath) => {
-            const makerDefinition = {
-              name: makerPath,
-              platforms: [process.platform],
-              config: {
-                devCert,
-              },
-            };
+      // If config.forge is set in package.json, it should not exist in the packaged app's package.json
+      const cleanPackageJSON = await readMetadata({
+        src: path.resolve(dir, 'out', `Test-App-${process.platform}-${process.arch}`),
+        logger: console.error,
+      });
+      expect(cleanPackageJSON).not.toHaveProperty('config.forge');
 
-            if (process.platform === 'win32') {
-              (makerDefinition.config as Record<string, unknown>).makeVersionWinStoreCompatible = true;
-            }
+      // However, it should remain in the original package.json
+      const normalPackageJSON = await readRawPackageJson(dir);
+      expect(normalPackageJSON).toHaveProperty('config.forge');
+    });
 
-            return makerDefinition;
+    describe('with custom outDir', () => {
+      it('should package to custom directory', async () => {
+        const outDir = `${dir}/foo`;
+
+        expect(fs.existsSync(outDir)).toEqual(false);
+
+        await api.package({ dir, outDir });
+
+        expect(fs.existsSync(outDir)).toEqual(true);
+      });
+
+      it('can make from custom directory', async () => {
+        await updatePackageJSON(dir, async (packageJSON) => {
+          // eslint-disable-next-line n/no-missing-require
+          packageJSON.config.forge.makers = [{ name: require.resolve('@electron-forge/maker-zip') } as IForgeResolvableMaker];
+        });
+
+        await api.make({ dir, skipPackage: true, outDir: `${dir}/foo` });
+
+        // Cleanup here to ensure things dont break in the make tests
+        await fs.promises.rm(path.resolve(dir, 'foo'), { recursive: true, force: true });
+        await fs.promises.rm(path.resolve(dir, 'out'), { recursive: true, force: true });
+      });
+    });
+
+    describe('with prebuilt native module deps installed', () => {
+      beforeAll(async () => {
+        await installDeps(pm, dir, ['ref-napi']);
+
+        return async () => {
+          await fs.promises.rm(path.resolve(dir, 'node_modules/ref-napi'), { recursive: true, force: true });
+          await updatePackageJSON(dir, async (packageJSON) => {
+            delete packageJSON.dependencies['ref-napi'];
           });
+        };
+      });
+
+      it('should succeed', async () => {
+        await api.package({ dir });
+      });
+    });
+  });
+
+  describe('make', () => {
+    let devCert: string;
+
+    beforeAll(async () => {
+      dir = path.join(await ensureTestDirIsNonexistent(), 'electron-forge-test');
+      await api.init({ dir });
+
+      await updatePackageJSON(dir, async (packageJSON) => {
+        packageJSON.name = 'testapp';
+        packageJSON.version = '1.0.0-beta.1';
+        packageJSON.productName = 'Test-App';
+        packageJSON.config = packageJSON.config || {};
+        packageJSON.config.forge = {
+          ...packageJSON.config.forge,
+          packagerConfig: {
+            asar: false,
+          },
+        };
+
+        if (process.platform === 'linux') {
+          packageJSON.config.forge.packagerConfig = {
+            ...packageJSON.config.forge.packagerConfig,
+            executableName: 'testapp',
+          };
+        }
+        packageJSON.homepage = 'http://www.example.com/';
+        packageJSON.author = 'Test Author';
+      });
+
+      await api.package({ dir });
+
+      return async () => {
+        await fs.promises.rm(dir, { recursive: true, force: true });
+      };
+    });
+
+    beforeAll(async () => {
+      if (process.platform === 'win32') {
+        await fs.promises.copyFile(path.join(__dirname, '..', 'fixture', 'bogus-private-key.pvk'), path.join(dir, 'default.pvk'));
+        devCert = await createDefaultCertificate('CN=Test Author', { certFilePath: dir });
       }
 
-      const goodMakers = getMakers(true);
-      const badMakers = getMakers(false);
+      if (process.platform !== 'win32') {
+        process.env.DISABLE_SQUIRREL_TEST = 'true';
+      }
+    });
 
-      describe.each(goodMakers.map((m) => [path.basename(m.name), m]))('for target %s', async (_, target) => {
-        beforeAll(async () => {
-          await updatePackageJSON(dir, async (packageJSON) => {
-            packageJSON.config.forge.makers = [target as IForgeResolvableMaker];
-          });
-        });
-        it('can make without errors', async () => {
-          const outputs = await api.make({
-            dir,
-            skipPackage: true,
-          });
-          for (const outputResult of outputs) {
-            for (const output of outputResult.artifacts) {
-              expect(fs.existsSync(output)).toEqual(true);
-              expect(output.startsWith(path.resolve(dir, 'out', 'make'))).toEqual(true);
-            }
+    function getMakers(good: boolean) {
+      const allMakers = [
+        '@electron-forge/maker-appx',
+        '@electron-forge/maker-deb',
+        '@electron-forge/maker-dmg',
+        '@electron-forge/maker-flatpak',
+        '@electron-forge/maker-rpm',
+        '@electron-forge/maker-snap',
+        '@electron-forge/maker-squirrel',
+        '@electron-forge/maker-wix',
+        '@electron-forge/maker-zip',
+      ];
+      return allMakers
+        .map((maker) => require.resolve(maker))
+        .filter((makerPath) => {
+          const MakerClass = require(makerPath).default;
+          const maker = new MakerClass();
+
+          // Skip the Squirrel.Windows maker test on non-Windows platforms
+          if (makerPath.includes('MakerSquirrel') && process.platform !== 'win32') {
+            return false;
           }
+          return maker.isSupportedOnCurrentPlatform() === good && maker.externalBinariesExist() === good;
+        })
+        .map((makerPath) => {
+          const makerDefinition = {
+            name: makerPath,
+            platforms: [process.platform],
+            config: {
+              devCert,
+            },
+          };
+
+          if (process.platform === 'win32') {
+            (makerDefinition.config as Record<string, unknown>).makeVersionWinStoreCompatible = true;
+          }
+
+          return makerDefinition;
+        });
+    }
+
+    const goodMakers = getMakers(true);
+    const badMakers = getMakers(false);
+
+    describe.each(goodMakers.map((m) => [path.basename(m.name), m]))('for target %s', async (_, target) => {
+      beforeAll(async () => {
+        await updatePackageJSON(dir, async (packageJSON) => {
+          packageJSON.config.forge.makers = [target as IForgeResolvableMaker];
         });
       });
 
-      describe.each(badMakers.map((m) => [path.basename(m.name), m]))('for target %s', async (_, target) => {
-        beforeAll(async () => {
-          await updatePackageJSON(dir, async (packageJSON) => {
-            packageJSON.config.forge.makers = [target as IForgeResolvableMaker];
-          });
+      it('can make without errors', async () => {
+        const outputs = await api.make({
+          dir,
+          skipPackage: true,
         });
+        for (const outputResult of outputs) {
+          for (const output of outputResult.artifacts) {
+            expect(fs.existsSync(output)).toEqual(true);
+            expect(output.startsWith(path.resolve(dir, 'out', 'make'))).toEqual(true);
+          }
+        }
+      });
+    });
 
-        it(`fails`, async () => {
-          await expect(
-            api.make({
-              dir,
-              skipPackage: true,
-            })
-          ).rejects.toThrow();
+    describe.each(badMakers.map((m) => [path.basename(m.name), m]))('for target %s', async (_, target) => {
+      beforeAll(async () => {
+        await updatePackageJSON(dir, async (packageJSON) => {
+          packageJSON.config.forge.makers = [target as IForgeResolvableMaker];
         });
       });
 
-      it('throws an error when given an unrecognized platform', async () => {
-        await expect(api.make({ dir, platform: 'dos' })).rejects.toThrow(/invalid platform/);
-      });
-
-      it("throws an error when the specified maker doesn't support the current platform", async () => {
-        const makerPath = path.resolve(__dirname, '../fixture/maker-unsupported');
+      it(`throws due to unsupported platform`, async () => {
         await expect(
           api.make({
             dir,
-            overrideTargets: [
-              {
-                name: makerPath,
-              } as IForgeResolvableMaker,
-            ],
             skipPackage: true,
           })
-        ).rejects.toThrow(/the maker declared that it cannot run/);
+        ).rejects.toThrow();
       });
+    });
 
-      it("throws an error when the specified maker doesn't implement isSupportedOnCurrentPlatform()", async () => {
-        const makerPath = path.resolve(__dirname, '../fixture/maker-incompatible');
-        await expect(
-          api.make({
-            dir,
-            overrideTargets: [
-              {
-                name: makerPath,
-              } as IForgeResolvableMaker,
-            ],
-            skipPackage: true,
-          })
-        ).rejects.toThrow(/incompatible with this version/);
-      });
+    it('throws an error when given an unrecognized platform', async () => {
+      await expect(api.make({ dir, platform: 'dos' })).rejects.toThrow(/invalid platform/);
+    });
 
-      it('throws an error when no makers are configured for the given platform', async () => {
-        await expect(
-          api.make({
-            dir,
-            overrideTargets: [
-              {
-                name: path.resolve(__dirname, '../fixture/maker-wrong-platform'),
-              } as IForgeResolvableMaker,
-            ],
-            platform: 'linux',
-            skipPackage: true,
-          })
-        ).rejects.toThrow('Could not find any make targets configured for the "linux" platform.');
-      });
+    it("throws an error when the specified maker doesn't support the current platform", async () => {
+      const makerPath = path.resolve(__dirname, '../fixture/maker-unsupported');
+      await expect(
+        api.make({
+          dir,
+          overrideTargets: [
+            {
+              name: makerPath,
+            } as IForgeResolvableMaker,
+          ],
+          skipPackage: true,
+        })
+      ).rejects.toThrow(/the maker declared that it cannot run/);
+    });
 
-      it.runIf(process.platform === 'darwin')('can make for the MAS platform successfully', async () => {
-        await expect(
-          api.make({
-            dir,
-            overrideTargets: [require.resolve('@electron-forge/maker-zip'), require.resolve('@electron-forge/maker-dmg')],
-            platform: 'mas',
-          })
-        ).resolves.toHaveLength(2);
-      });
+    it("throws an error when the specified maker doesn't implement isSupportedOnCurrentPlatform()", async () => {
+      const makerPath = path.resolve(__dirname, '../fixture/maker-incompatible');
+      await expect(
+        api.make({
+          dir,
+          overrideTargets: [
+            {
+              name: makerPath,
+            } as IForgeResolvableMaker,
+          ],
+          skipPackage: true,
+        })
+      ).rejects.toThrow(/incompatible with this version/);
+    });
+
+    it('throws an error when no makers are configured for the given platform', async () => {
+      await expect(
+        api.make({
+          dir,
+          overrideTargets: [
+            {
+              name: path.resolve(__dirname, '../fixture/maker-wrong-platform'),
+            } as IForgeResolvableMaker,
+          ],
+          platform: 'linux',
+          skipPackage: true,
+        })
+      ).rejects.toThrow('Could not find any make targets configured for the "linux" platform.');
+    });
+
+    it.runIf(process.platform === 'darwin')('can make for the MAS platform successfully', async () => {
+      await expect(
+        api.make({
+          dir,
+          overrideTargets: [require.resolve('@electron-forge/maker-zip'), require.resolve('@electron-forge/maker-dmg')],
+          platform: 'mas',
+        })
+      ).resolves.toHaveLength(2);
     });
   });
 });
