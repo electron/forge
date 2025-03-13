b312e3200b4d0e6a32d3c261dc56d8c5a4a3869c
Erick Zhao <erick@hotmail.ca>
2025-03-13 14:29:24 -0700

    init packages properly

--- packages/plugin/fuses/spec/FusesPlugin.slow.spec.ts
+++ packages/plugin/fuses/spec/FusesPlugin.slow.spec.ts
@@ -1,9 +1,11 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
+import { PACKAGE_MANAGERS } from '@electron-forge/core-utils';
 import { CrossSpawnOptions, spawn } from '@malept/cross-spawn-promise';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
+import { initLink } from '../../../api/core/src/api/init-scripts/init-link';
 import { getElectronExecutablePath } from '../src/util/getElectronExecutablePath';
 
 describe('FusesPlugin', () => {
@@ -28,6 +30,13 @@ describe('FusesPlugin', () => {
     delete process.env.TS_NODE_PROJECT;
     await fs.promises.copyFile(path.join(appPath, 'package.json.tmpl'), path.join(appPath, 'package.json'));
     await spawn('yarn', ['install'], spawnOptions);
+
+    // Installing deps removes symlinks that were added at the start of this
+    // spec via `api.init`. So we should re-link local forge dependencies
+    // again.
+    process.env.LINK_FORGE_DEPENDENCIES_ON_INIT = '1';
+    await initLink(PACKAGE_MANAGERS['yarn'], appPath);
+    delete process.env.LINK_FORGE_DEPENDENCIES_ON_INIT;
   });
 
   afterAll(async () => {
