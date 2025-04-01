1be86f8db89d6f0c57b48651b294a6927fd32fce
Josef Fröhle <github@josef-froehle.de>
2025-04-01 08:26:04 +0000

    Allow modification of ChecksumCalculation

--- packages/publisher/s3/src/PublisherS3.ts
+++ packages/publisher/s3/src/PublisherS3.ts
@@ -48,6 +48,8 @@ export default class PublisherS3 extends PublisherStatic<PublisherS3Config> {
       region: this.config.region,
       endpoint: this.config.endpoint,
       forcePathStyle: !!this.config.s3ForcePathStyle,
+      requestChecksumCalculation: this.config.requestChecksumCalculation || "WHEN_SUPPORTED",
+      responseChecksumValidation: this.config.responseChecksumValidation || "WHEN_SUPPORTED",
     });
 
     d('creating s3 client with options:', this.config);
