9662559a0b959744a58e5fdf58f4d8df084fb66a
sethcg <sethcg@hotmail.com>
2025-05-02 15:34:08 -0400

    fixed linting error

--- packages/template/vite-react-typescript/tmpl/renderer.tsx
+++ packages/template/vite-react-typescript/tmpl/renderer.tsx
@@ -26,13 +26,13 @@
  * ```
  */
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './index.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './index.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
