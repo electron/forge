da27ff0648581a6fcb3fd16fbbaa70d94300fffd
sethcg <sethcg@hotmail.com>
2025-05-02 15:34:08 -0400

    fixed linting error

--- packages/template/vite-react-typescript/tmpl/index.tsx
+++ packages/template/vite-react-typescript/tmpl/index.tsx
@@ -1,14 +1,16 @@
 function App() {
-    console.log('👋 This message is being logged by "index.tsx", using React and Vite');
-  
-    return (
-      <>
-        <div>      
-          <h1>💖 Hello World!</h1>
-          <p>Welcome to your Electron application.</p>
-        </div>
-      </>
-    )
-  }
-  
-  export default App
+  console.log(
+    '👋 This message is being logged by "index.tsx", using React and Vite'
+  );
+
+  return (
+    <>
+      <div>
+        <h1>💖 Hello World!</h1>
+        <p>Welcome to your Electron application.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
