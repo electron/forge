aeeb398ca9c19cb4eeaa7d18b2d89092b95281fc
Jan Hannemann <jhannemann@slack-corp.com>
2025-08-21 12:17:36 -0700

    test: unit test and maker test inclusion

---
+++ packages/maker/msix/spec/util/arch.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest';
+
+import { toMsixArch } from '../../src/util/arch';
+
+describe('toMsixArch', () => {
+  [
+    {
+      arch: 'x64',
+      expectedMsixArch: 'x64',
+    },
+    {
+      arch: 'arm64',
+      expectedMsixArch: 'arm64',
+    },
+    {
+      arch: 'x86',
+      expectedMsixArch: 'x86',
+    },
+    {
+      arch: 'arm',
+      expectedMsixArch: 'arm',
+    },
+    {
+      arch: 'ia32',
+      expectedMsixArch: 'x86',
+    },
+  ].forEach((test) => {
+    it(`${test.arch} converts to ${test.expectedMsixArch}`, () => {
+      expect(toMsixArch(test.arch)).toBe(test.expectedMsixArch);
+    });
+  });
+
+  it(`throw for arch's without a match`, () => {
+    expect(toMsixArch('armv7l')).toThrowError();
+  });
+});
