c9e9c2971e480021bf08d1f0c5f4b7a7e693aeb2
sethcg <sethcg@hotmail.com>
2025-08-31 15:05:41 -0400

    attempt to fix tests

--- packages/template/vite-react/spec/ViteReactTemplate.slow.spec.ts
+++ packages/template/vite-react/spec/ViteReactTemplate.slow.spec.ts
@@ -16,6 +16,8 @@ describe('ViteReactTemplate', () => {
   let dir: string;
 
   beforeAll(async () => {
+    await spawnPackageManager(PACKAGE_MANAGERS['yarn'], ['install']);
+    await spawnPackageManager(PACKAGE_MANAGERS['yarn'], ['build']);
     await spawnPackageManager(PACKAGE_MANAGERS['yarn'], [
       'run',
       'link:prepare',
