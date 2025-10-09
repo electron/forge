c9145758b22d8ac8e0a3e4f3509a4ad1a51348e4
Samuel Attard <sam@electronjs.org>
2025-10-09 15:40:10 -0700

    fix: do squirrel maker in a tmp folder (#4022)

--- packages/maker/squirrel/src/MakerSquirrel.ts
+++ packages/maker/squirrel/src/MakerSquirrel.ts
@@ -1,3 +1,4 @@
+import os from 'node:os';
 import path from 'node:path';
 
 import { MakerBase, MakerOptions } from '@electron-forge/maker-base';
@@ -37,51 +38,64 @@ export default class MakerSquirrel extends MakerBase<MakerSquirrelConfig> {
     const outPath = path.resolve(makeDir, `squirrel.windows/${targetArch}`);
     await this.ensureDirectory(outPath);
 
-    const winstallerConfig: ElectronWinstallerOptions = {
-      name:
-        typeof packageJSON.name === 'string'
-          ? packageJSON.name.replace(/-/g, '_')
-          : undefined, // squirrel hates hyphens
-      title: appName,
-      noMsi: true,
-      exe: `${forgeConfig.packagerConfig.executableName || appName}.exe`,
-      setupExe: `${appName}-${packageJSON.version} Setup.exe`,
-      ...this.config,
-      appDirectory: dir,
-      outputDirectory: outPath,
-    };
+    const tmpFolder = await fs.mkdtemp(
+      path.resolve(os.tmpdir(), 'squirrel-maker-'),
+    );
 
-    await createWindowsInstaller(winstallerConfig);
+    await fs.copy(dir, tmpFolder);
 
-    const nupkgVersion = convertVersion(packageJSON.version);
+    try {
+      const winstallerConfig: ElectronWinstallerOptions = {
+        name:
+          typeof packageJSON.name === 'string'
+            ? packageJSON.name.replace(/-/g, '_')
+            : undefined, // squirrel hates hyphens
+        title: appName,
+        noMsi: true,
+        exe: `${forgeConfig.packagerConfig.executableName || appName}.exe`,
+        setupExe: `${appName}-${packageJSON.version} Setup.exe`,
+        ...this.config,
+        appDirectory: tmpFolder,
+        outputDirectory: outPath,
+      };
 
-    const artifacts = [
-      path.resolve(outPath, 'RELEASES'),
-      path.resolve(outPath, winstallerConfig.setupExe || `${appName}Setup.exe`),
-      path.resolve(
+      await createWindowsInstaller(winstallerConfig);
+
+      const nupkgVersion = convertVersion(packageJSON.version);
+
+      const artifacts = [
+        path.resolve(outPath, 'RELEASES'),
+        path.resolve(
+          outPath,
+          winstallerConfig.setupExe || `${appName}Setup.exe`,
+        ),
+        path.resolve(
+          outPath,
+          `${winstallerConfig.name}-${nupkgVersion}-full.nupkg`,
+        ),
+      ];
+      const deltaPath = path.resolve(
         outPath,
-        `${winstallerConfig.name}-${nupkgVersion}-full.nupkg`,
-      ),
-    ];
-    const deltaPath = path.resolve(
-      outPath,
-      `${winstallerConfig.name}-${nupkgVersion}-delta.nupkg`,
-    );
-    if (
-      winstallerConfig.remoteReleases &&
-      !winstallerConfig.noDelta &&
-      (await fs.pathExists(deltaPath))
-    ) {
-      artifacts.push(deltaPath);
+        `${winstallerConfig.name}-${nupkgVersion}-delta.nupkg`,
+      );
+      if (
+        winstallerConfig.remoteReleases &&
+        !winstallerConfig.noDelta &&
+        (await fs.pathExists(deltaPath))
+      ) {
+        artifacts.push(deltaPath);
+      }
+      const msiPath = path.resolve(
+        outPath,
+        winstallerConfig.setupMsi || `${appName}Setup.msi`,
+      );
+      if (!winstallerConfig.noMsi && (await fs.pathExists(msiPath))) {
+        artifacts.push(msiPath);
+      }
+      return artifacts;
+    } finally {
+      await fs.remove(tmpFolder);
     }
-    const msiPath = path.resolve(
-      outPath,
-      winstallerConfig.setupMsi || `${appName}Setup.msi`,
-    );
-    if (!winstallerConfig.noMsi && (await fs.pathExists(msiPath))) {
-      artifacts.push(msiPath);
-    }
-    return artifacts;
   }
 }
 
