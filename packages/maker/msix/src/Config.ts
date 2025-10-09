479e73a8604c16c04ed11522d832430ce81fb85d
Samuel Attard <sattard@anthropic.com>
2025-10-08 19:34:03 -0700

    fix(msix): use temp staging folder and fix types

--- packages/maker/msix/src/Config.ts
+++ packages/maker/msix/src/Config.ts
@@ -1,13 +1,31 @@
-import { PackagingOptions as MSIXPackagingOptions } from 'electron-windows-msix';
+import {
+  ManifestGenerationVariables,
+  PackagingOptions as MSIXPackagingOptions,
+} from 'electron-windows-msix';
+
+type SemiPartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /**
  * The configuration object for the MSIX maker.
  * The `outputDir` and `appDir` parameters are preconfigured by Forge so that the
  * Maker uses the package output and can be then used to publish the app.
+ * 
+ * Certain manifest variables are given good defaults by Forge, you can override these
+ * if required.
  *
  * @see https://github.com/bitdisaster/electron-windows-msix/blob/master/src/types.ts
  */
 export type MakerMSIXConfig = Omit<
   MSIXPackagingOptions,
-  'outputDir' | 'appDir'
->;
+  'outputDir' | 'appDir' | 'manifestVariables'
+> & {
+  manifestVariables?: SemiPartial<
+    ManifestGenerationVariables,
+    | 'packageDescription'
+    | 'appExecutable'
+    | 'packageVersion'
+    | 'publisher'
+    | 'packageIdentity'
+    | 'targetArch'
+  >;
+};
