67428c7a001f191e502ed34baa1c1a25fb59da79
Erick Zhao <erick@hotmail.ca>
2025-10-10 18:06:03 -0700

    test again

--- packages/template/base/src/BaseTemplate.ts
+++ packages/template/base/src/BaseTemplate.ts
@@ -140,18 +140,17 @@ export class BaseTemplate implements ForgeTemplate {
 
     const pm = await resolvePackageManager();
 
-    // As of pnpm v10, no postinstall scripts will run unless allowlisted through `onlyBuiltDependencies`
     if (pm.executable === 'pnpm') {
+      d('Adding Electron dependencies to `onlyBuiltDependencies`');
       packageJSON.pnpm = {
         onlyBuiltDependencies: ['electron', 'electron-winstaller'],
       };
-    }
-
-    if (
+    } else if (
       pm.executable === 'yarn' &&
       typeof pm.version === 'string' &&
       semver.gte(pm.version, '2.0.0')
     ) {
+      d('Detected Yarn 2+, adding packageManager field to package.json');
       packageJSON.packageManager = `yarn@${pm.version}`;
     }
 
