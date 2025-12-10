e2b6201de6694c718a5cd857a70703fce28ebfca
Erick Zhao <erick@hotmail.ca>
2025-12-10 14:36:42 -0800

    ci: add trusted publishing to Forge 7 (#4088)
    * first pass
    
    * chmod +x

---
+++ tools/version.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Error: Working directory is not clean. Please commit or stash your changes before running lerna:version."
+  exit 1
+fi
+
+echo "Running lerna version..."
+lerna version \
+  --force-publish \
+  --no-changelog \
+	--conventional-commits \
+  --exact \
+  --no-git-tag-version \
+  --no-push
+
+# Releaser may decline to apply version changes. Exit early in that case.
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No version changes were made. Exiting."
+  exit 0
+fi
+
+BRANCH_NAME="v7/$(date +'%y%m%d-%I-%M')"
+echo "Creating branch: $BRANCH_NAME"
+git checkout -b "$BRANCH_NAME"
+
+echo "Committing changes..."
+git commit -a -m 'chore: version bump' -m '<trigger_release>'
+
+echo "Version bump complete! Branch $BRANCH_NAME created and changes committed."
