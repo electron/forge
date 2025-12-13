5b24ac946fad8bd45a13a8de3cee579330ed9282
Erick Zhao <erick@hotmail.ca>
2025-12-12 17:14:51 -0800

    gg

--- packages/api/core/src/api/init.ts
+++ packages/api/core/src/api/init.ts
@@ -22,6 +22,8 @@ import { initNPM } from './init-scripts/init-npm';
 
 const d = debug('electron-forge:init');
 
+console.log('erick test 123');
+
 export interface InitOptions {
   /**
    * The path to the app to be initialized
