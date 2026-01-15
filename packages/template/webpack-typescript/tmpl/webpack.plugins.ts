3d524f74ca744a4ad20042684affc8e66e8d5eaa
Erick Zhao <erick@hotmail.ca>
2026-01-15 15:13:55 -0800

    fix(template): modernize eslint deps (#4132)

--- packages/template/webpack-typescript/tmpl/webpack.plugins.ts
+++ packages/template/webpack-typescript/tmpl/webpack.plugins.ts
@@ -1,6 +1,6 @@
 import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 export const plugins = [
