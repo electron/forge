329ffcc3577574a22353a5ba2932435d7b9f5923
SpacingBat3 <spacingbat3@gmail.com>
2026-07-19 16:19:43 +0200

    fix(plugin-vite): expose `VitePlugin*Config`

--- packages/plugin/vite/src/VitePlugin.ts
+++ packages/plugin/vite/src/VitePlugin.ts
@@ -538,4 +538,8 @@ function getServerURLs(urls: vite.ResolvedServerUrls) {
   return output;
 }
 
-export { VitePlugin };
+export { VitePlugin, VitePluginConfig };
+export type {
+  VitePluginBuildConfig,
+  VitePluginRendererConfig,
+} from './Config.ts';
