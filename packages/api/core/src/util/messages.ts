bfeb3552c07b7adc980ca1b1d73c6225e8e00e94
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2026-07-25 09:13:17 +0530

    feat(core): add truncateMiddle helper function to core util messages

--- packages/api/core/src/util/messages.ts
+++ packages/api/core/src/util/messages.ts
@@ -9,3 +9,12 @@ export function warn(interactive: boolean, message: string): void {
     console.warn(message);
   }
 }
+
+export function truncateMiddle(str: string, maxLength: number): string {
+  if (typeof str !== 'string' || str.length <= maxLength || maxLength <= 3) {
+    return str;
+  }
+  const charsToShow = Math.ceil((maxLength - 3) / 2);
+  const backChars = Math.floor((maxLength - 3) / 2);
+  return `${str.slice(0, charsToShow)}...${str.slice(str.length - backChars)}`;
+}
